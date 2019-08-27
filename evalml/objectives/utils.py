79e62918500b2f3116c70c7361a26f757928937f
Jeremy Shih <jeremyliweishih@gmail.com>
2019-08-27 11:24:03 -0400

    Added all cases to get_objevtive

--- evalml/objectives/utils.py
+++ evalml/objectives/utils.py
@@ -11,27 +11,27 @@ def get_objective(objective):
     objective = objective.lower()
     extra_param = None
 
-    if '_' in objective:
-        extra_param = objective.split('_')[1]
-        objective = objective.split('_')[0]
-
-    if extra_param:
-        options = {
-            "f1": standard_metrics.F1(average=extra_param),
-            "precision": standard_metrics.Precision(average=extra_param),
-            "recall": standard_metrics.Recall(average=extra_param),
-            "auc": standard_metrics.AUC(average=extra_param),
-        }
-    else:
-        options = {
-            "f1": standard_metrics.F1(),
-            "precision": standard_metrics.Precision(),
-            "recall": standard_metrics.Recall(),
-            "auc": standard_metrics.AUC(),
-            "log_loss": standard_metrics.LogLoss(),
-            "mcc": standard_metrics.MCC(),
-            "r2": standard_metrics.R2(),
-        }
+    options = {
+        "f1": standard_metrics.F1(),
+        'f1_micro': standard_metrics.F1Micro(),
+        'f1_macro': standard_metrics.F1Macro(),
+        'f1_weighted': standard_metrics.F1Weighted(),
+        "precision": standard_metrics.Precision(),
+        "precision_micro": standard_metrics.PrecisionMicro(),
+        "precision_macro": standard_metrics.PrecisionMacro(),
+        "precision_weighted": standard_metrics.PrecisionWeighted(),
+        "recall": standard_metrics.Recall(),
+        "recall_micro": standard_metrics.RecallMicro(),
+        "recall_macro": standard_metrics.RecallMacro(),
+        "recall_weighted": standard_metrics.RecallWeighted(),
+        "auc": standard_metrics.AUC(),
+        "auc_micro": standard_metrics.AUCMicro(),
+        "auc_macro": standard_metrics.AUCMacro(),
+        "auc_weighted": standard_metrics.AUCWeighted(),
+        "log_loss": standard_metrics.LogLoss(),
+        "mcc": standard_metrics.MCC(),
+        "r2": standard_metrics.R2(),
+    }
 
     return options[objective]
 
