76f6e0927f4e56bda2deaf81407ad51897997e6c
Jeremy Shih <jeremyliweishih@gmail.com>
2019-09-06 10:56:58 -0400

    Remove auto

--- evalml/problem_types/problem_types.py
+++ evalml/problem_types/problem_types.py
@@ -1,12 +1,7 @@
-from enum import Enum, auto
+from enum import Enum
 
 
-class AutoName(Enum):
-    def _generate_next_value_(name, start, count, last_values):
-        return name
-
-
-class ProblemTypes(AutoName):
-    BINARY = auto()
-    MULTICLASS = auto()
-    REGRESSION = auto()
+class ProblemTypes(Enum):
+    BINARY = 'BINARY'
+    MULTICLASS = 'MULTICLASS'
+    REGRESSION = 'REGRESSION'
