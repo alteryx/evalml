c26dfa33aa8efb33e4e609429e96685d21bb8077
Angela Lin <angela97lin@gmail.com>
2019-09-24 13:53:08 -0400

    adding skeleton estimator/transfomer classes

---
+++ evalml/pipelines/components/estimator.py
@@ -0,0 +1,18 @@
+from component_base import ComponentBase
+
+
+class Estimator(ComponentBase):
+    def __init__(self, name, component_type, hyperparameters, needs_fitting=False, component_obj=None):
+        super().__init__(name, component_type, hyperparameters, needs_fitting, component_obj)
+
+    def fit(self, X, y, objective_fit_size=.2):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def predict_proba(self, X):
+        pass
+
+    def score(self, X, y, other_objectives=None):
+        pass
---
+++ evalml/pipelines/components/transformer.py
@@ -0,0 +1,12 @@
+from component_base import ComponentBase
+
+
+class Transformer(ComponentBase):
+    def __init__(self, name, component_type, hyperparameters, needs_fitting=False, component_obj=None):
+        super().__init__(name, component_type, hyperparameters, needs_fitting, component_obj)
+
+    def fit(self, X, y, objective_fit_size=.2):
+        pass
+
+    def fit_transform(self, X, y, objective_fit_size=.2):
+        pass
