2160ba3d62c329cbe0736a5191eeb4d52cdddcd0
christopherbunn <chris.l.bunn@gmail.com>
2019-09-25 12:48:16 -0400

    Added new metrics to standard_metrics init

--- evalml/objectives/__init__.py
+++ evalml/objectives/__init__.py
@@ -2,8 +2,10 @@
 from .fraud_cost import FraudCost
 from .lead_scoring import LeadScoring
 from .standard_metrics import (
-    F1, F1Micro, F1Macro, F1Weighted, Precision, PrecisionMicro, PrecisionMacro, PrecisionWeighted, Recall, RecallMicro, RecallMacro, RecallWeighted,
-    AUC, AUCMicro, AUCMacro, AUCWeighted, LogLoss, MCC, R2
+    F1, F1Micro, F1Macro, F1Weighted, Precision, PrecisionMicro, PrecisionMacro,
+    PrecisionWeighted, Recall, RecallMicro, RecallMacro, RecallWeighted,
+    AUC, AUCMicro, AUCMacro, AUCWeighted, LogLoss, MCC, R2, MAE, MSE, MSLE,
+    MedianAE, MaxError, ExpVariance
     )
 from .objective_base import ObjectiveBase
 from .utils import get_objective, get_objectives
