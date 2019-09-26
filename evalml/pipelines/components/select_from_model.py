7b41aeb11eee7ced882a828bc51262511bd07eae
Jeremy Shih <jeremyliweishih@gmail.com>
2019-09-26 11:51:50 -0400

    Add selectfrommodel

--- evalml/pipelines/components/select_from_model.py
+++ evalml/pipelines/components/select_from_model.py
@@ -1,22 +1,25 @@
 from transformer import Transformer
 
 from sklearn.feature_selection import SelectFromModel
+from skopt.space import Integer, Real
 
+# TODO: AutoML must know to pass in the same estimator
 class SelectFromModel(Transformer):
-    def __init__(self):
+    def __init__(self, estimator, number_features, percent_features, threshold):
         name = 'Select From Model'
         component_type = 'feature_selection'
-        potential_parameters = None
+        hyper_parameters = {
+            "percent_features": Real(.01, 1)
+            "threshold": ['mean', -np.inf]
+        }
 
         feature_selection = SelectFromModel(
             estimator=estimator,
             max_features=max(1, int(percent_features * number_features)),
-            threshold=-np.inf
+            threshold=threshold
         )
-        if hyperparameters:
-            scaler = StandardScaler()
 
-        super().__init__(name=name, component_type=component_type, hyper_parameters=hyper_parameters, needs_fitting=True, component_obj=encoder)
+        super().__init__(name=name, component_type=component_type, hyper_parameters=hyper_parameters, needs_fitting=True, component_obj=feature_selection)
 
     def fit(self, X, objective_fit_size=.2):
         self.component_obj.fit(X)
--- evalml/pipelines/components/standard_scaler.py
+++ evalml/pipelines/components/standard_scaler.py
@@ -9,7 +9,7 @@ class StandardScaler(Transformer):
         hyper_parameters = None
 
         scaler = StandardScaler()
-        super().__init__(name=name, component_type=component_type, hyper_parameters=hyper_parameters, needs_fitting=True, component_obj=encoder)
+        super().__init__(name=name, component_type=component_type, hyper_parameters=hyper_parameters, needs_fitting=True, component_obj=scaler)
 
     def fit(self, X, objective_fit_size=.2):
         self.component_obj.fit(X)
