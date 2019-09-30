d7dff8c920ec1ffb4e699313060fc61aab506fe6
Jeremy Shih <jeremyliweishih@gmail.com>
2019-09-30 16:59:04 -0400

    Changed to lower-case

--- evalml/problem_types/problem_types.py
+++ evalml/problem_types/problem_types.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class ProblemTypes(Enum):
     """Enum for type of machine learning problem: BINARY, MULTICLASS, or REGRESSION"""
-    BINARY = 'BINARY'
-    MULTICLASS = 'MULTICLASS'
-    REGRESSION = 'REGRESSION'
+    BINARY = 'binary'
+    MULTICLASS = 'multiclass'
+    REGRESSION = 'regression'
