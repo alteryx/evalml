ca0ed03b6fa7c5b4adc4d011f5df21d8ad4cdd20
Jeremy Shih <jeremyliweishih@gmail.com>
2019-10-07 16:09:18 -0400

    Added RF Regression

--- evalml/pipelines/components/__init__.py
+++ evalml/pipelines/components/__init__.py
@@ -1,14 +1,16 @@
 # flake8:noqa
 from .component_base import ComponentBase
 from .component_types import ComponentTypes
-from .transformer import Transformer
+from .estimator import (
+    Estimator,
+    LinearRegressor,
+    LogisticRegressionClassifier,
+    RandomForestClassifier,
+    RandomForestRegressor,
+    XGBoostClassifier
+)
 from .onehot_encoder import OneHotEncoder
 from .select_from_model import SelectFromModel
-from .standard_scaler import StandardScaler
 from .simple_imputer import SimpleImputer
-from .estimator import (Estimator,
-                        LogisticRegressionClassifier,
-                        RandomForestClassifier,
-                        XGBoostClassifier,
-                        RandomForestRegressor,
-                        LinearRegressor)
+from .standard_scaler import StandardScaler
+from .transformer import Transformer
