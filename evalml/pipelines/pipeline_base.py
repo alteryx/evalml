2a0adce12f8a97e3fa7454dc4bc67fc8e5892a3d
Jeremy Shih <jeremyliweishih@gmail.com>
2019-10-07 15:24:46 -0400

    Fixed SelectFromModel

--- evalml/pipelines/pipeline_base.py
+++ evalml/pipelines/pipeline_base.py
@@ -68,9 +68,7 @@ class Pipeline:
         X_t = X
         y_t = y
         for component in self.component_list[:-1]:
-            print(component)
             X_t = component.fit_transform(X_t, y_t)
-            print(X_t)
         self.component_list[-1].fit(X_t, y_t)
 
     def _transform(self, X):
