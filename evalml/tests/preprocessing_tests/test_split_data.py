31f638b95b1bbf74307cc1b7f58da731fda94e88
christopherbunn <chris.l.bunn@gmail.com>
2019-10-07 17:18:43 -0400

    Updated tests to use new split method

--- evalml/tests/preprocessing_tests/test_split_data.py
+++ evalml/tests/preprocessing_tests/test_split_data.py
@@ -7,7 +7,7 @@ def test_split_regression(X_y_reg):
     X, y = X_y_reg
     X = pd.DataFrame(X)
     y = pd.Series(y)
-    X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.25)
+    X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.25, regression=True)
     assert len(X_train) == 75
     assert len(X_test) == 25
     assert len(y_train) == 75
