f824f0595a8f363c7811800fa7d5597f5d84dae8
christopherbunn <chris.l.bunn@gmail.com>
2019-10-08 12:26:26 -0400

    Removed hardcoded test values

--- evalml/tests/preprocessing_tests/test_split_data.py
+++ evalml/tests/preprocessing_tests/test_split_data.py
@@ -7,19 +7,25 @@ def test_split_regression(X_y_reg):
     X, y = X_y_reg
     X = pd.DataFrame(X)
     y = pd.Series(y)
-    X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.25, regression=True)
-    assert len(X_train) == 75
-    assert len(X_test) == 25
-    assert len(y_train) == 75
-    assert len(y_test) == 25
+    test_pct = 0.25
+    X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_pct, regression=True)
+    test_size = len(X) * test_pct
+    train_size = len(X) - test_size
+    assert len(X_train) == train_size
+    assert len(X_test) == test_size
+    assert len(y_train) == train_size
+    assert len(y_test) == test_size
 
 
 def test_split_classification(X_y):
     X, y = X_y
     X = pd.DataFrame(X)
     y = pd.Series(y)
-    X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.25)
-    assert len(X_train) == 75
-    assert len(X_test) == 25
-    assert len(y_train) == 75
-    assert len(y_test) == 25
+    test_pct = 0.25
+    X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_pct)
+    test_size = len(X) * 0.25
+    train_size = len(X) - test_size
+    assert len(X_train) == train_size
+    assert len(X_test) == test_size
+    assert len(y_train) == train_size
+    assert len(y_test) == test_size
