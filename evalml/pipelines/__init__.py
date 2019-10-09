640a572fb139d0a5d761087c00a28c5d6f3eaa20
Jeremy Shih <jeremyliweishih@gmail.com>
2019-10-09 11:45:53 -0400

    Lint

--- evalml/pipelines/__init__.py
+++ evalml/pipelines/__init__.py
@@ -1,6 +1,14 @@
 # flake8:noqa
+from .classification import (
+    LogisticRegressionPipeline,
+    RFClassificationPipeline,
+    XGBoostPipeline
+)
 from .pipeline_base import PipelineBase
-from .classification import RFClassificationPipeline, XGBoostPipeline, LogisticRegressionPipeline
 from .regression import RFRegressionPipeline
-
-from .utils import get_pipelines, list_model_types, save_pipeline, load_pipeline
+from .utils import (
+    get_pipelines,
+    list_model_types,
+    load_pipeline,
+    save_pipeline
+)
