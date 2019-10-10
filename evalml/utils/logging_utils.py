c671859ee9a83c723029edc9d5194bcc7b186088
Angela Lin <angela97lin@gmail.com>
2019-10-10 17:19:28 -0400

    moving logging into its own util module; (#119)

---
+++ evalml/utils/logging_utils.py
@@ -0,0 +1,28 @@
+from colorama import Style
+
+
+class Logger:
+    def __init__(self, verbose=True):
+        self.verbose = True
+
+    def log(self, msg, color=None, new_line=True):
+        if not self.verbose:
+            return
+
+        if color:
+            msg = color + msg + Style.RESET_ALL
+
+        if new_line:
+            print(msg)
+        else:
+            print(msg, end="")
+
+    def log_title(self, title):
+        self.log("*" * (len(title) + 4), color=Style.BRIGHT)
+        self.log("* %s *" % title, color=Style.BRIGHT)
+        self.log("*" * (len(title) + 4), color=Style.BRIGHT)
+        self.log("")
+
+    def log_subtitle(self, title, underline="=", color=None):
+        self.log("%s" % title, color=color)
+        self.log(underline * len(title), color=color)
