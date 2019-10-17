73f559de9a8658a713e8d3a6bb0d70a3b36eb51c
Jeremy Shih <jeremyliweishih@gmail.com>
2019-10-17 11:09:40 -0400

    Remove extlinks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -195,11 +195,4 @@ templates_path = ["_templates"]
 def setup(app):
     app.add_stylesheet("style.css")
 
-
-extlinks = {
-    'issue': ('https://github.com/featurelabs/evalml/issues/%s', 'GH#'),
-    'pr': ('https://github.com/featurelabs/evalml/pull/%s', 'GH#'),
-    'user': ('https://github.com/%s', '@')
-}
-
 html_show_sphinx = False
