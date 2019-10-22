5c1ae8c43f594878db5507539a055bf02fae06ae
Angela Lin <angela97lin@gmail.com>
2019-10-22 12:50:06 -0400

    fixing commented out + flake8

--- evalml/tests/pipeline_tests/test_pipelines.py
+++ evalml/tests/pipeline_tests/test_pipelines.py
@@ -1,6 +1,6 @@
 import os
 
-# import pandas as pd
+import pandas as pd
 import pytest
 
 from evalml.model_types import ModelTypes
@@ -15,28 +15,28 @@ from evalml.pipelines.utils import (
 from evalml.problem_types import ProblemTypes
 
 
-# def test_list_model_types():
-#     assert set(list_model_types(ProblemTypes.BINARY)) == set([ModelTypes.RANDOM_FOREST, ModelTypes.XGBOOST, ModelTypes.LINEAR_MODEL])
-#     assert set(list_model_types(ProblemTypes.REGRESSION)) == set([ModelTypes.RANDOM_FOREST])
+def test_list_model_types():
+    assert set(list_model_types(ProblemTypes.BINARY)) == set([ModelTypes.RANDOM_FOREST, ModelTypes.XGBOOST, ModelTypes.LINEAR_MODEL])
+    assert set(list_model_types(ProblemTypes.REGRESSION)) == set([ModelTypes.RANDOM_FOREST])
 
 
-# def test_get_pipelines():
-#     assert len(get_pipelines(problem_type=ProblemTypes.BINARY)) == 3
-#     assert len(get_pipelines(problem_type=ProblemTypes.BINARY, model_types=[ModelTypes.LINEAR_MODEL])) == 1
-#     assert len(get_pipelines(problem_type=ProblemTypes.REGRESSION)) == 1
-#     with pytest.raises(RuntimeError, match="Unrecognized model type for problem type"):
-#         get_pipelines(problem_type=ProblemTypes.REGRESSION, model_types=["random_forest", "xgboost"])
+def test_get_pipelines():
+    assert len(get_pipelines(problem_type=ProblemTypes.BINARY)) == 3
+    assert len(get_pipelines(problem_type=ProblemTypes.BINARY, model_types=[ModelTypes.LINEAR_MODEL])) == 1
+    assert len(get_pipelines(problem_type=ProblemTypes.REGRESSION)) == 1
+    with pytest.raises(RuntimeError, match="Unrecognized model type for problem type"):
+        get_pipelines(problem_type=ProblemTypes.REGRESSION, model_types=["random_forest", "xgboost"])
 
 
-# def test_serialization(X_y, tmpdir):
-#     X, y = X_y
-#     path = os.path.join(str(tmpdir), 'pipe.pkl')
-#     objective = Precision()
+def test_serialization(X_y, tmpdir):
+    X, y = X_y
+    path = os.path.join(str(tmpdir), 'pipe.pkl')
+    objective = Precision()
 
-#     pipeline = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]))
-#     pipeline.fit(X, y)
-#     save_pipeline(pipeline, path)
-#     assert pipeline.score(X, y) == load_pipeline(path).score(X, y)
+    pipeline = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]))
+    pipeline.fit(X, y)
+    save_pipeline(pipeline, path)
+    assert pipeline.score(X, y) == load_pipeline(path).score(X, y)
 
 
 @pytest.fixture
@@ -55,28 +55,28 @@ def test_load_pickled_pipeline_with_custom_objective(X_y, pickled_pipeline_path)
     X, y = X_y
     # checks that class is not defined before loading in pipeline
     with pytest.raises(NameError):
-        MockPrecision()
+        MockPrecision()  # noqa: F821: ignore flake8's "undefined name" error
     objective = Precision()
     pipeline = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]))
     pipeline.fit(X, y)
     assert load_pipeline(pickled_pipeline_path).score(X, y) == pipeline.score(X, y)
 
 
-# def test_reproducibility(X_y):
-#     X, y = X_y
-#     X = pd.DataFrame(X)
+def test_reproducibility(X_y):
+    X, y = X_y
+    X = pd.DataFrame(X)
 
-#     objective = FraudCost(
-#         retry_percentage=.5,
-#         interchange_fee=.02,
-#         fraud_payout_percentage=.75,
-#         amount_col=10
-#     )
+    objective = FraudCost(
+        retry_percentage=.5,
+        interchange_fee=.02,
+        fraud_payout_percentage=.75,
+        amount_col=10
+    )
 
-#     clf = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]), random_state=0)
-#     clf.fit(X, y)
+    clf = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]), random_state=0)
+    clf.fit(X, y)
 
-#     clf_1 = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]), random_state=0)
-#     clf_1.fit(X, y)
+    clf_1 = LogisticRegressionPipeline(objective=objective, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]), random_state=0)
+    clf_1.fit(X, y)
 
-#     assert clf_1.score(X, y) == clf.score(X, y)
+    assert clf_1.score(X, y) == clf.score(X, y)
