2ac3b4cfbf6241958c3204bba16526be518ffa95
christopherbunn <chris.l.bunn@gmail.com>
2019-11-05 10:05:26 -0500

    Fixed lint and test errors

--- evalml/tests/pipeline_tests/test_pipelines.py
+++ evalml/tests/pipeline_tests/test_pipelines.py
@@ -169,6 +169,6 @@ def test_multiple_feature_selectors(X_y):
 
 def test_feature_importance_plot(X_y):
     X, y = X_y
-    clf = LogisticRegressionPipeline(objective=Precision, penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]), random_state=0)
+    clf = LogisticRegressionPipeline(objective='precision', penalty='l2', C=1.0, impute_strategy='mean', number_features=len(X[0]), random_state=0)
     clf.fit(X, y)
     assert type(clf.plot_feature_importance()) is matplotlib.figure.Figure
