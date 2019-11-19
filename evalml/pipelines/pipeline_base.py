3f5991d0cdc0db0a33aeb0520b69f4b979a60764
Angela Lin <angela97lin@gmail.com>
2019-11-19 14:45:37 -0500

    cleanup

--- evalml/pipelines/pipeline_base.py
+++ evalml/pipelines/pipeline_base.py
@@ -4,10 +4,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from .components import Estimator, handle_component
+from .pipeline_plots import PipelinePlots
 
 from evalml.objectives import get_objective
 from evalml.utils import Logger
-from .pipeline_plots import PipelinePlots
+
 
 class PipelineBase:
 
@@ -74,77 +75,6 @@ class PipelineBase:
                 name += " + {}".format(component.name)
 
         return name
-
-    # def visualize(self, to_file=None):
-        # """
-        # Create a UML diagram-ish graph of our pipeline.
-        # Args:
-        #     to_file (str, optional) : Path to where the plot should be saved.
-        #         If set to None (as by default), the plot will not be saved.
-        # Returns:
-        #     graphviz.Digraph : Graph object that can directly be displayed in
-        #         Jupyter notebooks.
-        # """
-        # try:
-        #     import graphviz
-        # except ImportError:
-        #     raise ImportError('Please install graphviz to visualize pipelines.')
-
-        # # Try rendering a dummy graph to see if a working backend is installed
-        # try:
-        #     graphviz.Digraph().pipe()
-        # except graphviz.backend.ExecutableNotFound:
-        #     raise RuntimeError(
-        #         "To plot entity sets, a graphviz backend is required.\n" +
-        #         "Install the backend using one of the following commands:\n" +
-        #         "  Mac OS: brew install graphviz\n" +
-        #         "  Linux (Ubuntu): sudo apt-get install graphviz\n" +
-        #         "  Windows: conda install python-graphviz\n"
-        #     )
-
-        # if to_file:
-        #     # Explicitly cast to str in case a Path object was passed in
-        #     to_file = str(to_file)
-        #     split_path = to_file.split('.')
-        #     if len(split_path) < 2:
-        #         raise ValueError("Please use a file extension like '.pdf'" +
-        #                          " so that the format can be inferred")
-
-        #     format = split_path[-1]
-        #     valid_formats = graphviz.backend.FORMATS
-        #     if format not in valid_formats:
-        #         raise ValueError("Unknown format. Make sure your format is" +
-        #                          " amongst the following: %s" % valid_formats)
-        # else:
-        #     format = None
-
-        # # Initialize a new directed graph
-        # graph = graphviz.Digraph(name=self.name, format=format,
-        #                          graph_attr={'splines': 'ortho'})
-        # graph.attr(rankdir='LR')
-
-        # # Draw components
-        # for component in self.component_list:
-        #     label = '%s\l' % (component.name)  # noqa: W605
-        #     if len(component.parameters) > 0:
-        #         parameters = '\l'.join([key + ' : ' + "{:0.2f}".format(val) if (isinstance(val, float))
-        #                                 else key + ' : ' + str(val)
-        #                                 for key, val in component.parameters.items()])  # noqa: W605
-        #         label = '%s |%s\l' % (component.name, parameters)  # noqa: W605
-        #     graph.node(component.name, shape='record', label=label)
-
-        # # Draw edges
-        # for i in range(len(self.component_list[:-1])):
-        #     graph.edge(self.component_list[i].name, self.component_list[i + 1].name)
-
-        # if to_file:
-        #     # Graphviz always appends the format to the file name, so we need to
-        #     # remove it manually to avoid file names like 'file_name.pdf.pdf'
-        #     offset = len(format) + 1  # Add 1 for the dot
-        #     output_path = to_file[:-offset]
-        #     graph.render(output_path, cleanup=True)
-
-        # return graph
 
     def get_component(self, name):
         return next((component for component in self.component_list if component.name == name), None)
