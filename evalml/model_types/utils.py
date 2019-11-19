1526d85018af1082634aefc8034bc3fb2d28ff4a
Angela Lin <angela97lin@gmail.com>
2019-11-19 13:20:04 -0500

    revert change

--- evalml/model_types/utils.py
+++ evalml/model_types/utils.py
@@ -1,14 +1,14 @@
-from .model_types import ModelType
+from .model_types import ModelTypes
 
 
 def handle_model_types(model_type):
     """Handles model_type by either returning the ModelTypes or converting from a str
 
     Args:
-        model_type (str or ModelType) : model type that needs to be handled
+        model_type (str or ModelTypes) : model type that needs to be handled
 
     Returns:
-        ModelType
+        ModelTypes
     """
 
     if isinstance(model_type, str):
@@ -17,6 +17,6 @@ def handle_model_types(model_type):
         except KeyError:
             raise KeyError('Model type \'{}\' does not exist'.format(model_type))
         return tpe
-    if isinstance(model_type, ModelType):
+    if isinstance(model_type, ModelTypes):
         return model_type
-    raise ValueError('`handle_model_types` was not passed a str or ModelType object')
+    raise ValueError('`handle_model_types` was not passed a str or ModelTypes object')
