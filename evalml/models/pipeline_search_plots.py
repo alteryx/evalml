9b306f7ec86614348dedae4a4faaa9410f59d00c
Angela Lin <angela97lin@gmail.com>
2019-11-21 18:20:28 -0500

    more testing for docs

--- evalml/models/pipeline_search_plots.py
+++ evalml/models/pipeline_search_plots.py
@@ -103,4 +103,4 @@ class PipelineSearchPlots:
                                line=dict(dash='dash')))
 
         figure = go.Figure(layout=layout, data=data)
-        return figure
+        return go.FigureWidget(figure)
