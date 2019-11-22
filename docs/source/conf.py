eefdbccea8b4bdc7be10c9aa4162e6a5a0314e70
Angela Lin <angela97lin@gmail.com>
2019-11-22 17:12:23 -0500

    update, rereun tests?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,14 +12,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+import evalml
 import os
 import sys
+import sphinx_rtd_theme
+import subprocess
 from sphinx.util import rpartition
 from sphinx.ext.autodoc import (Documenter, MethodDocumenter)
-import evalml
-import sphinx_rtd_theme
 
-import subprocess
+
 path = os.path.join('..', '..')
 sys.path.insert(0, os.path.abspath(path))
 
