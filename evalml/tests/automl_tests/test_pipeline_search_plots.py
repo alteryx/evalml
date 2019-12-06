33021166bf6a5afe384bb8088b63913ea0e4a498
christopherbunn <chris.l.bunn@gmail.com>
2019-12-06 16:22:39 -0500

    Fixed other lint error

--- evalml/tests/automl_tests/test_pipeline_search_plots.py
+++ evalml/tests/automl_tests/test_pipeline_search_plots.py
@@ -5,8 +5,8 @@ import plotly.graph_objects as go
 import pytest
 from sklearn.model_selection import StratifiedKFold
 
-from evalml.models.auto_base import AutoBase
 from evalml.models import AutoClassifier
+from evalml.models.auto_base import AutoBase
 from evalml.models.pipeline_search_plots import PipelineSearchPlots
 from evalml.pipelines import LogisticRegressionPipeline
 from evalml.problem_types import ProblemTypes
