2b3642490aee684fef3a5714ee8912e399d03cd0
Angela Lin <angela97lin@gmail.com>
2019-12-12 14:32:02 -0500

    removing catboost feature selectors

--- evalml/pipelines/components/transformers/feature_selection/__init__.py
+++ evalml/pipelines/components/transformers/feature_selection/__init__.py
@@ -2,4 +2,3 @@
 from .feature_selector import FeatureSelector
 from .rf_classifier_feature_selector import RFClassifierSelectFromModel
 from .rf_regressor_feature_selector import RFRegressorSelectFromModel
-from .catboost_classifier_feature_selector import CatBoostClassifierSelectFromModel
