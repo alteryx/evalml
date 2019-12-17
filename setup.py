6970bfcfe41d4e6d86e4383b86a26b1d29725750
Angela Lin <angela97lin@gmail.com>
2019-12-17 16:18:58 -0500

    v0.6.0 (#288)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='evalml',
-    version='0.5.2',
+    version='0.6.0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
     url='http://www.featurelabs.com/',
