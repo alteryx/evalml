4f5d448d65b6eab89bccbd494d9a05f525a58ec4
christopherbunn <chris.l.bunn@gmail.com>
2020-02-07 10:58:49 -0500

    Updated docstring, renamed n_points, and improved _grid_points access

--- evalml/tuners/grid_search_tuner.py
+++ evalml/tuners/grid_search_tuner.py
@@ -8,13 +8,18 @@ from .tuner_exceptions import NoParamsException
 class GridSearchTuner:
     """Grid Search Optimizer"""
 
-    def __init__(self, space, points=10, random_state=None):
+    def __init__(self, space, n_points=10, random_state=None):
         """ Generate all of the possible points to search for in the grid
 
         Arguments:
-            points: The number of points to uniformly sample from \
+            n_points: The number of points to uniformly sample from \
                 Real dimensions.
-            random_state: Not used in grid search, kept for compatibility
+            space: A list of all dimensions available to tune
+            random_state: Unused in this class
+
+        Example:
+            >>> from skopt.space import Real
+            >>> GridSearchTuner([Real(1,10)], n_points=5)
         """
         raw_dimensions = list()
         for dimension in space:
@@ -24,23 +29,21 @@ class GridSearchTuner:
             elif isinstance(dimension, Real) or isinstance(dimension, Integer):
                 low = dimension.low
                 high = dimension.high
-                delta = (high - low) / (points - 1)
-
+                delta = (high - low) / (n_points - 1)
                 if isinstance(dimension, Integer):
-                    range_values = [int((x * delta) + low) for x in range(points)]
+                    range_values = [int((x * delta) + low) for x in range(n_points)]
                 else:
-                    range_values = [(x * delta) + low for x in range(points)]
+                    range_values = [(x * delta) + low for x in range(n_points)]
             else:
                 return Exception("Invalid dimension type in tuner")
             raw_dimensions.append(range_values)
-        self.grid_points = list(itertools.product(*raw_dimensions))
+        self._grid_points = itertools.product(*raw_dimensions)
 
     def add(self, parameters, score):
-        # Since this is a grid search, we don't need to store the results.
         pass
 
     def propose(self):
-        if len(self.grid_points) > 0:
-            return self.grid_points.pop()
-        else:
+        try:
+            return next(self._grid_points)
+        except StopIteration:
             raise NoParamsException("Grid search has exhausted all possible parameters.")
