6d139f6344e6ac2ac3ee68a17ae4a47c20877e96
Jeremy Shih <jeremyliweishih@gmail.com>
2020-02-19 18:11:46 -0500

    Fit then transform

--- evalml/pipelines/pipeline_base.py
+++ evalml/pipelines/pipeline_base.py
@@ -131,9 +131,10 @@ class PipelineBase:
         for component in self.component_list[:-1]:
             self.input_feature_names.update({component.name: list(pd.DataFrame(X_t))})
             try:
-                X_t = component.fit_transform(X_t, y_t)
+                component.fit(X_t, y_t)
             except RuntimeError:
-                X_t = component.transform(X_t, y_t)
+                pass
+            X_t = component.transform(X_t, y_t)
 
         self.input_feature_names.update({self.estimator.name: list(pd.DataFrame(X_t))})
         self.estimator.fit(X_t, y_t)
