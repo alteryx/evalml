fb22edbbed5860095f5cccc2222ccec461126381
Jeremy Shih <jeremyliweishih@gmail.com>
2020-02-26 12:11:13 -0500

    Fix misc tests

--- evalml/tests/objective_tests/test_objectives.py
+++ evalml/tests/objective_tests/test_objectives.py
@@ -23,7 +23,18 @@ def test_get_objectives_types():
 def test_binary_average(X_y):
     X, y = X_y
 
-    pipeline = LogisticRegressionPipeline(objective=Precision(), penalty='l2', C=1.0, impute_strategy='mean', number_features=0)
+    objective = Precision()
+    parameters = {
+        'Simple Imputer': {
+            'impute_strategy': 'mean'
+        },
+        'Logistic Regression Classifier': {
+            'penalty': 'l2',
+            'C': 1.0,
+        }
+    }
+
+    pipeline = LogisticRegressionPipeline(objective=objective, parameters=parameters)
     pipeline.fit(X, y)
     y_pred = pipeline.predict(X)
 
