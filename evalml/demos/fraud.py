67688bd7aa1d59607c6a321cff29587046eae1fb
ange <ange@mit.edu>
2020-03-04 00:45:09 -0500

    test pkg

--- evalml/demos/fraud.py
+++ evalml/demos/fraud.py
@@ -1,6 +1,7 @@
 import os
 
 from evalml.preprocessing import load_data
+import pkg_resources
 
 
 def load_fraud(n_rows=None):
@@ -16,7 +17,8 @@ def load_fraud(n_rows=None):
     dir_path = os.path.dirname(os.path.abspath(__file__))
     # data_path = os.path.join(dir_path, "data")
     fraud_path = os.path.join(dir_path, "fraud_transactions.csv.tar.gz")
-    X, y = load_data(path=fraud_path,
+    
+    X, y = load_data(path=pkg_resources.resource_filename("evalml", "fraud_transactions.csv.tar.gz"),
                      index="id",
                      label="fraud",
                      n_rows=n_rows)
