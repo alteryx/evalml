6daaa274090f427be31bbc9f52b2910cd0b2c2c7
ange <ange@mit.edu>
2020-03-04 01:52:46 -0500

    revert data path

--- evalml/demos/fraud.py
+++ evalml/demos/fraud.py
@@ -15,8 +15,8 @@ def load_fraud(n_rows=None):
         pd.DataFrame, pd.Series: X, y
     """
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    # data_path = os.path.join(dir_path, "data")
-    fraud_path = os.path.join(dir_path, "fraud_transactions.csv.tar.gz")
+    data_path = os.path.join(dir_path, "data")
+    fraud_path = os.path.join(data_path, "fraud_transactions.csv.tar.gz")
     
     X, y = load_data(path=fraud_path,
                      index="id",
