91d62ec2c7a90baa88bc58072e6a798336135022
Dylan Sherry <sharshofski@gmail.com>
2020-03-06 16:12:51 -0500

    Clean up pipeline graphing code (#423)
    * Rename PipelinePlots to PipelineGraph
    
    * Replace class with methods
    
    * Update unit tests
    
    * Update docs
    
    * Lint
    
    * Define 'make_feature_importance_graph'
    
    * Renaming.
    
    * Rename test file
    
    * Changelog
    
    * Simplified path logic
    
    * Lint
    
    * Make graph helper functions not require pipeline
    
    * Fix test
    
    * Fix another test
    
    * Protect against nonexistant paths

--- evalml/tests/pipeline_tests/test_graphs.py
+++ evalml/tests/pipeline_tests/test_graphs.py
@@ -11,36 +11,43 @@ from evalml.pipelines import PipelineBase
 
 def test_returns_digraph_object():
     clf = PipelineBase('precision', component_list=['Simple Imputer', 'One Hot Encoder', 'Standard Scaler', 'Logistic Regression Classifier'], n_jobs=-1, random_state=0)
-    plot = clf.plot()
-    assert isinstance(plot, graphviz.Digraph)
+    graph = clf.graph()
+    assert isinstance(graph, graphviz.Digraph)
 
 
 def test_saving_png_file(tmpdir):
-    path = os.path.join(str(tmpdir), 'pipeline.png')
+    filepath = os.path.join(str(tmpdir), 'pipeline.png')
     pipeline = PipelineBase('precision', component_list=['Simple Imputer', 'One Hot Encoder', 'Standard Scaler', 'Logistic Regression Classifier'], n_jobs=-1, random_state=0)
-    pipeline.plot(to_file=path)
-    assert os.path.isfile(path)
+    pipeline.graph(filepath=filepath)
+    assert os.path.isfile(filepath)
 
 
 def test_missing_file_extension():
-    path = "test1"
+    filepath = "test1"
     pipeline = PipelineBase('precision', component_list=['Simple Imputer', 'One Hot Encoder', 'Standard Scaler', 'Logistic Regression Classifier'], n_jobs=-1, random_state=0)
-    with pytest.raises(ValueError, match="Please use a file extension"):
-        pipeline.plot(to_file=path)
+    with pytest.raises(ValueError, match="Unknown format"):
+        pipeline.graph(filepath=filepath)
 
 
 def test_invalid_format():
-    path = "test1.xzy"
+    filepath = "test1.xzy"
     pipeline = PipelineBase('precision', component_list=['Simple Imputer', 'One Hot Encoder', 'Standard Scaler', 'Logistic Regression Classifier'], n_jobs=-1, random_state=0)
     with pytest.raises(ValueError, match="Unknown format"):
-        pipeline.plot(to_file=path)
+        pipeline.graph(filepath=filepath)
+
+
+def test_invalid_path(tmpdir):
+    filepath = os.path.join(str(tmpdir), 'invalid', 'path', 'pipeline.png')
+    pipeline = PipelineBase('precision', component_list=['Simple Imputer', 'One Hot Encoder', 'Standard Scaler', 'Logistic Regression Classifier'], n_jobs=-1, random_state=0)
+    with pytest.raises(ValueError, match="Specified parent directory does not exist"):
+        pipeline.graph(filepath=filepath)
 
 
 def test_feature_importance_plot(X_y):
     X, y = X_y
     clf = PipelineBase('precision', component_list=['Simple Imputer', 'One Hot Encoder', 'Standard Scaler', 'Logistic Regression Classifier'], n_jobs=-1, random_state=0)
     clf.fit(X, y)
-    assert isinstance(clf.plot.feature_importances(), go.Figure)
+    assert isinstance(clf.feature_importance_graph(), go.Figure)
 
 
 def test_feature_importance_plot_show_all_features(X_y):
@@ -66,12 +73,12 @@ def test_feature_importance_plot_show_all_features(X_y):
     X, y = X_y
     clf = MockPipeline()
     clf.fit(X, y)
-    figure = clf.plot.feature_importances()
+    figure = clf.feature_importance_graph()
     assert isinstance(figure, go.Figure)
 
     data = figure.data[0]
     assert (np.all(data['x']))
 
-    figure = clf.plot.feature_importances(show_all_features=True)
+    figure = clf.feature_importance_graph(show_all_features=True)
     data = figure.data[0]
     assert (np.any(data['x'] == 0.0))
