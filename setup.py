f12350e6d4cb6dca2af7f17ff744be31cdd721a5
Dylan Sherry <sharshofski@gmail.com>
2020-03-06 00:13:37 -0500

    Move xgboost/catboost to extras

--- setup.py
+++ setup.py
@@ -11,4 +11,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     data_files=[('evalml/demos/data', ['evalml/demos/data/fraud_transactions.csv.tar.gz'])]
+    extras_require={
+        'complete': ['xgboost>=0.82,<1.0.0', 'catboost>=0.20']
+    }
 )
