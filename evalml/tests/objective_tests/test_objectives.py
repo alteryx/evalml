ea48f89b7866d3b3fbf92893f14f91faf754202c
Angela Lin <angela97lin@gmail.com>
2020-03-09 19:17:03 -0400

    removing irrelevant test

--- evalml/tests/objective_tests/test_objectives.py
+++ evalml/tests/objective_tests/test_objectives.py
@@ -41,14 +41,3 @@ def test_get_objectives_types():
     assert len(get_objectives(ProblemTypes.MULTICLASS)) == 14
     assert len(get_objectives(ProblemTypes.BINARY)) == 6
     assert len(get_objectives(ProblemTypes.REGRESSION)) == 6
-
-
-# todo: use to test logic?
-# def test_binary_average(X_y):
-#     X, y = X_y
-#     objective = Precision()
-#     pipeline = LogisticRegressionBinaryPipeline(penalty='l2', C=1.0, impute_strategy='mean', number_features=0)
-#     pipeline.fit(X, y, objective)
-#     y_pred = pipeline.predict(X, objective=objective)
-#     # assert Precision().score(y, y_pred) == PrecisionMicro().score(y, y_pred)
-#     assert Precision().score(y, y_pred) == PrecisionMacro().score(y, y_pred)
