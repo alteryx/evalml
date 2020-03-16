a15d75a00d5eafcee73a54408d5b02e772c23388
Dylan Sherry <sharshofski@gmail.com>
2020-03-16 19:31:51 -0400

    Have unit test read stdout

--- evalml/tests/utils_tests/test_logger.py
+++ evalml/tests/utils_tests/test_logger.py
@@ -9,8 +9,20 @@ def test_logger_verbose():
     logger.verbose = True
     assert logger.verbose
 
-def test_logger_log():
+def test_logger_log(capsys):
     logger = Logger()
     logger.log('Test message')
+    out, err = capsys.readouterr()
+    assert out == 'Test message\n'
+    assert len(err) == 0
+
     logger.log_title('Log title')
+    out, err = capsys.readouterr()
+    assert 'Log title' in out
+    assert len(err) == 0
+
     logger.log_subtitle('Log subtitle')
+    out, err = capsys.readouterr()
+    assert 'Log subtitle' in out
+    assert len(err) == 0
+
