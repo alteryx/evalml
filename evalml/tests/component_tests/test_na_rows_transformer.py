e76734b5ae4fe089e27bd0a7bcc05cbb8da70046
Angela Lin <angela97lin@gmail.com>
2020-03-18 18:01:00 -0400

    adding test for numpy arrays

--- evalml/tests/component_tests/test_na_rows_transformer.py
+++ evalml/tests/component_tests/test_na_rows_transformer.py
@@ -12,7 +12,6 @@ def test_fit():
     X["c"] = [np.nan, 0, 0, 0]
     X["d"] = [0, 0, 0, 0]
 
-    # test not passing in y
     transformer = DropNaNRowsTransformer()
     X_t = transformer.fit_transform(X)
     assert len(X_t) == 3
@@ -20,9 +19,20 @@ def test_fit():
     X_t = transformer.transform(X)
     assert len(X_t) == 3
 
+    # test passing in y
     transformer = DropNaNRowsTransformer()
     X_t = transformer.fit_transform(X, y)
     assert len(X_t) == 2
 
     X_t = transformer.transform(X, y)
     assert len(X_t) == 2
+
+    # testing using numpy arrays
+    X_arr = np.array([[1, 2, 3, 0],
+                     [np.nan, 0, 0, 1],
+                     [np.nan, 0, np.nan, 0],
+                     [np.nan, 0, 0, 0]])
+    y_arr = y.values
+    transformer = DropNaNRowsTransformer()
+    X_t = transformer.fit_transform(X_arr, y_arr)
+    assert len(X_t) == 1
