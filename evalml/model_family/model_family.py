b6f67ebba89321bc70d0302bec89ed47f0b2b385
Jeremy Shih <jeremyliweishih@gmail.com>
2020-03-19 11:19:06 -0400

    Add none modelfamily

--- evalml/model_family/model_family.py
+++ evalml/model_family/model_family.py
@@ -7,10 +7,12 @@ class ModelFamily(Enum):
     XGBOOST = 'xgboost'
     LINEAR_MODEL = 'linear_model'
     CATBOOST = 'catboost'
+    NONE = 'none'
 
     def __str__(self):
         model_family_dict = {ModelFamily.RANDOM_FOREST.name: "Random Forest",
                              ModelFamily.XGBOOST.name: "XGBoost Classifier",
                              ModelFamily.LINEAR_MODEL.name: "Linear Model",
-                             ModelFamily.CATBOOST.name: "CatBoost Classifier"}
+                             ModelFamily.CATBOOST.name: "CatBoost Classifier",
+                             ModelFamily.NONE.name: "None"}
         return model_family_dict[self.name]
