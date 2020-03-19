6b16e51f6f6ac5f928d7a3d45e6c602b2dab9172
Jeremy Shih <jeremyliweishih@gmail.com>
2020-03-19 11:16:30 -0400

    Move return statement

--- evalml/model_family/utils.py
+++ evalml/model_family/utils.py
@@ -12,9 +12,9 @@ def handle_model_family(model_family):
     if isinstance(model_family, str):
         try:
             tpe = ModelFamily[model_family.upper()]
+            return tpe
         except KeyError:
             raise KeyError('Model family \'{}\' does not exist'.format(model_family))
-        return tpe
     if isinstance(model_family, ModelFamily):
         return model_family
     raise ValueError('`handle_model_family` was not passed a str or ModelFamily object')
