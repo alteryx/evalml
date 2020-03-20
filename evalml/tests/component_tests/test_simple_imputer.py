922d484f3a5a0809f819aba819f8671cd5072e32
Angela Lin <angela97lin@gmail.com>
2020-03-20 11:04:09 -0400

    cleanup test

--- evalml/tests/component_tests/test_simple_imputer.py
+++ evalml/tests/component_tests/test_simple_imputer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pytest
 from pandas.testing import assert_frame_equal
 
 from evalml.pipelines.components import SimpleImputer
@@ -30,9 +31,9 @@ def test_mean():
                       [1, 2, 3, 0]])
     # test impute_strategy
     transformer = SimpleImputer(impute_strategy='mean')
-    X_expected_arr = pd.DataFrame([[1.0, 0, 1, 1.0],
-                                   [1.0, 2, 3, 2.0],
-                                   [1.0, 2, 3, 0.0]])
+    X_expected_arr = pd.DataFrame([[1, 0, 1, 1],
+                                   [1, 2, 3, 2],
+                                   [1, 2, 3, 0]])
     X_t = transformer.fit_transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
@@ -47,9 +48,9 @@ def test_constant():
                       ["b", 2, 3, 0]])
 
     transformer = SimpleImputer(impute_strategy='constant', fill_value=3)
-    X_expected_arr = pd.DataFrame([[3, 0, 1.0, 3.0],
-                                   ["a", 2, 3.0, 3.0],
-                                   ["b", 2, 3.0, 0.0]])
+    X_expected_arr = pd.DataFrame([[3, 0, 1, 3],
+                                   ["a", 2, 3, 3],
+                                   ["b", 2, 3, 0]])
     X_t = transformer.fit_transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
@@ -60,12 +61,12 @@ def test_constant():
 def test_most_frequent():
     X = pd.DataFrame([[np.nan, 0, 1, np.nan],
                       ["a", 2, np.nan, 3],
-                      ["b", 2, 3, 0]])
+                      ["b", 2, 1, 0]])
 
     transformer = SimpleImputer(impute_strategy='most_frequent')
-    X_expected_arr = pd.DataFrame([["a", 0, 1.0, 0.0],
-                                   ["a", 2, 3.0, 3.0],
-                                   ["b", 2, 3.0, 0.0]])
+    X_expected_arr = pd.DataFrame([["a", 0, 1, 0],
+                                   ["a", 2, 1, 3],
+                                   ["b", 2, 1, 0]])
     X_t = transformer.fit_transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
@@ -73,41 +74,45 @@ def test_most_frequent():
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
 
-def test_col_with_all_nan():
-    # test that col with all NaN is removed
-    X = pd.DataFrame([[np.nan, 0, 1, np.nan],
-                      [np.nan, 2, 3, 3],
-                      [np.nan, 2, 3, 0],
+def test_col_with_non_numeric():
+    # test col with all strings
+    X = pd.DataFrame([["a", 0, 1, np.nan],
+                      ["b", 2, 3, 3],
+                      ["a", 2, 3, 1],
                       [np.nan, 2, 3, 0]])
-    # test impute_strategy
+
     transformer = SimpleImputer(impute_strategy='mean')
-    X_expected_arr = pd.DataFrame([[0, 1, 1],
-                                   [2, 3, 3],
-                                   [2, 3, 0],
-                                   [2, 3, 0]])
+    with pytest.raises(ValueError, match="Cannot use mean strategy with non-numeric data"):
+        transformer.fit_transform(X)
+    with pytest.raises(ValueError, match="Cannot use mean strategy with non-numeric data"):
+        transformer.fit(X)
+        transformer.transform(X)
+
+    transformer = SimpleImputer(impute_strategy='median')
+    with pytest.raises(ValueError, match="Cannot use median strategy with non-numeric data"):
+        transformer.fit_transform(X)
+    with pytest.raises(ValueError, match="Cannot use median strategy with non-numeric data"):
+        transformer.fit(X)
+        transformer.transform(X)
+
+    transformer = SimpleImputer(impute_strategy='most_frequent')
+    X_expected_arr = pd.DataFrame([["a", 0, 1, 0],
+                                   ["b", 2, 3, 3],
+                                   ["a", 2, 3, 1],
+                                   ["a", 2, 3, 0]])
     X_t = transformer.fit_transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
     X_t = transformer.transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
-    # test that names of columns are retained
-    col = pd.Series([np.nan, 0, 1, 1])
-    X = pd.DataFrame()
-    X["a"] = y
-    X["b"] = y - 1
-    X["c"] = y
-    X["d"] = y
-
-    # test impute_strategy
-    transformer = SimpleImputer(impute_strategy='mean')
-    X_expected_arr = pd.DataFrame([[0, 1, 1],
-                                   [0, 1, 1],
-                                   [0, 1, 1],
-                                   [0, 1, 1]])
+    transformer = SimpleImputer(impute_strategy='constant', fill_value=2)
+    X_expected_arr = pd.DataFrame([["a", 0, 1, 2],
+                                   ["b", 2, 3, 3],
+                                   ["a", 2, 3, 1],
+                                   [2, 2, 3, 0]])
     X_t = transformer.fit_transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
-    # check names of columns
 
     X_t = transformer.transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
