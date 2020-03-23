2d5a35fbc38396fd8e19b4ddbda24a4c820d409d
Angela Lin <angela97lin@gmail.com>
2020-03-23 15:23:15 -0400

    removing decision function for multiclass

--- evalml/objectives/multiclass_classification_objective.py
+++ evalml/objectives/multiclass_classification_objective.py
@@ -10,6 +10,3 @@ class MultiClassificationObjective(ClassificationObjective):
     problem_type (ProblemTypes): Type of problem this objective is. Set to ProblemTypes.MULTICLASS.
     """
     problem_type = ProblemTypes.MULTICLASS
-
-    def decision_function(self, ypred_proba, threshold=None, X=None):
-        raise NotImplementedError("decision_function for a multiclass classification objective is not yet defined!")
