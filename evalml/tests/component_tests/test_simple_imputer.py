334a00f15fe7fc5bb4817ce21d2f819945953fab
Dylan Sherry <sharshofski@gmail.com>
2020-03-26 04:40:13 -0400

    Update component unit tests

--- evalml/tests/component_tests/test_simple_imputer.py
+++ evalml/tests/component_tests/test_simple_imputer.py
@@ -12,7 +12,7 @@ def test_median():
                       [10, 2, np.nan, 2],
                       [10, 2, 5, np.nan],
                       [6, 2, 7, 0]])
-    transformer = SimpleImputer(impute_strategy='median')
+    transformer = SimpleImputer({'impute_strategy': 'median'})
     X_expected_arr = pd.DataFrame([[8, 0, 1, 2],
                                    [1, 2, 3, 2],
                                    [10, 2, 4, 2],
@@ -30,7 +30,7 @@ def test_mean():
                       [1, 2, 3, 2],
                       [1, 2, 3, 0]])
     # test impute_strategy
-    transformer = SimpleImputer(impute_strategy='mean')
+    transformer = SimpleImputer({'impute_strategy': 'mean'})
     X_expected_arr = pd.DataFrame([[1, 0, 1, 1],
                                    [1, 2, 3, 2],
                                    [1, 2, 3, 0]])
@@ -47,7 +47,7 @@ def test_constant():
                       ["a", 2, np.nan, 3],
                       ["b", 2, 3, 0]])
 
-    transformer = SimpleImputer(impute_strategy='constant', fill_value=3)
+    transformer = SimpleImputer({'impute_strategy': 'constant', 'fill_value': 3})
     X_expected_arr = pd.DataFrame([[3, 0, 1, 3],
                                    ["a", 2, 3, 3],
                                    ["b", 2, 3, 0]])
@@ -63,7 +63,7 @@ def test_most_frequent():
                       ["a", 2, np.nan, 3],
                       ["b", 2, 1, 0]])
 
-    transformer = SimpleImputer(impute_strategy='most_frequent')
+    transformer = SimpleImputer({'impute_strategy': 'most_frequent'})
     X_expected_arr = pd.DataFrame([["a", 0, 1, 0],
                                    ["a", 2, 1, 3],
                                    ["b", 2, 1, 0]])
@@ -81,19 +81,19 @@ def test_col_with_non_numeric():
                       ["a", 2, 3, 1],
                       [np.nan, 2, 3, 0]])
 
-    transformer = SimpleImputer(impute_strategy='mean')
+    transformer = SimpleImputer({'impute_strategy': 'mean'})
     with pytest.raises(ValueError, match="Cannot use mean strategy with non-numeric data"):
         transformer.fit_transform(X)
     with pytest.raises(ValueError, match="Cannot use mean strategy with non-numeric data"):
         transformer.fit(X)
 
-    transformer = SimpleImputer(impute_strategy='median')
+    transformer = SimpleImputer({'impute_strategy': 'median'})
     with pytest.raises(ValueError, match="Cannot use median strategy with non-numeric data"):
         transformer.fit_transform(X)
     with pytest.raises(ValueError, match="Cannot use median strategy with non-numeric data"):
         transformer.fit(X)
 
-    transformer = SimpleImputer(impute_strategy='most_frequent')
+    transformer = SimpleImputer({'impute_strategy': 'most_frequent'})
     X_expected_arr = pd.DataFrame([["a", 0, 1, 0],
                                    ["b", 2, 3, 3],
                                    ["a", 2, 3, 1],
@@ -104,7 +104,7 @@ def test_col_with_non_numeric():
     X_t = transformer.transform(X)
     assert_frame_equal(X_expected_arr, X_t, check_dtype=False)
 
-    transformer = SimpleImputer(impute_strategy='constant', fill_value=2)
+    transformer = SimpleImputer({'impute_strategy': 'constant', 'fill_value': 2})
     X_expected_arr = pd.DataFrame([["a", 0, 1, 2],
                                    ["b", 2, 3, 3],
                                    ["a", 2, 3, 1],
