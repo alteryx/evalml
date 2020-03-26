c506e9259649f4d87f3ae33ce92db40ac16fdb7d
Angela Lin <angela97lin@gmail.com>
2020-03-26 17:06:27 -0400

    use cls instead for pep8 standards

--- evalml/objectives/objective_base.py
+++ evalml/objectives/objective_base.py
@@ -11,34 +11,34 @@ class ObjectiveBase(ABC):
     @property
     @classmethod
     @abstractmethod
-    def name(self):
+    def name(cls):
         """Returns a name describing the objective."""
         raise NotImplementedError("This objective must have a `name` attribute as a class variable")
 
     @property
     @classmethod
     @abstractmethod
-    def problem_type(self):
+    def problem_type(cls):
         """Returns a ProblemTypes enum describing the problem type the objective can handle."""
         raise NotImplementedError("This objective must have a `problem_type` attribute as a class variable")
 
     @property
     @classmethod
     @abstractmethod
-    def greater_is_better(self):
+    def greater_is_better(cls):
         """Returns a boolean determining if a greater score indicates better model performance."""
         raise NotImplementedError("This objective must have a `greater_is_better` boolean attribute as a class variable")
 
     @property
     @classmethod
     @abstractmethod
-    def score_needs_proba(self):
+    def score_needs_proba(cls):
         """Returns a boolean determining if `score()` needs probability estimates."""
         raise NotImplementedError("This objective must have a `score_needs_proba` boolean attribute as a class variable")
 
     @classmethod
     @abstractmethod
-    def objective_function(self, y_predicted, y_true, X=None):
+    def objective_function(cls, y_predicted, y_true, X=None):
         """Computes the relative value of the provided predictions compared to the true values, according a specified metric
          Arguments:
             y_predicted (pd.Series) : predicted values of length [n_samples]
@@ -50,7 +50,7 @@ class ObjectiveBase(ABC):
         """
         raise NotImplementedError("`objective_function` must be implemented.")
 
-    def score(self, y_predicted, y_true, X=None):
+    def score(cls, y_predicted, y_true, X=None):
         """Returns a numerical score indicating performance based on the differences between the predicted and actual values.
 
         Arguments:
