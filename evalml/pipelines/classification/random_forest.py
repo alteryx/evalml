d9cd7e06121d5249005335a259ff13e3a3313831
Jeremy Shih <jeremyliweishih@gmail.com>
2020-03-30 13:04:35 -0400

    Take hyperparameter ranges from components (#516)
    * Add hyperparameter classproperty
    
    * Remove hyperparemeters, add overriding feature
    
    * Add overriding to docs
    
    * changelog
    
    * Add testcase for hyperpameters
    
    * Update comment
    
    * Separate tests'
    
    * Add no hyperparams test'
    
    * Make gridsearch take list as only values accepted
    
    * Fix tests'
    
    * Change _hyperparameters to custom_hyperparameters
    
    * Add testcases for mixed dtypes list for tuners
    
    * Fix indexing:
    
    * Add custom_hyperparameters to docs since vscode doesn't like to save
    
    * Revert SPT

--- evalml/pipelines/classification/random_forest.py
+++ evalml/pipelines/classification/random_forest.py
@@ -1,5 +1,3 @@
-from skopt.space import Integer, Real
-
 from evalml.pipelines import PipelineBase
 
 
@@ -8,10 +6,3 @@ class RFClassificationPipeline(PipelineBase):
     _name = "Random Forest Classification Pipeline"
     component_graph = ['One Hot Encoder', 'Simple Imputer', 'RF Classifier Select From Model', 'Random Forest Classifier']
     supported_problem_types = ['binary', 'multiclass']
-
-    hyperparameters = {
-        "n_estimators": Integer(10, 1000),
-        "max_depth": Integer(1, 32),
-        "impute_strategy": ["mean", "median", "most_frequent"],
-        "percent_features": Real(.01, 1)
-    }
