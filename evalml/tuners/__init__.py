f51ea2df74842b00a9606bb42cc6b99ed9254de2
Dylan Sherry <sharshofski@gmail.com>
2020-03-31 18:39:58 -0400

    Remove duplicate

--- evalml/tuners/__init__.py
+++ evalml/tuners/__init__.py
@@ -1,7 +1,6 @@
 # flake8:noqa
 from .skopt_tuner import SKOptTuner
 from .tuner import Tuner
-from .tuner_exceptions import NoParamsException
 from .random_search_tuner import RandomSearchTuner
 from .grid_search_tuner import GridSearchTuner
 from .tuner_exceptions import NoParamsException
