abafdcd4839be8adba2ab18ef077a934f086e223
Angela Lin <angela97lin@gmail.com>
2020-04-01 12:10:48 -0400

    add accuracy

--- evalml/objectives/__init__.py
+++ evalml/objectives/__init__.py
@@ -3,6 +3,7 @@ from .fraud_cost import FraudCost
 from .lead_scoring import LeadScoring
 from .objective_base import ObjectiveBase
 from .standard_metrics import (
+    Accuracy,
     AUC,
     AUCMacro,
     AUCMicro,
