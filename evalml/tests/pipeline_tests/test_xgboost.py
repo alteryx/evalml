377ed8f55467322a161672af979230e50e85e54e
Dylan Sherry <sharshofski@gmail.com>
2020-04-01 03:00:38 -0400

    Use SEED_BOUNDS in unit tests

--- evalml/tests/pipeline_tests/test_xgboost.py
+++ evalml/tests/pipeline_tests/test_xgboost.py
@@ -9,7 +9,12 @@ from sklearn.pipeline import Pipeline
 
 from evalml.objectives import PrecisionMicro
 from evalml.pipelines import XGBoostPipeline
-from evalml.utils import get_random_seed, get_random_state, import_or_raise
+from evalml.utils import (
+    SEED_BOUNDS,
+    get_random_seed,
+    get_random_state,
+    import_or_raise
+)
 
 importorskip('xgboost', reason='Skipping test because xgboost not installed')
 
@@ -70,7 +75,7 @@ def test_xg_multi(X_y_multi):
     X, y = X_y_multi
 
     random_seed = 42
-    xgb_random_seed = get_random_seed(get_random_state(random_seed), min_bound=-2**30, max_bound=2**30)
+    xgb_random_seed = get_random_seed(get_random_state(random_seed), min_bound=SEED_BOUNDS.min_bound, max_bound=SEED_BOUNDS.max_bound)
     xgb = import_or_raise("xgboost")
     imputer = SimpleImputer(strategy='mean')
     enc = ce.OneHotEncoder(use_cat_names=True, return_df=True)
