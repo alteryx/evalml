8d38d283335891ba8ac671e76a89ed21587ccb66
Angela Lin <angela97lin@gmail.com>
2020-04-02 17:10:43 -0400

    init things, tbc

--- evalml/tests/utils_tests/test_logger.py
+++ evalml/tests/utils_tests/test_logger.py
@@ -1,28 +1,37 @@
-from evalml.utils import Logger
+from evalml.utils import *
+from colorama import Style
 
 
 def test_logger_verbose():
-    logger = Logger()
-    assert logger.verbose
-    logger = Logger(False)
-    assert not logger.verbose
-    logger.verbose = True
-    assert logger.verbose
+    logger = get_logger('evalml')
+    logger.error('THIS IS AN ERRORORRORORORORORORORO COPY')
+
+    logger.info('THIS IS A INFO COPY')
+
+    log_title(logger, "HIIIIIIII")
+    # logger = Logger()
+    # assert logger.verbose
+    # logger = Logger(False)
+    # assert not logger.verbose
+    # logger.verbose = True
+    # assert logger.verbose
 
 
 def test_logger_log(capsys):
-    logger = Logger()
-    logger.log('Test message')
-    out, err = capsys.readouterr()
-    assert out == 'Test message\n'
-    assert len(err) == 0
+    logger = get_logger('evalml')
 
-    logger.log_title('Log title')
-    out, err = capsys.readouterr()
-    assert 'Log title' in out
-    assert len(err) == 0
+    # logger = Logger()
+    # logger.log('Test message')
+    # out, err = capsys.readouterr()
+    # assert out == 'Test message\n'
+    # assert len(err) == 0
 
-    logger.log_subtitle('Log subtitle')
-    out, err = capsys.readouterr()
-    assert 'Log subtitle' in out
-    assert len(err) == 0
+    # logger.log_title('Log title')
+    # out, err = capsys.readouterr()
+    # assert 'Log title' in out
+    # assert len(err) == 0
+
+    # logger.log_subtitle('Log subtitle')
+    # out, err = capsys.readouterr()
+    # assert 'Log subtitle' in out
+    # assert len(err) == 0
--- evalml/utils/logger.py
+++ evalml/utils/logger.py
@@ -1,4 +1,72 @@
-from colorama import Style
+from colorama import Style, Fore, Back
+
+import logging
+import sys
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    out_handler = logging.StreamHandler(sys.stdout)
+    err_handler = logging.StreamHandler(sys.stdout)
+    out_handler.setFormatter(LevelFormatter())
+    # out_handler.setFormatter(logging.Formatter(fmt=fmt, datefmt=date_fmt))
+    logger.setLevel('DEBUG')
+    logger.addHandler(out_handler)
+    return logger
+
+
+def log(logger, msg, color=None, new_line=True):
+    if color:
+        msg = color + msg + Style.RESET_ALL
+
+    if new_line:
+        print(msg)
+    else:
+        print(msg, end="")
+
+def log_title(logger, title):
+    log(logger, "*" * (len(title) + 4), color=Fore.RED+Back.GREEN)
+    log(logger, "* %s *" % title, color=Fore.RED)
+    log(logger, "*" * (len(title) + 4), color=Fore.RED)
+
+# def log_subtitle(logger, title, underline="=", color=None):
+#     self.log("")
+#     self.log("%s" % title, color=color)
+#     self.log(underline * len(title), color=color)
+
+
+
+class LevelFormatter(logging.Formatter):
+
+    date_fmt = '%m/%d/%Y %I:%M:%S %p'
+    default_fmt = "%(asctime)s %(name)s - %(levelname)s: %(message)s"
+    err_fmt  = "ERROR: %(message)s"
+    debug_fmt  = "DEBUG: %(module)s: %(lineno)d: %(message)s"
+    info_fmt = "%(message)s"
+
+    def __init__(self):
+        super().__init__(fmt=self.default_fmt, datefmt=self.date_fmt)
+
+    def format(self, record):
+        if record.levelno == logging.DEBUG:
+            self._style._fmt = LevelFormatter.debug_fmt
+
+        elif record.levelno == logging.INFO:
+            self._style._fmt = LevelFormatter.info_fmt
+
+        elif record.levelno == logging.ERROR:
+            self._style._fmt = LevelFormatter.err_fmt
+
+        result = logging.Formatter.format(self, record)
+        return result
+
+
+
+
+
+
+
+
 
 
 class Logger:
