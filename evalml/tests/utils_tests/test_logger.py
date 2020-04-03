ed0c10f924779d87eefd15857b512a4b9e16ac6c
Angela Lin <angela97lin@gmail.com>
2020-04-03 00:53:01 -0400

    starting to clean up

--- evalml/tests/utils_tests/test_logger.py
+++ evalml/tests/utils_tests/test_logger.py
@@ -1,37 +1,22 @@
-from evalml.utils import *
 from colorama import Style
 
-
-def test_logger_verbose():
-    logger = get_logger('evalml')
-    logger.error('THIS IS AN ERRORORRORORORORORORORO COPY')
-
-    logger.info('THIS IS A INFO COPY')
-
-    log_title(logger, "HIIIIIIII")
-    # logger = Logger()
-    # assert logger.verbose
-    # logger = Logger(False)
-    # assert not logger.verbose
-    # logger.verbose = True
-    # assert logger.verbose
+from evalml.utils import *
 
 
 def test_logger_log(capsys):
-    logger = get_logger('evalml')
+    logger = get_logger('evalml', print_std=True)
 
-    # logger = Logger()
-    # logger.log('Test message')
-    # out, err = capsys.readouterr()
-    # assert out == 'Test message\n'
-    # assert len(err) == 0
+    log(logger, 'Test message')
+    out, err = capsys.readouterr()
+    assert out == 'Test message\n'
+    assert len(err) == 0
 
-    # logger.log_title('Log title')
-    # out, err = capsys.readouterr()
-    # assert 'Log title' in out
-    # assert len(err) == 0
+    log_title(logger, 'Log title')
+    out, err = capsys.readouterr()
+    assert 'Log title' in out
+    assert len(err) == 0
 
-    # logger.log_subtitle('Log subtitle')
-    # out, err = capsys.readouterr()
-    # assert 'Log subtitle' in out
-    # assert len(err) == 0
+    log_subtitle(logger, 'Log subtitle')
+    out, err = capsys.readouterr()
+    assert 'Log subtitle' in out
+    assert len(err) == 0
--- evalml/utils/logger.py
+++ evalml/utils/logger.py
@@ -1,59 +1,74 @@
-from colorama import Style, Fore, Back
-
 import logging
 import sys
+from logging.handlers import RotatingFileHandler
+
+from colorama import Back, Fore, Style
 
 
-# Logging features we should have
+"""
+Logging features we should have
 
-# Support different log levels
-# Include timestamp and file label in every log message
-# All references to logger are static (this is already done I think)
-# If AutoBase.__init__ has verbose=False, we should only log to file, except for fatal errors
-# We should always log to a file in addition to stdout.
-# Ability in code to enable/disable output to stdout/stderr
-# Ability in code to configure verbosity level for stdout stream (or file stream)
-# Ability in code to log to file instead of / in addition to stdout/stderr
+Support different log levels
+    - Make sure DEBUG, WARNING, ERROR do what they should via format!
+
+Include timestamp and file label in every log message
+
+All references to logger are static (this is already done I think)
+
+If AutoBase.__init__ has verbose=False, we should only log to file, except for fatal errors
+
+We should always log to a file in addition to stdout.
+
+Ability in code to enable/disable output to stdout/stderr
+
+Ability in code to configure verbosity level for stdout stream (or file stream)
+
+Ability in code to log to file instead of / in addition to stdout/stderr
+"""
 
 
-def get_logger(logger_name):
+def get_logger(logger_name, print_std=False):
     logger = logging.getLogger(logger_name)
-    out_handler = logging.StreamHandler(sys.stdout)
-    err_handler = logging.StreamHandler(sys.stdout)
-    out_handler.setFormatter(LevelFormatter())
+    if print_std:
+        out_handler = logging.StreamHandler(sys.stdout)
+        out_handler.setFormatter(LevelFormatter())
+        logger.addHandler(out_handler)
     # out_handler.setFormatter(logging.Formatter(fmt=fmt, datefmt=date_fmt))
     logger.setLevel('DEBUG')
-    logger.addHandler(out_handler)
+    file_handler = RotatingFileHandler('evalml.log', maxBytes=20000, backupCount=5)
+
+    logger.addHandler(file_handler)
     return logger
 
+
 def log(logger, msg, color=None):
+    logger.setLevel('INFO')
     if color:
         msg = color + msg + Style.RESET_ALL
+    logger.info(msg)
+
 
 def log_title(logger, title):
-    log(logger, "*" * (len(title) + 4), color=Fore.RED+Back.GREEN)
+    logger.setLevel('INFO')
+    log(logger, "*" * (len(title) + 4), color=Fore.RED + Back.GREEN)
     log(logger, "* %s *" % title, color=Fore.RED)
     log(logger, "*" * (len(title) + 4), color=Fore.RED)
     log(logger, "")
 
+
 def log_subtitle(logger, title, underline="=", color=None):
+    logger.setLevel('INFO')
     log(logger, "")
     log(logger, "%s" % title, color=color)
     log(logger, underline * len(title), color=color)
-
-# def log_subtitle(logger, title, underline="=", color=None):
-#     self.log("")
-#     self.log("%s" % title, color=color)
-#     self.log(underline * len(title), color=color)
-
 
 
 class LevelFormatter(logging.Formatter):
 
     date_fmt = '%m/%d/%Y %I:%M:%S %p'
     default_fmt = "%(asctime)s %(name)s - %(levelname)s: %(message)s"
-    err_fmt  = "ERROR: %(message)s"
-    debug_fmt  = "DEBUG: %(module)s: %(lineno)d: %(message)s"
+    err_fmt = "ERROR: %(message)s"
+    debug_fmt = "DEBUG: %(module)s: %(lineno)d: %(message)s"
     info_fmt = "%(message)s"
 
     def __init__(self):
@@ -71,8 +86,6 @@ class LevelFormatter(logging.Formatter):
 
         result = logging.Formatter.format(self, record)
         return result
-
-
 
 
 class Logger:
