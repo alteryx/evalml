cbf048c3878384425c31897ab36492724d457ba2
Gaurav Sheni <gaurav.sheni@alteryx.com>
2020-04-15 19:26:43 -0400

    updat test cases

--- evalml/tests/objective_tests/test_standard_metrics.py
+++ evalml/tests/objective_tests/test_standard_metrics.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 from evalml.exceptions import DimensionMismatchError
-from evalml.objectives import AccuracyBinary, BalancedAccuracy
+from evalml.objectives import AccuracyBinary, BalancedAccuracyBinary, BalancedAccuracyMulticlass
 
 
 def test_accuracy():
@@ -21,13 +21,25 @@ def test_accuracy():
         acc.score(y_predicted=np.array([0]), y_true=np.array([1, 0]))
 
 
-def test_binary_accuracy():
-    bacc = BalancedAccuracy()
-    assert bacc.score(np.array([0, 1, 0, 0, 1, 0]),
-                      np.array([0, 1, 0, 0, 0, 1])) == pytest.approx(0.625, 1e-5)
+def test_binary_accuracy_binary():
+    baccb = BalancedAccuracyBinary()
+    assert baccb.score(np.array([0, 1, 0, 0, 1, 0]),
+                       np.array([0, 1, 0, 0, 0, 1])) == pytest.approx(0.625, 1e-5)
 
-    assert bacc.score(np.array([0, 1, 0, 0, 1, 0]),
-                      np.array([0, 1, 0, 0, 1, 0])) == 1.000
+    assert baccb.score(np.array([0, 1, 0, 0, 1, 0]),
+                       np.array([0, 1, 0, 0, 1, 0])) == 1.000
 
-    assert bacc.score(np.array([0, 1, 0, 0, 1, 0]),
-                      np.array([1, 0, 1, 1, 0, 1])) == 0.000
+    assert baccb.score(np.array([0, 1, 0, 0, 1, 0]),
+                       np.array([1, 0, 1, 1, 0, 1])) == 0.000
+
+
+def test_binary_accuracy_multi():
+    baccm = BalancedAccuracyMulticlass()
+    assert baccm.score(np.array([0, 1, 2, 0, 1, 2, 3]),
+                       np.array([0, 0, 2, 0, 0, 2, 3])) == pytest.approx(0.75, 1e-5)
+
+    assert baccm.score(np.array([0, 1, 2, 0, 1, 2, 3]),
+                       np.array([0, 1, 2, 0, 1, 2, 3])) == 1.000
+
+    assert baccm.score(np.array([0, 1, 2, 0, 1, 2, 3]),
+                       np.array([1, 0, 3, 1, 2, 1, 0])) == 0.000
