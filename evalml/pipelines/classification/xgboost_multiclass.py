6eedf5b5829bc41da8ded081ec0d21b0538b43d1
Jeremy Shih <jeremyliweishih@gmail.com>
2020-04-16 17:33:26 -0400

    Remove uncessary inits

--- evalml/pipelines/classification/xgboost_multiclass.py
+++ evalml/pipelines/classification/xgboost_multiclass.py
@@ -6,6 +6,3 @@ class XGBoostMulticlassPipeline(MulticlassClassificationPipeline):
     custom_name = "XGBoost Classifier w/ One Hot Encoder + Simple Imputer + RF Classifier Select From Model"
     component_graph = ['One Hot Encoder', 'Simple Imputer', 'RF Classifier Select From Model', 'XGBoost Classifier']
 
-    def __init__(self, parameters, random_state=0):
-        super().__init__(parameters=parameters,
-                         random_state=random_state)
--- evalml/pipelines/regression/xgboost_regression.py
+++ evalml/pipelines/regression/xgboost_regression.py
@@ -5,7 +5,3 @@ class XGBoostRegressionPipeline(RegressionPipeline):
     """XGBoost Pipeline for regression problems"""
     _name = "XGBoost Regression Pipeline"
     component_graph = ['One Hot Encoder', 'Simple Imputer', 'RF Regressor Select From Model', 'XGBoost Regressor']
-
-    def __init__(self, parameters, random_state=0):
-        super().__init__(parameters=parameters,
-                         random_state=random_state)
