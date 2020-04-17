82675a7e3e810323c9af23d2f2188d141cfe88ea
Dylan Sherry <sharshofski@gmail.com>
2020-04-16 22:41:22 -0400

    Lint

--- evalml/tests/objective_tests/test_standard_metrics.py
+++ evalml/tests/objective_tests/test_standard_metrics.py
@@ -4,22 +4,21 @@ import pytest
 from evalml.exceptions import DimensionMismatchError
 from evalml.objectives import (
     F1,
-    F1Micro,
-    F1Macro,
-    F1Weighted,
-    Precision,
-    PrecisionMicro,
-    PrecisionMacro,
-    PrecisionWeighted,
-    Recall,
-    RecallMicro,
-    RecallMacro,
-    RecallWeighted,
     AccuracyBinary,
     BalancedAccuracyBinary,
     BalancedAccuracyMulticlass,
+    F1Macro,
+    F1Micro,
+    F1Weighted,
+    Precision,
+    PrecisionMacro,
+    PrecisionMicro,
+    PrecisionWeighted,
+    Recall,
+    RecallMacro,
+    RecallMicro,
+    RecallWeighted
 )
-
 
 EPS = 1e-5
 
@@ -82,7 +81,7 @@ def test_f1_binary():
 def test_f1_micro_multi():
     obj = F1Micro()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/3.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 3.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -98,7 +97,7 @@ def test_f1_macro_multi():
     obj = F1Macro()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) \
-                     == pytest.approx(2 * (1/3.0) * (1/9.0) / (1/3.0 + 1/9.0), EPS)
+        == pytest.approx(2 * (1 / 3.0) * (1 / 9.0) / (1 / 3.0 + 1 / 9.0), EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -114,7 +113,7 @@ def test_f1_weighted_multi():
     obj = F1Weighted()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) \
-                     == pytest.approx(2 * (1/3.0) * (1/9.0) / (1/3.0 + 1/9.0), EPS)
+        == pytest.approx(2 * (1 / 3.0) * (1 / 9.0) / (1 / 3.0 + 1 / 9.0), EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -144,7 +143,7 @@ def test_precision_binary():
 def test_precision_micro_multi():
     obj = PrecisionMicro()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/3.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 3.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -159,7 +158,7 @@ def test_precision_micro_multi():
 def test_precision_macro_multi():
     obj = PrecisionMacro()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/9.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 9.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -174,7 +173,7 @@ def test_precision_macro_multi():
 def test_precision_weighted_multi():
     obj = PrecisionWeighted()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/9.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 9.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -201,7 +200,7 @@ def test_recall_binary():
 def test_recall_micro_multi():
     obj = RecallMicro()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/3.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 3.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -216,7 +215,7 @@ def test_recall_micro_multi():
 def test_recall_macro_multi():
     obj = RecallMacro()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/3.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 3.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
@@ -231,7 +230,7 @@ def test_recall_macro_multi():
 def test_recall_weighted_multi():
     obj = RecallWeighted()
     assert obj.score(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1/3.0, EPS)
+                     np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1 / 3.0, EPS)
 
     assert obj.score(np.array([0, 0, 0, 1, 1, 1, 2, 2, 2]),
                      np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])) == pytest.approx(1.0, EPS)
