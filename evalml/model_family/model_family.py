2276ca5583369859b8f6ce7f899c1102b97f33ef
Dylan Sherry <sharshofski@gmail.com>
2020-04-17 20:06:20 -0400

    Update ModelFamily enum vals.

--- evalml/model_family/model_family.py
+++ evalml/model_family/model_family.py
@@ -11,8 +11,8 @@ class ModelFamily(Enum):
 
     def __str__(self):
         model_family_dict = {ModelFamily.RANDOM_FOREST.name: "Random Forest",
-                             ModelFamily.XGBOOST.name: "XGBoost Classifier",
-                             ModelFamily.LINEAR_MODEL.name: "Linear Model",
-                             ModelFamily.CATBOOST.name: "CatBoost Classifier",
+                             ModelFamily.XGBOOST.name: "XGBoost",
+                             ModelFamily.LINEAR_MODEL.name: "Linear",
+                             ModelFamily.CATBOOST.name: "CatBoost",
                              ModelFamily.NONE.name: "None"}
         return model_family_dict[self.name]
