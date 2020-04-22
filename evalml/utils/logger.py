89d8d5a1e5cbe0cc8316b82fdcc2e36706b93095
Angela Lin <angela97lin@gmail.com>
2020-04-22 16:03:25 -0400

    init

--- evalml/utils/logger.py
+++ evalml/utils/logger.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from colorama import Style
 
 
@@ -10,6 +13,27 @@ class Logger:
 
     def __init__(self, verbose=True):
         self.verbose = verbose
+        logger = logging.getLogger('evalml')
+        if not len(logger.handlers):
+            out_handler = logging.StreamHandler(sys.stdout)
+            date_fmt = '%m/%d/%Y %I:%M:%S %p'
+            fmt = "%(message)s"
+            # default_fmt = "%(asctime)s %(name)s - %(levelname)s: %(message)s"
+            # err_fmt = "%(asctime)s %(name)s - %(levelname)s - %(module)s: %(lineno)d: %(message)s"
+            # debug_fmt = "%(asctime)s %(name)s - %(levelname)s - %(module)s: %(lineno)d: %(message)s"
+            out_handler.setFormatter(logging.Formatter(fmt=fmt, datefmt=date_fmt))
+            logger.addHandler(out_handler)
+            logger.setLevel('INFO')
+        self.logger = logger
+
+    def warn(self):
+        pass
+
+    def error(self):
+        pass
+
+    def print(self):
+        pass
 
     def log(self, msg, color=None, new_line=True):
         if not self.verbose:
@@ -18,10 +42,7 @@ class Logger:
         if color:
             msg = color + msg + Style.RESET_ALL
 
-        if new_line:
-            print(msg)
-        else:
-            print(msg, end="")
+        self.logger.info(msg)
 
     def log_title(self, title):
         self.log("*" * (len(title) + 4), color=Style.BRIGHT)
