a4358f760e92c79f7f5a76a0889e90066eb8accd
Dylan Sherry <sharshofski@gmail.com>
2020-04-23 10:35:13 -0400

    Remove unnecessary raise in test mock

--- evalml/tests/automl_tests/test_autobase.py
+++ evalml/tests/automl_tests/test_autobase.py
@@ -95,11 +95,6 @@ def test_rankings(X_y, X_y_reg):
 def test_detect_label_leakage(mock_detect_label_leakage, mock_log, mock_fit, capsys, X_y):
     X, y = X_y
     mock_detect_label_leakage.return_value = {'var 1': 0.1234, 'var 2': 0.5678}
-
-    class DummyException(Exception):
-        pass
-
-    mock_fit.side_effect = DummyException  # a hack to avoid fitting models in unit test, not necessary here
     automl = AutoClassificationSearch(max_pipelines=1, random_state=0)
     automl.search(X, y, raise_errors=False)
     mock_log.assert_called_with("WARNING: Possible label leakage: var 1, var 2")
