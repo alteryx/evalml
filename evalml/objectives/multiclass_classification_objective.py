27fc6f84b4bdbc777c8b20272272f4766d96de82
Dylan Sherry <sharshofski@gmail.com>
2020-04-24 17:31:24 -0400

    Remove hyphen from multiclass in two places

--- evalml/objectives/multiclass_classification_objective.py
+++ evalml/objectives/multiclass_classification_objective.py
@@ -5,7 +5,7 @@ from evalml.problem_types import ProblemTypes
 
 class MulticlassClassificationObjective(ObjectiveBase):
     """
-    Base class for all multi-class classification objectives.
+    Base class for all multiclass classification objectives.
 
     problem_type (ProblemTypes): Specifies the type of problem this objective is defined for (multiclass classification).
     """
