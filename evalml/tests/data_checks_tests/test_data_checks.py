8d9ab4deb440059188512327cec05a1bf5cd2a3f
Angela Lin <angela97lin@gmail.com>
2020-04-28 15:26:32 -0400

    fix tests

--- evalml/tests/data_checks_tests/test_data_checks.py
+++ evalml/tests/data_checks_tests/test_data_checks.py
@@ -6,15 +6,15 @@ def test_data_check(X_y):
     X, y = X_y
 
     class MockDataCheck(DataCheck):
-        def validate(X, y, verbose=True):
+        def validate(self, X, y, verbose=True):
             return [], []
 
     class MockDataCheckWarning(DataCheck):
-        def validate(X, y, verbose=True):
+        def validate(self, X, y, verbose=True):
             return [], []
 
     class MockDataCheckError(DataCheck):
-        def validate(X, y, verbose=True):
+        def validate(self, X, y, verbose=True):
             return [], []
 
     data_checks_list = [MockDataCheck(), MockDataCheckWarning(), MockDataCheckError()]
