6854c8125c54062fddb9c9f22299e648685eb815
Angela Lin <angela97lin@gmail.com>
2020-04-28 14:51:48 -0400

    cleanup

--- evalml/data_checks/__init__.py
+++ evalml/data_checks/__init__.py
@@ -1,4 +1,4 @@
 # flake8:noqas
-from data_checks.data_check import DataCheck
-from data_checks.data_checks import DataChecks
-from data_checks.messsage import Message, DataCheckWarning, DataCheckError
+from .data_check import DataCheck
+from .data_checks import DataChecks
+from .messsage import Message, DataCheckWarning, DataCheckError
