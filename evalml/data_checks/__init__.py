41da8aa7d94159e76c174ced52e16860274a69d8
Angela Lin <angela97lin@gmail.com>
2020-04-28 14:40:47 -0400

    linting and cleanup

---
+++ evalml/data_checks/__init__.py
@@ -0,0 +1,4 @@
+# flake8:noqas
+from data_checks.data_check import DataCheck
+from data_checks.data_checks import DataChecks
+from data_checks.messsage import Message, DataCheckWarning, DataCheckError
--- evalml/tests/data_checks_tests/test_message.py
+++ evalml/tests/data_checks_tests/test_message.py
@@ -1,15 +1,19 @@
-from evalml.data_checks.messsage import Message, DataCheckError, DataCheckWarning
+from evalml.data_checks.messsage import (
+    DataCheckError,
+    DataCheckWarning,
+    Message
+)
+
 
 def test_data_check_message():
-    Message m = Message("test message")
+    m = Message("test message")
     assert m.message == "test message"
     assert str(m) == "test message"
 
-    DataCheckWarning data_warning = DataCheckWarning("test warning")
+    data_warning = DataCheckWarning("test warning")
     assert data_warning.message == "test warning"
     assert str(data_warning) == "test warning"
 
-    DataCheckError data_error = DataCheckError("test error")
+    data_error = DataCheckError("test error")
     assert data_error.message == "test error"
     assert str(data_error) == "test error"
-
