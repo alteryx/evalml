cd1e042cfce4941ff8d4a974d596055c259c1e98
Angela Lin <angela97lin@gmail.com>
2020-05-05 14:05:04 -0400

    revert conf.py since nothing crucial changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,16 +47,15 @@ release = evalml.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
     'nbsphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.extlinks',
     'sphinx.ext.graphviz',
     'sphinx.ext.inheritance_diagram'
 ]
-
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
