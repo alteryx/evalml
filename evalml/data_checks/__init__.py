fa5b65f8c333dee83829fed35c11b27a9e378d50
Angela Lin <angela97lin@gmail.com>
2020-05-04 23:07:32 -0400

    lint and docs

--- evalml/data_checks/__init__.py
+++ evalml/data_checks/__init__.py
@@ -2,3 +2,4 @@
 from .data_check import DataCheck
 from .data_checks import DataChecks
 from .data_check_message import DataCheckMessage, DataCheckWarning, DataCheckError
+from .data_check_message_type import DataCheckMessageType
