16e1363f0b59bcdb07300970f17fbf0c594a0ff6
Angela Lin <angela97lin@gmail.com>
2020-05-05 14:29:02 -0400

    Define `DataCheck` and `DataChecks` classes (#739)
    * init
    
    * changelog
    
    * init more tests
    
    * api ref
    
    * updating via comments, still some work to do
    
    * removing verbose flag
    
    * addressing comments cont
    
    * lint and docs
    
    * finish addressing comments
    
    * extract mock classes as fixtures
    
    * update funkay
    
    * revert conf.py since nothing crucial changed

---
+++ evalml/tests/data_checks_tests/test_data_check.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import pytest
+
+from evalml.data_checks.data_check import DataCheck
+from evalml.data_checks.data_check_message import (
+    DataCheckError,
+    DataCheckWarning
+)
+
+
+@pytest.fixture
+def mock_data_check_class():
+    class MockDataCheck(DataCheck):
+        def validate(self, X, y=None):
+            return []
+    return MockDataCheck
+
+
+def test_data_check_name(mock_data_check_class):
+    assert mock_data_check_class().name == "MockDataCheck"
+    assert mock_data_check_class.name == "MockDataCheck"
+
+    class Funky_Name1DataCheck(mock_data_check_class):
+        """Mock data check with a funky name"""
+
+    assert Funky_Name1DataCheck().name == "Funky_Name1DataCheck"
+    assert Funky_Name1DataCheck.name == "Funky_Name1DataCheck"
+
+
+def test_empty_data_check_validate(mock_data_check_class):
+    assert mock_data_check_class().validate(pd.DataFrame()) == []
+
+
+def test_data_check_validate_simple(X_y):
+    X, y = X_y
+
+    class MockDataCheck(DataCheck):
+        def validate(self, X, y=None):
+            return [DataCheckError("error one", self.name), DataCheckWarning("warning one", self.name)]
+
+    data_check = MockDataCheck()
+    errors_warnings = data_check.validate(X, y=y)
+    assert errors_warnings == [DataCheckError("error one", "MockDataCheck"), DataCheckWarning("warning one", "MockDataCheck")]
+
+
+def test_data_check_with_param():
+    X = pd.DataFrame()
+
+    class MockDataCheckWithParam(DataCheck):
+        def __init__(self, num):
+            self.num = num
+
+        def validate(self, X, y=None):
+            if self.num != 10:
+                return [DataCheckError("Expected num == 10", self.name)]
+            return []
+
+    data_check = MockDataCheckWithParam(num=10)
+    errors_warnings = data_check.validate(X, y=None)
+    assert errors_warnings == []
+
+    data_check = MockDataCheckWithParam(num=0)
+    errors_warnings = data_check.validate(X, y=None)
+    assert errors_warnings == [DataCheckError("Expected num == 10", "MockDataCheckWithParam")]
