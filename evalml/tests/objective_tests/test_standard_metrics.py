51262021f869201cd55144a689fa81a09d24deb3
Angela Lin <angela97lin@gmail.com>
2020-05-08 15:27:30 -0400

    revert to valueerrors

--- evalml/tests/objective_tests/test_standard_metrics.py
+++ evalml/tests/objective_tests/test_standard_metrics.py
@@ -26,89 +26,85 @@ EPS = 1e-5
 
 
 def test_input_contains_nan(capsys):
-    y_pred = np.array([np.nan, 0, 0])
+    y_predicted = np.array([np.nan, 0, 0])
     y_true = np.array([1, 2, 1])
     for objective in OPTIONS.values():
-        out, err = capsys.readouterr()
-        objective.score(y_true, y_pred)
-        assert out == "WARNING: y_predicted contains NaN or infinity"
-        assert err == ""
+        with pytest.raises(ValueError, match="y_predicted contains NaN or infinity"):
+            objective.score(y_true, y_predicted)
+
     y_true = np.array([np.nan, 0, 0])
-    y_pred = np.array([1, 2, 0])
+    y_predicted = np.array([1, 2, 0])
     for objective in OPTIONS.values():
         with pytest.raises(ValueError, match="y_true contains NaN or infinity"):
-            objective.score(y_true, y_pred)
+            objective.score(y_true, y_predicted)
 
 
 def test_input_contains_inf(capsys):
-    y_pred = np.array([np.inf, 0, 0])
+    y_predicted = np.array([np.inf, 0, 0])
     y_true = np.array([1, 0, 0])
     for objective in OPTIONS.values():
-        out, err = capsys.readouterr()
-        objective.score(y_true, y_pred)
-        assert out == "WARNING: y_predicted contains NaN or infinity"
-        assert err == ""
+        with pytest.raises(ValueError, match="y_predicted contains NaN or infinity"):
+            objective.score(y_true, y_predicted)
+
     y_true = np.array([np.inf, 0, 0])
-    y_pred = np.array([1, 0, 0])
+    y_predicted = np.array([1, 0, 0])
     for objective in OPTIONS.values():
-        out, err = capsys.readouterr()
-        objective.score(y_true, y_pred)
-        assert out == "WARNING: y_true contains infinity values"
-        assert err == ""
+        with pytest.raises(ValueError, match="y_true contains NaN or infinity"):
+            objective.score(y_true, y_predicted)
 
 
 def test_different_input_lengths():
-    y_pred = np.array([0, 0])
+    y_predicted = np.array([0, 0])
     y_true = np.array([1])
     for objective in OPTIONS.values():
         with pytest.raises(ValueError, match="Inputs have mismatched dimensions"):
-            objective.score(y_true, y_pred)
+            objective.score(y_true, y_predicted)
 
     y_true = np.array([0, 0])
-    y_pred = np.array([1, 2, 0])
+    y_predicted = np.array([1, 2, 0])
     for objective in OPTIONS.values():
         with pytest.raises(ValueError, match="Inputs have mismatched dimensions"):
-            objective.score(y_true, y_pred)
+            objective.score(y_true, y_predicted)
 
 
 def test_zero_input_lengths():
-    y_pred = np.array([])
+    y_predicted = np.array([])
     y_true = np.array([])
     for objective in OPTIONS.values():
         with pytest.raises(ValueError, match="Length of inputs is 0"):
-            objective.score(y_true, y_pred)
+            objective.score(y_true, y_predicted)
 
 
 def test_probabilities_not_in_0_1_range():
-    y_pred = np.array([0.3, 1.001, 0.3])
+    y_predicted = np.array([0.3, 1.001, 0.3])
     y_true = np.array([1, 0, 1])
     for objective in OPTIONS.values():
         if objective.score_needs_proba:
             with pytest.raises(ValueError, match="y_predicted contains probability estimates"):
-                objective.score(y_true, y_pred)
+                objective.score(y_true, y_predicted)
 
-    y_pred = np.array([0.3, -0.001, 0.3])
+    y_predicted = np.array([0.3, -0.001, 0.3])
     y_true = np.array([1, 0, 1])
     for objective in OPTIONS.values():
         if objective.score_needs_proba:
             with pytest.raises(ValueError, match="y_predicted contains probability estimates"):
-                objective.score(y_true, y_pred)
+                objective.score(y_true, y_predicted)
 
 
 def test_binary_more_than_two_unique_values():
-    y_pred = np.array([0, 1, 2])
+    y_predicted = np.array([0, 1, 2])
     y_true = np.array([1, 0, 1])
     for objective in OPTIONS.values():
         if isinstance(objective, BinaryClassificationObjective) and not objective.score_needs_proba:
             with pytest.raises(ValueError, match="y_predicted contains more than two unique values"):
-                objective.score(y_true, y_pred)
+                objective.score(y_true, y_predicted)
 
     y_true = np.array([0, 1, 2])
-    y_pred = np.array([1, 0, 1])
+    y_predicted = np.array([1, 0, 1])
     for objective in OPTIONS.values():
         if isinstance(objective, BinaryClassificationObjective) and not objective.score_needs_proba:
             with pytest.raises(ValueError, match="y_true contains more than two unique values"):
-                objective.score(y_true, y_pred)
+                objective.score(y_true, y_predicted)
 
 
 def test_accuracy_binary():
