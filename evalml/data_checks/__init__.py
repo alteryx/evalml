c5c8846c79aa5e2cb14682d09c4e00015d628276
Angela Lin <angela97lin@gmail.com>
2020-05-08 15:08:30 -0400

    Port over highly-null Data Check and define BasicDataChecks and DisableDataChecks classes (#745)
    * init
    
    * changelog
    
    * docstr test'
    
    * cleanup
    
    * add basic and empty data checks
    
    * codecov
    
    * address PR comments
    
    * add edge cases
    
    * cleanup via pr
    
    * oops, revert to master
    
    * clean up docstr
    
    * cleanup
    
    * cleanup
    
    * update docstr

--- evalml/data_checks/__init__.py
+++ evalml/data_checks/__init__.py
@@ -3,3 +3,6 @@ from .data_check import DataCheck
 from .data_checks import DataChecks
 from .data_check_message import DataCheckMessage, DataCheckWarning, DataCheckError
 from .data_check_message_type import DataCheckMessageType
+from .detect_highly_null_data_check import DetectHighlyNullDataCheck
+from .default_data_checks import DefaultDataChecks
+from .utils import EmptyDataChecks
--- evalml/tests/data_checks_tests/test_data_checks.py
+++ evalml/tests/data_checks_tests/test_data_checks.py
@@ -1,9 +1,13 @@
+import pandas as pd
+
 from evalml.data_checks.data_check import DataCheck
 from evalml.data_checks.data_check_message import (
     DataCheckError,
     DataCheckWarning
 )
 from evalml.data_checks.data_checks import DataChecks
+from evalml.data_checks.default_data_checks import DefaultDataChecks
+from evalml.data_checks.utils import EmptyDataChecks
 
 
 def test_data_checks(X_y):
@@ -27,8 +31,26 @@ def test_data_checks(X_y):
 
     data_checks_list = [MockDataCheck(), MockDataCheckWarning(), MockDataCheckError(), MockDataCheckErrorAndWarning()]
     data_checks = DataChecks(data_checks=data_checks_list)
-    errors_warnings = data_checks.validate(X, y)
-    assert errors_warnings == [DataCheckWarning("warning one", "MockDataCheckWarning"),
-                               DataCheckError("error one", "MockDataCheckError"),
-                               DataCheckError("error two", "MockDataCheckErrorAndWarning"),
-                               DataCheckWarning("warning two", "MockDataCheckErrorAndWarning")]
+    messages = data_checks.validate(X, y)
+    assert messages == [DataCheckWarning("warning one", "MockDataCheckWarning"),
+                        DataCheckError("error one", "MockDataCheckError"),
+                        DataCheckError("error two", "MockDataCheckErrorAndWarning"),
+                        DataCheckWarning("warning two", "MockDataCheckErrorAndWarning")]
+
+
+def test_empty_data_checks(X_y):
+    X, y = X_y
+    data_checks = EmptyDataChecks()
+    messages = data_checks.validate(X, y)
+    assert messages == []
+
+
+def test_default_data_checks(X_y):
+    X = pd.DataFrame({'lots_of_null': [None, None, None, None, 5],
+                      'all_null': [None, None, None, None, None],
+                      'also_all_null': [None, None, None, None, None],
+                      'no_null': [1, 2, 3, 4, 5]})
+    data_checks = DefaultDataChecks()
+    messages = data_checks.validate(X)
+    assert messages == [DataCheckWarning("Column 'all_null' is 95.0% or more null", "DetectHighlyNullDataCheck"),
+                        DataCheckWarning("Column 'also_all_null' is 95.0% or more null", "DetectHighlyNullDataCheck")]
