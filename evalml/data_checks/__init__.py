21e25a2ff5f695590f90bd393b8549eb8d90f9a6
Angela Lin <angela97lin@gmail.com>
2020-05-22 01:36:43 -0400

    begin doc fix

--- evalml/data_checks/__init__.py
+++ evalml/data_checks/__init__.py
@@ -3,7 +3,9 @@ from .data_check import DataCheck
 from .data_checks import DataChecks
 from .data_check_message import DataCheckMessage, DataCheckWarning, DataCheckError
 from .data_check_message_type import DataCheckMessageType
-from .detect_highly_null_data_check import DetectHighlyNullDataCheck
 from .default_data_checks import DefaultDataChecks
 from .utils import EmptyDataChecks
+from .detect_highly_null_data_check import DetectHighlyNullDataCheck
+from .detect_id_columns_data_check import DetectIDColumnsDataCheck
 from .detect_label_leakage_data_check import DetectLabelLeakageDataCheck
+from .detect_outliers_data_check import DetectOutliersDataCheck
