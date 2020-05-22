f07a4250ea89a3acae8f541eb73e781ee8cea8bb
Angela Lin <angela97lin@gmail.com>
2020-05-22 13:58:35 -0400

    add to input test

--- evalml/tests/data_checks_tests/test_detect_outliers_data_check.py
+++ evalml/tests/data_checks_tests/test_detect_outliers_data_check.py
@@ -52,6 +52,11 @@ def test_outliers_data_check_input_formats():
     X.iloc[72, :] = pd.Series(np.random.randn(100) * 100)
 
     outliers_check = DetectOutliersDataCheck()
+    messages = [message.message for message in outliers_check.validate(X.to_numpy())]
+    assert messages == ["Row '3' is likely to have outlier data",
+                        "Row '25' is likely to have outlier data",
+                        "Row '55' is likely to have outlier data",
+                        "Row '72' is likely to have outlier data"]
     assert outliers_check.validate(X.to_numpy()) == [DataCheckWarning("Row '3' is likely to have outlier data", "DetectOutliersDataCheck"),
                                                      DataCheckWarning("Row '25' is likely to have outlier data", "DetectOutliersDataCheck"),
                                                      DataCheckWarning("Row '55' is likely to have outlier data", "DetectOutliersDataCheck"),
