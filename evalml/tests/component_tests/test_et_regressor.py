c26b2f4361b7368c19f479ee7dfb68bb435383e3
Becca McBrayer <rebecca.mcbrayer@gmail.com>
2020-05-27 11:13:54 -0400

    Linting and a few test tweaks

--- evalml/tests/component_tests/test_et_regressor.py
+++ evalml/tests/component_tests/test_et_regressor.py
@@ -1,11 +1,8 @@
-from unittest.mock import patch
-
-import category_encoders as ce
 import numpy as np
-import pandas as pd
 import pytest
 from sklearn.ensemble import ExtraTreesRegressor as SKExtraTreesRegressor
 
+from evalml.exceptions import MethodPropertyNotFoundError
 from evalml.model_family import ModelFamily
 from evalml.pipelines.components.estimators.regressors import ExtraTreesRegressor
 from evalml.problem_types import ProblemTypes
@@ -22,7 +19,7 @@ def test_problem_types():
 
 def test_fit_predict(X_y):
     X, y = X_y
-    
+
     sk_clf = SKExtraTreesRegressor()
     sk_clf.fit(X, y)
     y_pred_sk = sk_clf.predict(X)
@@ -37,10 +34,16 @@ def test_fit_predict(X_y):
 def test_feature_importances(X_y):
     X, y = X_y
 
+    # testing that feature importances can't be called before fit
+    clf = ExtraTreesRegressor()
+    with pytest.raises(MethodPropertyNotFoundError):
+        feature_importances = clf.feature_importances
+
     sk_clf = SKExtraTreesRegressor(random_state=0)
     sk_clf.fit(X, y)
+    sk_feature_importances = sk_clf.feature_importances_
 
-    clf = ExtraTreesRegressor()
     clf.fit(X, y)
+    feature_importances = clf.feature_importances
 
-    np.testing.assert_almost_equal(sk_clf.feature_importances_, clf.feature_importances, decimal=5)
+    np.testing.assert_almost_equal(sk_feature_importances, feature_importances, decimal=5)
