bb73610c73951804bd0e098e94340f723dd29fc6
Becca McBrayer <rebecca.mcbrayer@gmail.com>
2020-05-28 14:39:45 -0400

    Update et_regression pipeline tests to remove redundancy

--- evalml/tests/pipeline_tests/regression_pipeline_tests/test_et_regression.py
+++ evalml/tests/pipeline_tests/regression_pipeline_tests/test_et_regression.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import category_encoders as ce
 import numpy as np
 import pandas as pd
@@ -8,6 +10,13 @@ from sklearn.pipeline import Pipeline
 
 from evalml.objectives import R2
 from evalml.pipelines import ETRegressionPipeline
+
+
+def make_mock_et_regression_pipeline():
+    class MockETRegressionPipeline(ETRegressionPipeline):
+        component_graph = ['Extra Trees Regressor']
+
+    return MockETRegressionPipeline({})
 
 
 def test_et_init(X_y_reg):
@@ -56,53 +65,20 @@ def test_summary():
     assert ETRegressionPipeline.summary == 'Extra Trees Regressor w/ One Hot Encoder + Simple Imputer + RF Regressor Select From Model'
 
 
-def test_et_regression(X_y_categorical_regression):
-    X, y = X_y_categorical_regression
-
-    imputer = SimpleImputer(strategy='most_frequent')
-    enc = ce.OneHotEncoder(use_cat_names=True, return_df=True)
-    estimator = ExtraTreesRegressor(random_state=0,
-                                    n_estimators=10,
-                                    max_depth=None,
-                                    max_features="auto",
-                                    n_jobs=-1)
-    rf_estimator = RandomForestRegressor(random_state=0, n_estimators=10, max_depth=3)
-    feature_selection = SelectFromModel(estimator=rf_estimator,
-                                        max_features=max(1, int(1 * X.shape[1])),
-                                        threshold=-np.inf)
-    sk_pipeline = Pipeline([("encoder", enc),
-                            ("imputer", imputer),
-                            ("feature_selection", feature_selection),
-                            ("estimator", estimator)])
-    sk_pipeline.fit(X, y)
-    sk_score = sk_pipeline.score(X, y)
-
-    objective = R2()
-    parameters = {
-        'Simple Imputer': {
-            'impute_strategy': 'most_frequent'
-        },
-        'RF Regressor Select From Model': {
-            "percent_features": 1.0,
-            "number_features": X.shape[1],
-            "n_estimators": 10,
-            "max_depth": 3,
-        },
-        'Extra Trees Regressor': {
-            "n_estimators": 10,
-            "max_features": "auto",
-        }
-    }
-    clf = ETRegressionPipeline(parameters=parameters)
+@patch('evalml.pipelines.regression.ETRegressionPipeline.fit')
+@patch('evalml.pipelines.regression.ETRegressionPipeline.predict')
+def test_et_score(mock_predict, mock_fit, X_y):
+    X, y = X_y
+    
+    mock_predict.return_value = y
+    clf = make_mock_et_regression_pipeline()
     clf.fit(X, y)
-    clf_scores = clf.score(X, y, [objective])
-    y_pred = clf.predict(X)
-    np.testing.assert_almost_equal(y_pred, sk_pipeline.predict(X), decimal=5)
-    np.testing.assert_almost_equal(sk_score, clf_scores[objective.name], decimal=5)
 
-    # testing objective parameter passed in does not change results
-    y_pred_with_objective = clf.predict(X, objective)
-    np.testing.assert_almost_equal(y_pred, y_pred_with_objective, decimal=5)
+    objective_names = ['r2']
+    scores = clf.score(X, y, objective_names)
+    mock_predict.assert_called()
+
+    assert scores == {'R2': 1.0}
 
 
 def test_etr_input_feature_names(X_y_reg):
