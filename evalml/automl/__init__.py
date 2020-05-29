c5be618c10b9fa07ba8fbc51a016b5cf1b2d60d7
Dylan Sherry <sharshofski@gmail.com>
2020-05-29 14:41:09 -0400

    Add AutoSearchBase to init so it can be included in docs

--- evalml/automl/__init__.py
+++ evalml/automl/__init__.py
@@ -1,3 +1,4 @@
 # flake8:noqas
 from .auto_classification_search import AutoClassificationSearch
 from .auto_regression_search import AutoRegressionSearch
+from .auto_search_base import AutoSearchBase
