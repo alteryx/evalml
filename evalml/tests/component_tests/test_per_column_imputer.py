f81c94344455b485923ca36ae6530e92639def4a
Jeremy Shih <jeremyliweishih@gmail.com>
2020-06-08 15:25:01 -0400

    Fix tests

--- evalml/tests/component_tests/test_per_column_imputer.py
+++ evalml/tests/component_tests/test_per_column_imputer.py
@@ -6,6 +6,16 @@ from pandas.testing import assert_frame_equal
 from evalml.pipelines.components import PerColumnImputer
 
 
+@pytest.fixture
+def non_numeric_df():
+    X = pd.DataFrame([["a", "a", "a", "a"],
+                      ["b", "b", "b", "b"],
+                      ["a", "a", "a", "a"],
+                      [np.nan, np.nan, np.nan, np.nan]])
+    X.columns = ['A', 'B', 'C', 'D']
+    return X
+
+
 def test_all_strategies():
     X = pd.DataFrame([[2, 4, 6, "a"],
                       [4, 6, 8, "a"],
@@ -29,23 +39,38 @@ def test_all_strategies():
 
     transformer = PerColumnImputer(impute_strategies=strategies)
     X_t = transformer.fit_transform(X)
-
     assert_frame_equal(X_expected, X_t, check_dtype=False)
+
+
+def test_fit_transform():
+    X = pd.DataFrame([[2],
+                      [4],
+                      [6],
+                      [np.nan]])
+
+    X_expected = pd.DataFrame([[2],
+                               [4],
+                               [6],
+                               [4]])
+
+    X.columns = ['A']
+    X_expected.columns = ['A']
+
+    strategies = {'A': 'median'}
 
     transformer = PerColumnImputer(impute_strategies=strategies)
     transformer.fit(X)
     X_t = transformer.transform(X)
 
-    assert_frame_equal(X_expected, X_t, check_dtype=False)
+    transformer = PerColumnImputer(impute_strategies=strategies)
+    X_fit_transform = transformer.fit_transform(X)
+
+    assert_frame_equal(X_t, X_fit_transform, check_dtype=False)
 
 
-def test_col_with_non_numeric():
+def test_non_numeric_errors(non_numeric_df):
     # test col with all strings
-    X = pd.DataFrame([["a", "a", "a", "a"],
-                      ["b", "b", "b", "b"],
-                      ["a", "a", "a", "a"],
-                      [np.nan, np.nan, np.nan, np.nan]])
-    X.columns = ['A', 'B', 'C', 'D']
+    X = non_numeric_df
 
     # mean with all strings
     strategies = {'A': 'mean'}
@@ -65,6 +90,10 @@ def test_col_with_non_numeric():
         transformer = PerColumnImputer(impute_strategies=strategies)
         transformer.fit(X)
 
+
+def test_non_numeric_valid(non_numeric_df):
+    X = non_numeric_df
+
     # most frequent with all strings
     strategies = {'C': 'most_frequent'}
     transformer = PerColumnImputer(impute_strategies=strategies)
@@ -78,9 +107,6 @@ def test_col_with_non_numeric():
     X_t = transformer.fit_transform(X)
     assert_frame_equal(X_expected, X_t, check_dtype=False)
 
-    X_t = transformer.transform(X)
-    assert_frame_equal(X_expected, X_t, check_dtype=False)
-
     # constant with all strings
     strategies = {'D': ('constant', 100)}
     transformer = PerColumnImputer(impute_strategies=strategies)
@@ -92,7 +118,4 @@ def test_col_with_non_numeric():
     X_expected.columns = ['A', 'B', 'C', 'D']
 
     X_t = transformer.fit_transform(X)
-    assert_frame_equal(X_expected, X_t, check_dtype=False)
-
-    X_t = transformer.transform(X)
     assert_frame_equal(X_expected, X_t, check_dtype=False)
