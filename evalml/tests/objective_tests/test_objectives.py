e8ae86ceedb8caacd234608709183bafa9f40d5c
Dylan Sherry <sharshofski@gmail.com>
2020-06-17 01:16:26 -0400

    Add unit test to cover dtype checking in objectives, for codecov

--- evalml/tests/objective_tests/test_objectives.py
+++ evalml/tests/objective_tests/test_objectives.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import pytest
 
 from evalml.exceptions import ObjectiveNotFoundError
@@ -39,3 +41,17 @@ def test_get_objectives_types():
     assert len(get_objectives(ProblemTypes.MULTICLASS)) == 13
     assert len(get_objectives(ProblemTypes.BINARY)) == 7
     assert len(get_objectives(ProblemTypes.REGRESSION)) == 7
+
+
+def test_objective_output_type(X_y):
+    _, y_np = X_y
+    print(y_np)
+    assert isinstance(y_np, np.ndarray)
+    all_objectives = (get_objectives(ProblemTypes.REGRESSION) +
+                      get_objectives(ProblemTypes.BINARY) +
+                      get_objectives(ProblemTypes.MULTICLASS))
+    for objective in all_objectives:
+        print('Testing objective {}'.format(objective.name))
+        expected_value = 1.0 if objective.greater_is_better else 0.0
+        np.testing.assert_almost_equal(objective.score(y_np, y_np), expected_value)
+        np.testing.assert_almost_equal(objective.score(pd.Series(y_np), pd.Series(y_np)), expected_value)
