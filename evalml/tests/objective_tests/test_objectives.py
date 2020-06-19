a02708f058b4cfc2ef035050c7ce53c9d9ac3421
Dylan Sherry <sharshofski@gmail.com>
2020-06-19 11:13:51 -0400

    Add fix for multiclass validation--wasn't working with pd types. Add unit test coverage for pd+np dtypes with objective scoring

--- evalml/tests/objective_tests/test_objectives.py
+++ evalml/tests/objective_tests/test_objectives.py
@@ -3,7 +3,14 @@ import pandas as pd
 import pytest
 
 from evalml.exceptions import ObjectiveNotFoundError
-from evalml.objectives import Precision, get_objective, get_objectives
+from evalml.objectives import (
+    BinaryClassificationObjective,
+    MulticlassClassificationObjective,
+    Precision,
+    RegressionObjective,
+    get_objective,
+    get_objectives
+)
 from evalml.objectives.objective_base import ObjectiveBase
 from evalml.problem_types import ProblemTypes
 
@@ -43,15 +50,31 @@ def test_get_objectives_types():
     assert len(get_objectives(ProblemTypes.REGRESSION)) == 7
 
 
-def test_objective_output_type(X_y):
-    _, y_np = X_y
-    print(y_np)
-    assert isinstance(y_np, np.ndarray)
+def test_objective_outputs(X_y, X_y_multi):
+    _, y_binary_np = X_y
+    assert isinstance(y_binary_np, np.ndarray)
+    _, y_multi_np = X_y_multi
+    assert isinstance(y_multi_np, np.ndarray)
+    y_true_multi_np = y_multi_np
+    y_pred_multi_np = y_multi_np
+    # convert to a simulated predicted probability, which must range between 0 and 1
+    classes = np.unique(y_multi_np)
+    y_pred_proba_multi_np = np.concatenate([(y_multi_np == val).astype(float).reshape(-1, 1) for val in classes], axis=1)
+
     all_objectives = (get_objectives(ProblemTypes.REGRESSION) +
                       get_objectives(ProblemTypes.BINARY) +
                       get_objectives(ProblemTypes.MULTICLASS))
     for objective in all_objectives:
         print('Testing objective {}'.format(objective.name))
         expected_value = 1.0 if objective.greater_is_better else 0.0
-        np.testing.assert_almost_equal(objective.score(y_np, y_np), expected_value)
-        np.testing.assert_almost_equal(objective.score(pd.Series(y_np), pd.Series(y_np)), expected_value)
+        if isinstance(objective, (RegressionObjective, BinaryClassificationObjective)):
+            np.testing.assert_almost_equal(objective.score(y_binary_np, y_binary_np), expected_value)
+            np.testing.assert_almost_equal(objective.score(pd.Series(y_binary_np), pd.Series(y_binary_np)), expected_value)
+        if isinstance(objective, MulticlassClassificationObjective):
+            y_predicted = y_pred_multi_np
+            y_predicted_pd = pd.Series(y_predicted)
+            if objective.score_needs_proba:
+                y_predicted = y_pred_proba_multi_np
+                y_predicted_pd = pd.DataFrame(y_predicted)
+            np.testing.assert_almost_equal(objective.score(y_true_multi_np, y_predicted), expected_value)
+            np.testing.assert_almost_equal(objective.score(pd.Series(y_true_multi_np), y_predicted_pd), expected_value)
