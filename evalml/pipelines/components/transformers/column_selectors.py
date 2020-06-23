96c41bf3c69c9e200e7b7a0f289716497f01e179
freddyaboulton <alfonsoboulton@gmail.com>
2020-06-23 15:05:25 -0400

    Adding docstring to _modify_columns method

--- evalml/pipelines/components/transformers/column_selectors.py
+++ evalml/pipelines/components/transformers/column_selectors.py
@@ -38,7 +38,7 @@ class ColumnSelector(Transformer):
 
     @abstractmethod
     def _modify_columns(self, cols, X, y=None):
-        pass
+        """How the transformer modifies the columns of the input data."""
 
     def fit(self, X, y=None):
         """'Fits' the transformer by checking if the column names are present in the dataset.
