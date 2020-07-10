82f14845280f21bcb323ef46e38be8068573182b
Becca McBrayer <rebecca.mcbrayer@gmail.com>
2020-07-10 10:11:52 -0400

    Add primitive output testing

--- evalml/tests/component_tests/test_text_featurization.py
+++ evalml/tests/component_tests/test_text_featurization.py
@@ -158,3 +158,79 @@ def test_index_col_names():
     X_t = tf.transform(X)
     assert len(X_t.columns) == 40
     assert X_t.dtypes.all() == np.float64
+
+
+def test_diversity_primitive_output():
+    X = pd.DataFrame(
+        {'diverse': ['This is a very diverse string which does not contain any repeated words at all',
+                     'Here here each each word word is is repeated repeated exactly exactly twice twice',
+                     'A sentence sentence with just a little overlap here and there there there']})
+    tf = TextFeaturizer(text_columns=['diverse'])
+    tf.fit(X)
+
+    expected_features = [1.0, 0.5, 0.75]
+    X_t = tf.transform(X)
+    features = X_t['DIVERSITY_SCORE(diverse)']
+    np.testing.assert_almost_equal(features, expected_features)
+
+
+def test_lsa_primitive_output():
+    X = pd.DataFrame(
+        {'lsa': ['do you hear the people sing? Singing the songs of angry men\n\tIt is the music of a people who will NOT be slaves again!',
+                 'I dreamed a dream in days gone by, when hope was high and life worth living',
+                 'Red, the blood of angry men - black, the dark of ages past']})
+    tf = TextFeaturizer(text_columns=['lsa'])
+    tf.fit(X)
+
+    expected_features = [[0.0200961, 0.002976],
+                         [0.0223392, 0.0058817],
+                         [0.0186072, -0.0006121]]
+    X_t = tf.transform(X)
+    cols = [col for col in X_t.columns if 'LSA' in col]
+    features = X_t[cols]
+    np.testing.assert_almost_equal(features, expected_features, decimal=3)
+
+
+def test_mean_characters_primitive_output():
+    X = pd.DataFrame(
+        {'mean_characters': ['I\'m singing in the rain! Just singing in the rain, what a glorious feeling, I\'m happy again!',
+                             'In sleep he sang to me, in dreams he came... That voice which calls to me, and speaks my name.',
+                             'I\'m gonna be the main event, like no king was before! I\'m brushing up on looking down, I\'m working on my ROAR!']})
+    tf = TextFeaturizer(text_columns=['mean_characters'])
+    tf.fit(X)
+
+    expected_features = [4.11764705882352, 3.45, 3.72727272727]
+    X_t = tf.transform(X)
+    features = X_t['MEAN_CHARACTERS_PER_WORD(mean_characters)']
+    np.testing.assert_almost_equal(features, expected_features)
+
+
+def test_part_of_speech_primitive_output():
+    X = pd.DataFrame(
+        {'part_of_speech': ['do you hear the people sing? Singing the songs of angry men\n\tIt is the music of a people who will NOT be slaves again!',
+                            'I dreamed a dream in days gone by, when hope was high and life worth living',
+                            'Red, the blood of angry men - black, the dark of ages past']})
+    tf = TextFeaturizer(text_columns=['part_of_speech'])
+    tf.fit(X)
+
+    expected_features = [[0, 0, 0, 0, 0, 2, 0, 0, 6, 0, 0, 0, 0, 2, 0],
+                         [0, 0, 0, 0, 1, 2, 0, 0, 3, 0, 0, 0, 0, 3, 0],
+                         [0, 0, 0, 0, 0, 4, 0, 0, 4, 0, 0, 0, 0, 0, 0]]
+    X_t = tf.transform(X)
+    cols = [col for col in X_t.columns if 'PART_OF_SPEECH' in col]
+    features = X_t[cols]
+    np.testing.assert_almost_equal(features, expected_features)
+
+
+def test_polarity_primitive_output():
+    X = pd.DataFrame(
+        {'polarity': ['This is neutral.',
+                      'Everything is bad. Nothing is happy, he hates milk and can\'t stand gross foods, we are being very negative.',
+                      'Everything is awesome! Everything is cool when you\'re part of a team! He loves milk and cookies!']})
+    tf = TextFeaturizer(text_columns=['polarity'])
+    tf.fit(X)
+
+    expected_features = [0.0, -0.214, 0.602]
+    X_t = tf.transform(X)
+    features = X_t['POLARITY_SCORE(polarity)']
+    np.testing.assert_almost_equal(features, expected_features)
