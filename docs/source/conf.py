170bda457343d4befdaac783774e65b48bac4b0c
Dylan Sherry <sharshofski@gmail.com>
2020-07-14 01:46:23 -0400

    Try nameko

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata_sphinx_theme"
+import sphinx_nameko_theme
+html_theme_path = [sphinx_nameko_theme.get_html_theme_path()]
+html_theme = "nameko"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
