cc63b19832e2416f20194234084d767d7e934713
angela97lin <angela97lin@gmail.com>
2020-07-22 18:03:14 -0400

    fix test

--- evalml/tests/pipeline_tests/regression_pipeline_tests/test_regression.py
+++ evalml/tests/pipeline_tests/regression_pipeline_tests/test_regression.py
@@ -9,12 +9,6 @@ def test_invalid_targets_regression_pipeline(target_type, dummy_regression_pipel
     X, y = load_wine()
     if target_type == "categorical":
         y = pd.Categorical(y)
-    elif target_type == "int":
-        unique_vals = y.unique()
-        y = y.map({unique_vals[i]: int(i) for i in range(len(unique_vals))})
-    elif target_type == "float":
-        unique_vals = y.unique()
-        y = y.map({unique_vals[i]: float(i) for i in range(len(unique_vals))})
     if target_type == "bool":
         y = y.map({"malignant": False, "benign": True})
         X, y = load_breast_cancer()
