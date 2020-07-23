66380cea3252a4669c92c6a154bff1ce00e9506b
freddyaboulton <alfonsoboulton@gmail.com>
2020-07-23 19:51:09 -0400

    Adding testing for user interface.

--- evalml/pipelines/explanations/_explainers.py
+++ evalml/pipelines/explanations/_explainers.py
@@ -1,5 +1,10 @@
-from evalml.pipelines.explanations._algorithms import _compute_shap_values, _normalize_shap_values
-from evalml.pipelines.explanations._user_interface import _make_single_prediction_table
+from evalml.pipelines.explanations._algorithms import (
+    _compute_shap_values,
+    _normalize_shap_values
+)
+from evalml.pipelines.explanations._user_interface import (
+    _make_single_prediction_table
+)
 
 
 def _explain_with_shap_values(pipeline, features, top_k=3, training_data=None, include_shap_values=False):
@@ -8,8 +13,11 @@ def _explain_with_shap_values(pipeline, features, top_k=3, training_data=None, i
     Arguments:
         pipeline (PipelineBase): Trained pipeline whose predictions we want to explain with SHAP.
         features (pd.DataFrame): Dataframe of features - needs to correspond to data the pipeline was fit on.
+        top_k (int): How many of the highest/lowest features to include in the table.
         training_data (pd.DataFrame): Training data the pipeline was fit on.
             For non-tree estimators, we need a sample of training data for the KernelSHAP algorithm.
+        include_shap_values (bool): Whether the SHAP values should be included in an extra column in the output.
+
     Returns:
         None: Displays a table to standard out
     """
@@ -18,3 +26,7 @@ def _explain_with_shap_values(pipeline, features, top_k=3, training_data=None, i
     normalized_shap_values = _normalize_shap_values(shap_values)
     return _make_single_prediction_table(shap_values, normalized_shap_values, top_k, include_shap_values)
 
+
+def _explain_prediction(pipeline, features, training_data=None):
+    print(_explain_with_shap_values(pipeline, features, training_data=training_data, top_k=3,
+                                    include_shap_values=False))
--- evalml/pipelines/explanations/_user_interface.py
+++ evalml/pipelines/explanations/_user_interface.py
@@ -2,10 +2,22 @@ from texttable import Texttable
 
 
 def _make_rows(shap_values, normalized_values, top_k, include_shap_values=False):
+    """Makes the rows (one row for each feature) for the SHAP table.
+
+    Arguments:
+        shap_values (dict): Dictionary mapping the feature names to their SHAP values. In a multiclass setting,
+            this dictionary for correspond to the SHAP values for a single class.
+        normalized_values (dict): Normalized SHAP values. Same structure as shap_values parameter.
+        top_k (int): How many of the highest/lowest features to include in the table.
+        include_shap_values (bool): Whether to include the SHAP values in their own column.
+
+    Returns:
+          list(str)
+    """
     tuples = [(value[0], feature_name) for feature_name, value in normalized_values.items()]
     tuples = sorted(tuples)
 
-    if len(tuples) <= 2*top_k:
+    if len(tuples) <= 2 * top_k:
         features_to_display = reversed(tuples)
     else:
         features_to_display = tuples[-top_k:][::-1] + tuples[:top_k][::-1]
@@ -23,6 +35,20 @@ def _make_rows(shap_values, normalized_values, top_k, include_shap_values=False)
 
 
 def _make_table(dtypes, alignment, shap_values, normalized_values, top_k, include_shap_values=False):
+    """Make a table displaying the SHAP values for a prediction.
+
+    Arguments:
+        dtypes (list(str)): The dtypes of each column in the table.
+        alignment (list(str)): How the data in each column in the table should be aligned.
+        shap_values (dict): Dictionary mapping the feature names to their SHAP values. In a multiclass setting,
+            this dictionary for correspond to the SHAP values for a single class.
+        normalized_values (dict): Normalized SHAP values. Same structure as shap_values parameter.
+        top_k (int): How many of the highest/lowest features to include in the table.
+        include_shap_values (bool): Whether to include the SHAP values in their own column.
+
+    Returns:
+        str
+    """
     table = Texttable()
     table.set_deco(Texttable.HEADER)
     table.set_cols_dtype(dtypes)
@@ -39,12 +65,17 @@ def _make_table(dtypes, alignment, shap_values, normalized_values, top_k, includ
 
 
 def _make_single_prediction_table(shap_values, normalized_values, top_k=3, include_shap_values=False):
-    """Makes a table from the normalized prediction explanation values for a single prediction.
+    """Makes a table from the SHAP values for a single prediction.
 
     Arguments:
-        normalized_values (dict): Dictionary mapping a feature name to a one-element list
-            containing its scaled value.
+        shap_values (list(dict) or dict): Dictionary mapping a feature name to a one-element list
+            containing its scaled value. In classification problems, the input will be a list storing a dict for
+            each class.
+        normalized_values (list(dict) or dict): Dictionary mapping a feature name to a one-element list
+            containing its scaled value. In classification problems, the input will be a list storing a dict for
+            each class.
         top_k (int): Will include the top_k highest and lowest features in the table.
+        include_shap_values (bool): Whether the SHAP values should be included as an extra column in the table.
 
     Returns:
         str
@@ -56,12 +87,15 @@ def _make_single_prediction_table(shap_values, normalized_values, top_k=3, inclu
         dtypes.append("f")
         alignment.append("c")
 
+    # Classification
     if isinstance(shap_values, list):
+        # Binary Classification
         if len(shap_values) == 2:
             strings = ["Positive Label\n"]
             table = _make_table(dtypes, alignment, shap_values[1], normalized_values[1], top_k, include_shap_values)
             strings += table.splitlines()
             return "\n".join(strings)
+        # Multiclass
         else:
             strings = []
             for class_index, (class_values, normalized_class_values) in enumerate(zip(shap_values, normalized_values)):
@@ -70,5 +104,6 @@ def _make_single_prediction_table(shap_values, normalized_values, top_k=3, inclu
                 strings += table.splitlines()
                 strings.append("\n")
             return "\n".join(strings)
+    # Regression
     else:
         return _make_table(dtypes, alignment, shap_values, normalized_values, top_k, include_shap_values)
