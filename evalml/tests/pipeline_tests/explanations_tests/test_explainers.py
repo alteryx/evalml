ab1e29582f89f7163d485c53d6d2fc3523bba98e
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2020-07-29 12:32:39 -0400

    Misc edits to `explain_prediction` documentation (#992)
    * Making clear in the user guide and api reference that Catboost/XGBoost are not fully supported.
    
    * Updating the release notes with the changes for PR 992.
    
    * Moving sentence about not supporting XGBoost and CatBoost to docstring.

--- evalml/tests/pipeline_tests/explanations_tests/test_explainers.py
+++ evalml/tests/pipeline_tests/explanations_tests/test_explainers.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from evalml.pipelines.prediction_explanations._explainers import (
-    _explain_prediction
+from evalml.pipelines.prediction_explanations.explainers import (
+    explain_prediction
 )
 
 test_features = [5, [1], np.ones((1, 15)), pd.DataFrame({"a": [1, 2, 3], "b": [1, 2, 3]}).iloc[0],
@@ -13,19 +13,19 @@ test_features = [5, [1], np.ones((1, 15)), pd.DataFrame({"a": [1, 2, 3], "b": [1
 
 
 @pytest.mark.parametrize("test_features", test_features)
-@patch("evalml.pipelines.prediction_explanations._explainers._compute_shap_values")
-@patch("evalml.pipelines.prediction_explanations._explainers._normalize_shap_values")
+@patch("evalml.pipelines.prediction_explanations.explainers._compute_shap_values")
+@patch("evalml.pipelines.prediction_explanations.explainers._normalize_shap_values")
 def test_explain_prediction_value_error(mock_normalize_shap_values, mock_compute_shap_values, test_features):
     with pytest.raises(ValueError, match="features must be stored in a dataframe of one row."):
-        _explain_prediction(None, input_features=test_features, training_data=None)
+        explain_prediction(None, input_features=test_features, training_data=None)
 
 
-@patch("evalml.pipelines.prediction_explanations._explainers._compute_shap_values", return_value={"a": [1], "b": [-2],
-                                                                                                  "c": [-0.25], "d": [2]})
-@patch("evalml.pipelines.prediction_explanations._explainers._normalize_shap_values", return_value={"a": [0.5],
-                                                                                                    "b": [-0.75],
-                                                                                                    "c": [-0.25],
-                                                                                                    "d": [0.75]})
+@patch("evalml.pipelines.prediction_explanations.explainers._compute_shap_values", return_value={"a": [1], "b": [-2],
+                                                                                                 "c": [-0.25], "d": [2]})
+@patch("evalml.pipelines.prediction_explanations.explainers._normalize_shap_values", return_value={"a": [0.5],
+                                                                                                   "b": [-0.75],
+                                                                                                   "c": [-0.25],
+                                                                                                   "d": [0.75]})
 def test_explain_prediction_runs(mock_normalize_shap_values, mock_compute_shap_values):
 
     answer = """Feature Name   Contribution to Prediction
@@ -37,7 +37,7 @@ def test_explain_prediction_runs(mock_normalize_shap_values, mock_compute_shap_v
 
     pipeline = MagicMock()
     features = pd.DataFrame({"a": [1], "b": [2]})
-    table = _explain_prediction(pipeline, features).splitlines()
+    table = explain_prediction(pipeline, features).splitlines()
 
     assert len(table) == len(answer)
     for row, row_answer in zip(table, answer):
