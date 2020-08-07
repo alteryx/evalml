b3e1aaa23ac7c4dcc98b4008fbe2f8a77052bd65
freddyaboulton <alfonsoboulton@gmail.com>
2020-08-07 17:46:54 -0400

    Adding docstring for _make_single_prediction_shap_table.

--- evalml/pipelines/prediction_explanations/_user_interface.py
+++ evalml/pipelines/prediction_explanations/_user_interface.py
@@ -116,6 +116,20 @@ class _SHAPMultiClassTableMaker(TableMaker):
 
 def _make_single_prediction_shap_table(pipeline, input_features, top_k=3, training_data=None,
                                        include_shap_values=False):
+    """Creates table summarizing the top_k positive and top_k negative contributing features to the prediction of a single datapoint.
+
+    Arguments:
+        pipeline (PipelineBase): Fitted pipeline whose predictions we want to explain with SHAP.
+        input_features (pd.DataFrame): Dataframe of features - needs to correspond to data the pipeline was fit on.
+        top_k (int): How many of the highest/lowest features to include in the table.
+        training_data (pd.DataFrame): Training data the pipeline was fit on.
+            This is required for non-tree estimators because we need a sample of training data for the KernelSHAP algorithm.
+        include_shap_values (bool): Whether the SHAP values should be included in an extra column in the output.
+            Default is False.
+
+    Returns:
+        str: Table
+    """
     if not (isinstance(input_features, pd.DataFrame) and input_features.shape[0] == 1):
         raise ValueError("features must be stored in a dataframe of one row.")
 
