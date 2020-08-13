56b7ee5c2f7877f518995ef10020d8a4fa016fa5
freddyaboulton <alfonsoboulton@gmail.com>
2020-08-13 17:03:32 -0400

    Fixing lint.

--- evalml/tests/test_all_test_dirs_included.py
+++ evalml/tests/test_all_test_dirs_included.py
@@ -1,5 +1,6 @@
-from setuptools import find_packages
 import os
+
+from setuptools import find_packages
 
 
 def test_all_test_dirs_included():
@@ -8,4 +9,3 @@ def test_all_test_dirs_included():
     all_test_dirs_with_init_files = [module for module in all_modules if "evalml.tests" in module]
     all_test_dirs = [dirname for dirname, _, files in os.walk(test_dir) if "__pycache__" not in dirname and "test" in os.path.split(dirname)[1]]
     assert len(all_test_dirs) == len(all_test_dirs_with_init_files)
-
