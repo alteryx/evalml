1055b4a1d866ecc9e09d0bf2584a8d88dd166731
bchen1116 <bryan.chen@alteryx.com>
2020-08-24 15:25:07 -0400

    update test components

--- evalml/tests/component_tests/test_components.py
+++ evalml/tests/component_tests/test_components.py
@@ -137,9 +137,11 @@ def test_describe_component():
         assert xgb_classifier.describe(return_dict=True) == {'name': 'XGBoost Classifier', 'parameters': {'eta': 0.1, 'max_depth': 3, 'min_child_weight': 1, 'n_estimators': 75}}
     except ImportError:
         pass
-    with pytest.raises(ImportError):
+    try:
         lg_classifier = LightGBMClassifier()
         assert lg_classifier.describe(return_dict=True) == {'name': 'LightGBM Classifier', 'parameters': {'boosting_type': 'gbdt', 'learning_rate': 0.1, 'n_estimators': 100, 'max_depth': 0, 'num_leaves': 31, 'min_child_samples': 20, 'n_jobs': -1}}
+    except ImportError:
+        pass
 
 
 def test_missing_attributes(X_y_binary):
