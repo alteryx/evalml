968a04dea19fe3ea273610d0a16492ec300ac17a
bchen1116 <bryan.chen@alteryx.com>
2020-09-04 13:09:56 -0400

    change wording

--- evalml/data_checks/class_imbalance_data_check.py
+++ evalml/data_checks/class_imbalance_data_check.py
@@ -25,7 +25,7 @@ class ClassImbalanceDataCheck(DataCheck):
             >>> y = pd.Series([0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
             >>> threshold = 0.10
             >>> target_check = ClassImbalanceDataCheck()
-            >>> assert target_check.validate(X, y, threshold) == [DataCheckWarning("Label 0 makes up 9.091% of the target data, which is below the acceptable threshold of 10%", "ClassImbalanceDataCheck")]
+            >>> assert target_check.validate(X, y, threshold) == [DataCheckWarning("Label 0 makes up 9.091% of the target data, which is below the recommended threshold of 10%", "ClassImbalanceDataCheck")]
         """
         if threshold <= 0 or threshold > 0.5:
             raise ValueError("Provided threshold {} is not within the range (0, 0.5]".format(threshold))
@@ -37,7 +37,7 @@ class ClassImbalanceDataCheck(DataCheck):
         below_threshold = counts.where(counts < threshold).dropna()
         # if there are items that occur less than the threshold, add them to the list of messages
         if len(below_threshold):
-            warning_msg = "Label {0:.0f} makes up {1:.3f}% of the target data, which is below the acceptable threshold of {2:.0f}%"
+            warning_msg = "Label {0:.0f} makes up {1:.3f}% of the target data, which is below the recommended threshold of {2:.0f}%"
             for index, value in below_threshold.items():
                 messages.append(DataCheckWarning(warning_msg.format(index, value * 100, threshold * 100), self.name))
 
