980d36027fdc09ecdba2df68edd576859780c25c
bchen1116 <bryan.chen@alteryx.com>
2020-09-08 17:32:06 -0400

    add tests

--- evalml/tests/automl_tests/test_pipeline_search_plots.py
+++ evalml/tests/automl_tests/test_pipeline_search_plots.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pandas as pd
 import pytest
 
@@ -53,3 +55,47 @@ def test_search_iteration_plot_class():
     assert isinstance(plot, SearchIterationPlot)
     assert x == [0, 1, 2]
     assert y == [0.60, 0.75, 0.50]
+
+
+@patch('evalml.model_understanding.graphs.jupyter_check')
+@patch('evalml.model_understanding.graphs.import_or_raise')
+def test_jupyter(import_check, jupyter_check):
+    class MockObjective:
+        def __init__(self):
+            self.name = 'Test Objective'
+            self.greater_is_better = True
+
+    class MockResults:
+        def __init__(self):
+            self.objective = MockObjective()
+            self.results = {
+                'pipeline_results': {
+                    2: {
+                        'score': 0.50
+                    },
+                    0: {
+                        'score': 0.60
+                    },
+                    1: {
+                        'score': 0.75
+                    },
+                },
+                'search_order': [1, 2, 0]
+            }
+            self.rankings = pd.DataFrame({
+                'score': [0.75, 0.60, 0.50]
+            })
+
+    mock_data = MockResults()
+
+    pytest.importorskip('plotly.graph_objects', reason='Skipping plotting test because plotly not installed')
+    jupyter_check.return_value = True
+    with pytest.warns(None) as graph_valid:
+        SearchIterationPlot(mock_data)
+        assert len(graph_valid) == 0
+        import_check.assert_called_with('ipywidgets', warning=True)
+
+    jupyter_check.return_value = False
+    with pytest.warns(None) as graph_valid:
+        SearchIterationPlot(mock_data)
+        assert len(graph_valid) == 0
