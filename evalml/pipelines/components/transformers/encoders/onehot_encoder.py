0e413d6f3ec2ef719d1a2843dd11f9695eae3ccd
freddyaboulton <alfonsoboulton@gmail.com>
2020-09-08 16:40:06 -0400

    Not using reindex.

--- evalml/pipelines/components/transformers/encoders/onehot_encoder.py
+++ evalml/pipelines/components/transformers/encoders/onehot_encoder.py
@@ -131,12 +131,13 @@ class OneHotEncoder(CategoricalEncoder):
         for col in X.columns:
             if col not in cat_cols:
                 X_t = pd.concat([X_t, X[col]], axis=1)
-        # The call to pd.concat below changes the type of the index. We call reindex in order to keep it the same.
-        X_t = X_t.reindex(X.index)
+        # The call to pd.concat above changes the type of the index so we will manually keep it the same.
+        if not X_t.empty:
+            X_t.index = X.index
 
         # Call sklearn's transform on the categorical columns
         if len(cat_cols) > 0:
-            X_cat = pd.DataFrame(self._encoder.transform(X[cat_cols]).toarray(), index=X_t.index)
+            X_cat = pd.DataFrame(self._encoder.transform(X[cat_cols]).toarray(), index=X.index)
             cat_cols_str = [str(c) for c in cat_cols]
             X_cat.columns = self._encoder.get_feature_names(input_features=cat_cols_str)
             X_t = pd.concat([X_t, X_cat], axis=1)
