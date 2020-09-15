6760c244d3c8d6435bb8e9d732ca3063cbc6abd8
Angela Lin <angela97lin@gmail.com>
2020-09-15 12:32:11 -0400

    fix model family updated test, still need to update for kwargs

--- evalml/tests/model_family_tests/test_model_family.py
+++ evalml/tests/model_family_tests/test_model_family.py
@@ -5,7 +5,7 @@ from evalml.model_family import ModelFamily, handle_model_family
 
 @pytest.fixture
 def correct_model_families():
-    correct_model_families = [ModelFamily.LINEAR_MODEL, ModelFamily.LIGHTGBM, ModelFamily.RANDOM_FOREST, ModelFamily.XGBOOST, ModelFamily.CATBOOST, ModelFamily.EXTRA_TREES, ModelFamily.BASELINE, ModelFamily.NONE]
+    correct_model_families = [ModelFamily.LINEAR_MODEL, ModelFamily.LIGHTGBM, ModelFamily.RANDOM_FOREST, ModelFamily.XGBOOST, ModelFamily.CATBOOST, ModelFamily.EXTRA_TREES, ModelFamily.BASELINE, ModelFamily.ENSEMBLE, ModelFamily.NONE]
     yield correct_model_families
 
 
