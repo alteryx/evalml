654549cdd833c882c62670dff6d1c92102182036
bchen1116 <bryan.chen@alteryx.com>
2020-09-21 15:48:29 -0400

    fix name

--- evalml/problem_types/problem_types.py
+++ evalml/problem_types/problem_types.py
@@ -3,12 +3,12 @@ from enum import Enum
 
 class ProblemTypes(Enum):
     """Enum for type of machine learning problem: BINARY, MULTICLASS, or REGRESSION."""
-    BINARY = 'binary classification'
-    MULTICLASS = 'multiclass classification'
+    BINARY = 'binary'
+    MULTICLASS = 'multiclass'
     REGRESSION = 'regression'
 
     def __str__(self):
-        problem_type_dict = {ProblemTypes.BINARY.name: "Binary Classification",
-                             ProblemTypes.MULTICLASS.name: "Multiclass Classification",
+        problem_type_dict = {ProblemTypes.BINARY.name: "Binary",
+                             ProblemTypes.MULTICLASS.name: "Multiclass",
                              ProblemTypes.REGRESSION.name: "Regression"}
         return problem_type_dict[self.name]
