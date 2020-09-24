6b0f75d8650a9177d7e0718052ab834ac5a1b9cb
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2020-09-24 18:57:51 -0400

    Adding some new kwargs to Catboost and LogisticRegression (#1202)
    * Adding allow_writing_files to Catboost init and solver and multiclass to logitistic regression init.
    
    * Adding PR 1202 to release notes.

--- evalml/pipelines/components/estimators/regressors/catboost_regressor.py
+++ evalml/pipelines/components/estimators/regressors/catboost_regressor.py
@@ -1,5 +1,4 @@
 import copy
-import warnings
 
 import pandas as pd
 from skopt.space import Integer, Real
@@ -31,16 +30,14 @@ class CatBoostRegressor(Estimator):
     SEED_MAX = SEED_BOUNDS.max_bound
 
     def __init__(self, n_estimators=10, eta=0.03, max_depth=6, bootstrap_type=None, silent=False,
-                 random_state=0, **kwargs):
+                 allow_writing_files=False, random_state=0, **kwargs):
         random_seed = get_random_seed(random_state, self.SEED_MIN, self.SEED_MAX)
         parameters = {"n_estimators": n_estimators,
                       "eta": eta,
                       "max_depth": max_depth,
                       'bootstrap_type': bootstrap_type,
-                      'silent': silent}
-        if kwargs.get('allow_writing_files', False):
-            warnings.warn("Parameter allow_writing_files is being set to False in CatBoostRegressor")
-        kwargs["allow_writing_files"] = False
+                      'silent': silent,
+                      'allow_writing_files': allow_writing_files}
         parameters.update(kwargs)
 
         cb_error_msg = "catboost is not installed. Please install using `pip install catboost.`"
