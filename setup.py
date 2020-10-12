74c681c9ca39163a32261e293a4a62443c5752ba
angela97lin <angela97lin@gmail.com>
2020-10-12 17:26:38 -0400

    testing

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     },
     data_files=[('evalml/demos/data', ['evalml/demos/data/fraud_transactions.csv.tar.gz', 'evalml/demos/data/churn.csv']),
                 ('evalml/tests/data', ['evalml/tests/data/tips.csv', 'evalml/tests/data/titanic.csv']),
-                ('', ['core-requirements.txt'])],
+                ],
         
-    # package_data = {
-    #     '': ['../core-requirements.txt'],
-    # },
+    package_data = {
+        '': ['../core-requirements.txt'],
+    },
 )
