18af4f328bbca396d5b7b5dbeb247b71f41fe9a1
angela97lin <angela97lin@gmail.com>
2020-10-12 16:30:42 -0400

    more testing

--- evalml/utils/cli_utils.py
+++ evalml/utils/cli_utils.py
@@ -16,24 +16,12 @@ logger = get_logger(__file__)
 
 
 def get_core_requirements():
-    reqs_path = os.path.join(os.path.dirname(evalml.__file__), 'core-requirements.txt')
+    reqs_path = os.path.join(os.path.dirname(evalml.__file__), '../core-requirements.txt')
     lines = open(reqs_path, 'r').readlines()
     lines = [line for line in lines if '-r ' not in line]
     reqs = requirements.parse(''.join(lines))
     reqs_names = [req.name for req in reqs]
     return reqs_names
-# import importlib_metadata
-# from importlib_metadata import files
-# # util = [p for p in files('evalml') if 'core-requirements.txt' in str(p)]
-# # util[0].read_text()
-# def get_core_requirements():
-
-#     util = [p for p in files('evalml') if 'core-requirements.txt' in str(p)]
-#     a = util[0].read_text().splitlines()
-#     l = []
-#     for b in a:
-#         l.append(b.partition(">")[0]);
-#     return l
 
 def print_info():
     """Prints information about the system, evalml, and dependencies of evalml.
