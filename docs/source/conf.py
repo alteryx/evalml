5ca5d2236aae2cb66de3d49e8fa12c309d1cc738
Dylan Sherry <sharshofski@gmail.com>
2020-10-19 19:31:06 -0400

    Remove unused sphinx extlinks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -202,11 +202,9 @@ epub_exclude_files = ['search.html']
 
 # If evalml is open-sourced: replace github specific style.css
 extlinks = {
-    "github_url": ("https://github.com/alteryx/evalml", "#"),
-    "twitter_url": ("https://twitter.com/alteryx?lang=en", "#"),
-    "issue": ("https://github.com/alteryx/evalml/issues/%s", "#"),
-    "pr": ("https://github.com/alteryx/evalml/pull/%s", "#"),
-    "user": ("https://github.com/%s", "@")
+    'issue': ("https://github.com/alteryx/evalml/issues/%s", "#"),
+    'pr': ("https://github.com/alteryx/evalml/pull/%s", "#"),
+    'user': ("https://github.com/%s", "@")
 }
 
 autosummary_generate = ["api_reference.rst"]
