df91116611d7a31196a11c6681925733bf0ca099
Dylan Sherry <sharshofski@gmail.com>
2020-10-19 19:32:59 -0400

    Remove unneeded change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -154,7 +154,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'EvalML.tex', 'EvalML Documentation',
-     'Alteryx, Inc.', 'manual'),
+     author, 'manual'),
 ]
 
 
