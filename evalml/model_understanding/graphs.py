04774de939e7817ba44cac4df6be1f6c17cbd6a4
bchen1116 <bryan.chen@alteryx.com>
2020-10-27 17:45:40 -0400

    clean up code:

--- evalml/model_understanding/graphs.py
+++ evalml/model_understanding/graphs.py
@@ -437,8 +437,7 @@ def partial_dependence(pipeline, X, feature, grid_resolution=100):
     elif isinstance(pipeline, evalml.pipelines.RegressionPipeline):
         pipeline._estimator_type = "regressor"
     pipeline.feature_importances_ = pipeline.feature_importance
-    if ((isinstance(feature, int) and X.iloc[:, feature].isnull().sum()) or
-        (isinstance(feature, str) and X[feature].isnull().sum())):
+    if ((isinstance(feature, int) and X.iloc[:, feature].isnull().sum()) or (isinstance(feature, str) and X[feature].isnull().sum())):
         warnings.warn("There are null values in the features, which will cause NaN values in the partial dependence output. Fill in these values to remove the NaN values.", NullsInColumnWarning)
     try:
         avg_pred, values = sk_partial_dependence(pipeline, X=X, features=[feature], grid_resolution=grid_resolution)
