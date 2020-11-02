a7d5e4d1166bce0daa2d0fb76715e892be4dcab6
freddyaboulton <alfonsoboulton@gmail.com>
2020-11-02 15:04:33 -0500

    Mapping tests back to design document examples. Not using lap to name columns.

--- evalml/pipelines/components/transformers/preprocessing/time_series.py
+++ evalml/pipelines/components/transformers/preprocessing/time_series.py
@@ -5,15 +5,14 @@ from evalml.pipelines.components.transformers.transformer import Transformer
 
 class DelayedFeaturesTransformer(Transformer):
     name = "Delayed Features Transformer"
-    needs_time_series_parameters = True
+    hyperparameter_ranges = {}
 
-    def __init__(self, max_lag=2, gap=1, random_state=0, **kwargs):
+    def __init__(self, max_delay=2, random_state=0, **kwargs):
 
-        parameters = {"max_lag": max_lag, "gap": gap}
+        parameters = {"max_delay": max_delay}
         parameters.update(kwargs)
         super().__init__(parameters=parameters, random_state=random_state)
-        self.max_lag = max_lag
-        self.gap = gap
+        self.max_delay = max_delay
 
     def fit(self, X, y=None):
         """Fits the LaggedFeatureExtractor."""
@@ -21,19 +20,22 @@ class DelayedFeaturesTransformer(Transformer):
     def transform(self, X, y=None):
         """Transforms the input data."""
         if not isinstance(X, pd.DataFrame):
-            X = pd.DataFrame(X)
+            if y is None:
+                X = pd.DataFrame(X, columns=["target"])
+            else:
+                X = pd.DataFrame(X)
         if y is not None and not isinstance(y, pd.Series):
             y = pd.Series(y)
 
         original_columns = X.columns
-        X = X.assign(**{f"{col}_lag_{self.gap + t}": X[col].shift(t)
-                        for t in range(self.max_lag + 1)
+        X = X.assign(**{f"{col}_delay_{t}": X[col].shift(t)
+                        for t in range(self.max_delay + 1)
                         for col in X})
         X.drop(columns=original_columns, inplace=True)
 
         # Handle cases where the label was not passed
         if y is not None:
-            X = X.assign(**{f"target_lag_{self.gap + t}": y.shift(t)
-                            for t in range(self.max_lag + 1)})
+            X = X.assign(**{f"target_delay_{t}": y.shift(t)
+                            for t in range(self.max_delay + 1)})
 
         return X
--- evalml/tests/component_tests/test_delayed_features_transformer.py
+++ evalml/tests/component_tests/test_delayed_features_transformer.py
@@ -1,18 +1,114 @@
 import pandas as pd
-
+import pytest
 from evalml.pipelines import DelayedFeaturesTransformer
 
 
-def test_lagged_feature_extractor():
-
+@pytest.fixture
+def delayed_features_data():
     X = pd.DataFrame({"feature": range(1, 32)})
     y = pd.Series(range(1, 32))
+    return X, y
 
-    answer = pd.DataFrame({"feature_lag_7": X.feature,
-                           "feature_lag_8": X.feature.shift(1),
-                           "feature_lag_9": X.feature.shift(2),
-                           "target_lag_7": y,
-                           "target_lag_8": y.shift(1),
-                           "target_lag_9": y.shift(2)})
 
-    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_lag=2, gap=7).fit_transform(X, y), answer)
+def test_delayed_features_transformer_init():
+
+    delayed_features = DelayedFeaturesTransformer(max_delay=4, random_state=1)
+    assert delayed_features.parameters == {"max_delay": 4}
+
+
+def test_lagged_feature_extractor_maxdelay3_gap1(delayed_features_data):
+    X, y = delayed_features_data
+
+    # Example 1 from the design document
+    answer = pd.DataFrame({"feature_delay_0": X.feature,
+                           "feature_delay_1": X.feature.shift(1),
+                           "feature_delay_2": X.feature.shift(2),
+                           "feature_delay_3": X.feature.shift(3),
+                           "target_delay_0": y,
+                           "target_delay_1": y.shift(1),
+                           "target_delay_2": y.shift(2),
+                           "target_delay_3": y.shift(3)})
+
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=3, gap=1).fit_transform(X, y), answer)
+
+    answer_only_y = pd.DataFrame({"target_delay_0": y,
+                                  "target_delay_1": y.shift(1),
+                                  "target_delay_2": y.shift(2),
+                                  "target_delay_3": y.shift(3)})
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=3, gap=1).fit_transform(y), answer_only_y)
+
+
+def test_lagged_feature_extractor_maxdelay5_gap1(delayed_features_data):
+
+    X, y = delayed_features_data
+
+    # Example 2 from the design document - Note that min_delay is not supported yet.
+    answer = pd.DataFrame({"feature_delay_0": X.feature,
+                           "feature_delay_1": X.feature.shift(1),
+                           "feature_delay_2": X.feature.shift(2),
+                           "feature_delay_3": X.feature.shift(3),
+                           "feature_delay_4": X.feature.shift(4),
+                           "feature_delay_5": X.feature.shift(5),
+                           "target_delay_0": y,
+                           "target_delay_1": y.shift(1),
+                           "target_delay_2": y.shift(2),
+                           "target_delay_3": y.shift(3),
+                           "target_delay_4": y.shift(4),
+                           "target_delay_5": y.shift(5)})
+
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=5, gap=1).fit_transform(X, y), answer)
+
+    answer_only_y = pd.DataFrame({"target_delay_0": y,
+                                  "target_delay_1": y.shift(1),
+                                  "target_delay_2": y.shift(2),
+                                  "target_delay_3": y.shift(3),
+                                  "target_delay_4": y.shift(4),
+                                  "target_delay_5": y.shift(5)})
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=5, gap=1).fit_transform(y), answer_only_y)
+
+
+def test_lagged_feature_extractor_maxdelay3_gap7(delayed_features_data):
+
+    X, y = delayed_features_data
+
+    # Example 3 from the design document
+    answer = pd.DataFrame({"feature_delay_0": X.feature,
+                           "feature_delay_1": X.feature.shift(1),
+                           "feature_delay_2": X.feature.shift(2),
+                           "feature_delay_3": X.feature.shift(3),
+                           "target_delay_0": y,
+                           "target_delay_1": y.shift(1),
+                           "target_delay_2": y.shift(2),
+                           "target_delay_3": y.shift(3)})
+
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=3, gap=7).fit_transform(X, y), answer)
+
+    answer_only_y = pd.DataFrame({"target_delay_0": y,
+                                  "target_delay_1": y.shift(1),
+                                  "target_delay_2": y.shift(2),
+                                  "target_delay_3": y.shift(3)})
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=3, gap=7).fit_transform(y), answer_only_y)
+
+
+def test_lagged_feature_extractor_numpy(delayed_features_data):
+    X, y = delayed_features_data
+    X_np = X.values
+    y_np = y.values
+
+    # Example 3 from the design document
+    answer = pd.DataFrame({"0_delay_0": X.feature,
+                           "0_delay_1": X.feature.shift(1),
+                           "0_delay_2": X.feature.shift(2),
+                           "0_delay_3": X.feature.shift(3),
+                           "target_delay_0": y,
+                           "target_delay_1": y.shift(1),
+                           "target_delay_2": y.shift(2),
+                           "target_delay_3": y.shift(3)})
+
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=3, gap=7).fit_transform(X_np, y_np), answer)
+
+    answer_only_y = pd.DataFrame({"target_delay_0": y,
+                                  "target_delay_1": y.shift(1),
+                                  "target_delay_2": y.shift(2),
+                                  "target_delay_3": y.shift(3)})
+    pd.testing.assert_frame_equal(DelayedFeaturesTransformer(max_delay=3, gap=7).fit_transform(y_np), answer_only_y)
