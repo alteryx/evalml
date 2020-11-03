d202699e63349383f4c2a08d02b5d3d32d554ac1
Angela Lin <angela97lin@gmail.com>
2020-11-03 14:30:42 -0500

    some cleanup, add to api ref

--- evalml/automl/callbacks.py
+++ evalml/automl/callbacks.py
@@ -7,27 +7,28 @@ def silent_error_callback(automl, exception):
     """No-op"""
     pass
 
-
 def log_error_callback(automl, exception):
-    """doesn't throw, but logs. default behavior. not sure if defining this here will mess things up"""
-    logger.error(f'AutoML search encountered an exception: {str(exception)}')
+    """Logs the exception thrown as an error. Will not throw. This is the default behavior for AutoMLSearch.
+    not sure if defining this here will mess things up"""
+    logger.error(f'AutoMLSearch encountered an exception: {str(exception)}')
 
 
 def raise_error_callback(automl, exception):
-    """throws"""
-    logger.error(f'AutoML search got fatal exception: {str(exception)}')
+    """Raises the exception thrown by the AutoMLSearch object. Also logs the exception as an error."""
+    logger.error(f'AutoMLSearch raised a fatal exception: {str(exception)}')
     raise exception
 
 
 def log_and_save_error_callback(automl, exception):
-    """doesn't throw, but adds exception to a list in results"""
+    """Logs the exception thrown by the AutoMLSearch object as a warning
+        and adds the exception to the 'errors' list in AutoMLSearch object results."""
     logger.warning(f'AutoML search encountered an exception: {str(exception)}')
-    automl._results['errors'] = automl._results.get('errors', [])
-    automl._results['errors'].append(exception)
+    # automl._results['errors'] = automl._results.get('errors', [])
+    # automl._results['errors'].append(exception)
 
-
-def raise_and_save_error(automl, exception):
-    """save exception and throw"""
-    logger.warning(f'AutoML search encountered an exception: {str(exception)}')
-    automl._results['errors'] = [exception]
+def raise_and_save_error_callback(automl, exception):
+    """Raises the exception thrown by the AutoMLSearch object, logs it as an error,
+        and adds the exception to the 'errors' list in AutoMLSearch object results."""
+    logger.error(f'AutoMLSearch raised a fatal exception: {str(exception)}')
+    # automl._results['errors'] = [exception]
     raise exception
