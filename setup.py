c5911392f164a0adebace6c3b6606fac91d0cfb6
freddyaboulton <alfonsoboulton@gmail.com>
2020-11-12 11:13:08 -0500

    Refactoring cli to print all dependencies.

--- setup.py
+++ setup.py
@@ -23,7 +23,4 @@ setup(
     },
     data_files=[('evalml/demos/data', ['evalml/demos/data/fraud_transactions.csv.tar.gz', 'evalml/demos/data/churn.csv']),
                 ('evalml/tests/data', ['evalml/tests/data/tips.csv', 'evalml/tests/data/titanic.csv'])],
-    package_data = {
-        '': ['../core-requirements.txt'],
-    },
 )
