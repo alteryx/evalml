f1bc8c1fcdba345377603fab1162a912398310b4
freddyaboulton <alfonsoboulton@gmail.com>
2020-11-16 13:09:12 -0500

    Adding a time series split class.

---
+++ evalml/automl/data_splitters/time_series_split.py
@@ -0,0 +1,75 @@
+import numpy as np
+from sklearn.model_selection import TimeSeriesSplit as SkTimeSeriesSplit
+from sklearn.model_selection._split import BaseCrossValidator
+
+
+class TimeSeriesSplit(BaseCrossValidator):
+    """Rolling Origin Cross Validation for time series problems."""
+
+    def __init__(self, max_delay=0, gap=0, n_folds=3):
+        """Create a TimeSeriesSplit.
+
+        This class uses max_delay and gap values to take into account that evalml time series pipelines perform
+        some feature and target engineering, e.g delaying input features and shifting the target variable by the
+        desired amount. If the data that will be split already has all the features and appropriate target values, and
+        then set max_delay and gap to 0.
+
+        Arguments:
+            max_delay (int): Max delay value for feature engineering. Time series pipelines create delayed features
+                from existing features. This process will introduce NaNs into the first max_delay number of rows. The
+                splitter uses the last max_delay number of rows from the previous split as the first max_delay number
+                of rows of the current split to avoid "throwing out" more data than in necessary.
+            gap (int): Gap used in time series problem. Time series pipelines shift the target variable by gap rows
+                since we are interested in
+            """
+        self.max_delay = max_delay
+        self.gap = gap
+        self.n_folds = n_folds
+        self._splitter = SkTimeSeriesSplit(n_splits=n_folds)
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        """Get the number of data splits."""
+        return self._splitter.n_splits
+
+    @staticmethod
+    def _check_if_empty(data):
+        return data is None or data.empty
+
+    def split(self, X, y=None, groups=None):
+        """Get the time series splits.
+
+        This method can handle passing in empty or None X and y data but note that X and y cannot be None or empty
+        at the same time.
+
+        Arguments:
+            X (pd.DataFrame, None): Features to split.
+            y (pd.DataFrame, None): Target variable to split.
+            groups: Ignored but kept for compatibility with sklearn api.
+
+        Returns:
+            Iterator of (train, test) indices tuples.
+        """
+        # Sklearn splitters always assume a valid X is passed but we need to support the
+        # TimeSeriesPipeline convention of being able to pass in empty X dataframes
+        if self._check_if_empty(X) and self._check_if_empty(y):
+            raise ValueError("Both X and y cannot be None in TimeSeriesSplit.split")
+        elif self._check_if_empty(X) and not self._check_if_empty(y):
+            split_kwargs = dict(X=y, groups=groups)
+            max_index = y.shape[0]
+        elif self._check_if_empty(y) and not self._check_if_empty(X):
+            split_kwargs = dict(X=X, groups=groups)
+            max_index = X.shape[0]
+        else:
+            split_kwargs = dict(X=X, y=y, groups=groups)
+            max_index = X.shape[0]
+
+        for train, test in self._splitter.split(**split_kwargs):
+            last_train = train[-1]
+            last_test = test[-1]
+            first_test = test[0]
+            max_test_index = min(last_test + 1 + self.gap, max_index)
+            new_train = np.concatenate([train, np.arange(last_train + 1, last_train + 1 + self.gap)])
+            new_test = np.concatenate([
+                np.arange(first_test - self.max_delay, first_test), test, np.arange(last_test + 1, max_test_index)
+            ])
+            yield new_train, new_test
---
+++ evalml/tests/automl_tests/test_time_series_split.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import pytest
+
+from evalml.automl.data_splitters import TimeSeriesSplit
+
+
+def test_time_series_split_init():
+    breakpoint()
+    ts_split = TimeSeriesSplit(gap=3, max_delay=4, n_folds=5)
+    assert ts_split.get_n_splits() == 5
+
+    with pytest.raises(ValueError, match="Both X and y cannot be None in TimeSeriesSplit.split"):
+        _ = list(ts_split.split(X=None, y=None))
+
+    with pytest.raises(ValueError, match="Both X and y cannot be None in TimeSeriesSplit.split"):
+        _ = list(ts_split.split(X=pd.DataFrame(), y=pd.Series([])))
+
+
+@pytest.mark.parametrize("max_delay,gap", [(0, 0), (1, 1), (2, 0), (4, 2)])
+@pytest.mark.parametrize("X_none,y_none", [(False, False), (True, False), (False, True)])
+def test_time_series_split(max_delay, gap, X_none, y_none):
+    X = pd.DataFrame({"features": range(1, 32)})
+    y = pd.Series(range(1, 32))
+
+    # Splitter does not need a daterange index. We use a daterange index so that the
+    # expected answer is easier to understand
+    y.index = pd.date_range("2020-10-01", "2020-10-31")
+    X.index = pd.date_range("2020-10-01", "2020-10-31")
+
+    answer = [(pd.date_range("2020-10-01", f"2020-10-{10 + gap}"), pd.date_range(f"2020-10-{11 - max_delay}", f"2020-10-{17 + gap}")),
+              (pd.date_range("2020-10-01", f"2020-10-{17 + gap}"), pd.date_range(f"2020-10-{18 - max_delay}", f"2020-10-{24 + gap}")),
+              (pd.date_range("2020-10-01", f"2020-10-{24 + gap}"), pd.date_range(f"2020-10-{25 - max_delay}", "2020-10-31"))]
+
+    if X_none:
+        X = None
+    if y_none:
+        y = None
+
+    ts_split = TimeSeriesSplit(gap=gap, max_delay=max_delay)
+    for i, (train, test) in enumerate(ts_split.split(X, y)):
+        if not X_none:
+            X_train, X_test = X.iloc[train], X.iloc[test]
+            pd.testing.assert_index_equal(X_train.index, answer[i][0])
+            pd.testing.assert_index_equal(X_test.index, answer[i][1])
+        if not y_none:
+            y_train, y_test = y.iloc[train], y.iloc[test]
+            pd.testing.assert_index_equal(y_train.index, answer[i][0])
+            pd.testing.assert_index_equal(y_test.index, answer[i][1])
