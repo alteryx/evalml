11558b25427cdfe7ae384ffb31e15abdcb915a2e
Angela Lin <angela97lin@gmail.com>
2020-11-30 17:35:04 -0500

    update to not use gs .shape[0]

--- evalml/tests/preprocessing_tests/test_split_data.py
+++ evalml/tests/preprocessing_tests/test_split_data.py
@@ -5,21 +5,21 @@ def test_split_regression(X_y_regression):
     X, y = X_y_regression
     test_pct = 0.25
     X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_pct, regression=True)
-    test_size = X.shape[0] * test_pct
-    train_size = X.shape[0] - test_size
-    assert X_train.shape[0] == train_size
-    assert X_test.shape[0] == test_size
-    assert y_train.shape[0] == train_size
-    assert y_test.shape[0] == test_size
+    test_size = len(X) * test_pct
+    train_size = len(X) - test_size
+    assert len(X_train) == train_size
+    assert len(X_test) == test_size
+    assert len(y_train) == train_size
+    assert len(y_test) == test_size
 
 
 def test_split_classification(X_y_binary):
     X, y = X_y_binary
     test_pct = 0.25
     X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_pct)
-    test_size = X.shape[0] * 0.25
-    train_size = X.shape[0] - test_size
-    assert X_train.shape[0] == train_size
-    assert X_test.shape[0] == test_size
-    assert y_train.shape[0] == train_size
-    assert y_test.shape[0] == test_size
+    test_size = len(X) * 0.25
+    train_size = len(X) - test_size
+    assert len(X_train) == train_size
+    assert len(X_test) == test_size
+    assert len(y_train) == train_size
+    assert len(y_test) == test_size
