c48b9ec227ce88789e81ae31e2039d348fd76bd1
Angela Lin <angela97lin@gmail.com>
2020-11-30 11:57:12 -0500

    fix some tests

--- evalml/tests/pipeline_tests/regression_pipeline_tests/test_regression.py
+++ evalml/tests/pipeline_tests/regression_pipeline_tests/test_regression.py
@@ -7,11 +7,11 @@ from evalml.demos import load_breast_cancer, load_diabetes, load_wine
 
 @pytest.mark.parametrize("target_type", ["category", "string", "bool"])
 def test_invalid_targets_regression_pipeline(target_type, dummy_regression_pipeline_class):
-    X, y = load_wine()
+    X, y = load_wine(return_pandas=True)
     if target_type == "category":
         y = pd.Series(y).astype("category")
     if target_type == "bool":
-        X, y = load_breast_cancer()
+        X, y = load_breast_cancer(return_pandas=True)
         y = y.map({"malignant": False, "benign": True})
     mock_regression_pipeline = dummy_regression_pipeline_class(parameters={})
     with pytest.raises(ValueError, match="Regression pipeline cannot handle targets with dtype"):
@@ -20,8 +20,6 @@ def test_invalid_targets_regression_pipeline(target_type, dummy_regression_pipel
 
 def test_woodwork_regression_pipeline(linear_regression_pipeline_class):
     X, y = load_diabetes()
-    X = ww.DataTable(X)
-    y = ww.DataColumn(y)
     mock_regression_pipeline = linear_regression_pipeline_class(parameters={})
     mock_regression_pipeline.fit(X, y)
     assert not pd.isnull(mock_regression_pipeline.predict(X)).any()
