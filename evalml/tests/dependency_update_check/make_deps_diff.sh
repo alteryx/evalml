e60a4beef22f5170b1500bad23ca23fb51c59b29
Dylan Sherry <sharshofski@gmail.com>
2020-11-30 18:25:31 -0500

    Fix shellcheck errors

--- evalml/tests/dependency_update_check/make_deps_diff.sh
+++ evalml/tests/dependency_update_check/make_deps_diff.sh
@@ -1,3 +1,3 @@
 allow_list=$(cat core-requirements.txt requirements.txt | grep -oE "^[a-zA-Z0-9\-_]+" | paste -d "|" -s -)
 echo "Allow list: ${allow_list}"
-pip freeze | grep -v "evalml.git" | grep -E ${allow_list} > ${DEPENDENCY_FILE_PATH}
+pip freeze | grep -v "evalml.git" | grep -E "${allow_list}" > "${DEPENDENCY_FILE_PATH}"
