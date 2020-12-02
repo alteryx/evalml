98ef7e924ad3afd9adf8352843bea34421a8a886
Dylan Sherry <sharshofski@gmail.com>
2020-12-02 13:56:41 -0500

    Add note about platform

--- tools/format_release_notes.sh
+++ tools/format_release_notes.sh
@@ -4,7 +4,9 @@ if [ "$1" = "-h" ]
   then
     echo -e 'Grab the "Future Releases" section from the release notes and format it in markdown for GitHub releases
 
-Usage: tools/format_release_notes.sh'
+Usage: tools/format_release_notes.sh
+
+Note: this was written to work properly on mac. It should work on linux if -r is used instead of -E for sed.'
     exit 0
 fi
 evalml_version=$(sed -n -E "s/.*version=\'([0-9A-Za-z\.\_]+)\'.*/\1/p" setup.py)
