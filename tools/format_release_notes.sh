3df8f974e3a0fad2df24df2ebc0c24f570dd88a5
Dylan Sherry <sharshofski@gmail.com>
2020-12-02 13:54:14 -0500

    Generalize

--- tools/format_release_notes.sh
+++ tools/format_release_notes.sh
@@ -8,7 +8,7 @@ Usage: tools/format_release_notes.sh'
     exit 0
 fi
 evalml_version=$(sed -n -E "s/.*version=\'([0-9A-Za-z\.\_]+)\'.*/\1/p" setup.py)
-content_raw=$(sed -n -e "/^\*\*v0.16.1/,/^\*\*v0.16.0/p;/^\*\*v0.16.0/q" docs/source/release_notes.rst | sed '/^\.\. warning::$/d' | tail -n +2 | sed '$ d' | awk 'NF')
+content_raw=$(sed -n -E '/^\*\*v[A-Za-z0-9\., ]+\*\*$/,$p' docs/source/release_notes.rst | tail -n +2 | sed -E '/\*\*v[A-Za-z0-9\., ]+\*\*$/q' | sed '$ d' | awk 'NF')
 date_formatted=$(date '+%b. %-d, %Y')
 content_markdown=$(echo -n "${content_raw}" | sed 's/^        \* /- /g' | sed 's/^    \* /### /g' | sed 's/    \*\*Breaking Changes\*\*/### Breaking Changes/g' | sed -E "s/:pr:\`([0-9]+)\`/#\1/g")
 full_markdown=$(echo -e "# v${evalml_version} ${date_formatted}\n${content_markdown}")
