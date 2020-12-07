8074de4753f096ca3c0a5024b011963521a270a4
Jeremy Shih <jeremyliweishih@gmail.com>
2020-12-07 14:57:40 -0500

    set x=None in no X test for fit

--- evalml/tests/pipeline_tests/regression_pipeline_tests/test_time_series_baseline_regression.py
+++ evalml/tests/pipeline_tests/regression_pipeline_tests/test_time_series_baseline_regression.py
@@ -20,7 +20,7 @@ def test_time_series_baseline_no_X(ts_data):
     X, y = ts_data
 
     clf = TimeSeriesBaselineRegressionPipeline(parameters={"pipeline": {"gap": 1, "max_delay": 1}})
-    clf.fit(X, y=y)
+    clf.fit(X=None, y=y)
 
     np.testing.assert_allclose(clf.predict(X=None, y=y), y)
 
