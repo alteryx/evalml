1c63a179314e75f611e28325e0edbd6f1b96683b
Jeremy Shih <jeremyliweishih@gmail.com>
2020-12-07 11:12:26 -0500

    codecove

--- evalml/tests/pipeline_tests/regression_pipeline_tests/test_timeseries_baseline_regression.py
+++ evalml/tests/pipeline_tests/regression_pipeline_tests/test_timeseries_baseline_regression.py
@@ -10,3 +10,12 @@ def test_time_series_baseline(ts_data):
     clf.fit(X, y)
 
     np.testing.assert_allclose(clf.predict(X, y), y)
+
+
+def test_time_series_baseline_no_X(ts_data):
+    X, y = ts_data
+
+    clf = TimeSeriesBaselineRegressionPipeline(parameters={"pipeline": {"gap": 1, "max_delay": 1}})
+    clf.fit(X, y=y)
+
+    np.testing.assert_allclose(clf.predict(X=None, y=y), y)
