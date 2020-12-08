cd72fd3fa630e557636eb3e95d67213b89d0ef3f
Parthiv Naresh <parthiv.naresh@alteryx.com>
2020-12-07 21:54:46 -0500

    Add ValueError check

--- evalml/objectives/standard_metrics.py
+++ evalml/objectives/standard_metrics.py
@@ -9,6 +9,7 @@ from .multiclass_classification_objective import (
     MulticlassClassificationObjective
 )
 from .regression_objective import RegressionObjective
+from .time_series_regression_objective import TimeSeriesRegressionObjective
 
 
 class AccuracyBinary(BinaryClassificationObjective):
@@ -345,15 +346,20 @@ class MAE(RegressionObjective):
         return metrics.mean_absolute_error(y_true, y_predicted)
 
 
-class MAPE(RegressionObjective):
-    """Mean absolute percentage error for time series regression."""
+class MAPE(TimeSeriesRegressionObjective):
+    """Mean absolute percentage error for time series regression. Scaled by 100 to return a percentage.
+
+    Only valid for nonzero inputs. Otherwise, will throw a ValueError
+    """
     name = "Mean Absolute Percentage Error"
     greater_is_better = False
-    score_needs_proba = False
     perfect_score = 0.0
 
     def objective_function(self, y_true, y_predicted, X=None):
-        return (np.abs((y_true - y_predicted) / y_true))[y_true != 0].mean() * 100
+        if (y_true == 0).any():
+            raise ValueError("Mean Absolute Percentage Error cannot be used when "
+                             "targets contain the value 0.")
+        return (np.abs((y_true - y_predicted) / y_true)).mean() * 100
 
 
 class MSE(RegressionObjective):
