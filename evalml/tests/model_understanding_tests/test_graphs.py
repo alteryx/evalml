47adf09f9a37f19d1979ae67d6bbe535290d1776
Parthiv Naresh <parthiv.naresh@alteryx.com>
2020-12-10 02:01:20 -0500

    Coverage

--- evalml/tests/model_understanding_tests/test_graphs.py
+++ evalml/tests/model_understanding_tests/test_graphs.py
@@ -1054,8 +1054,8 @@ def test_decision_tree_data_from_estimator_feature_length(fitted_tree_estimators
                                          .format(est_class._component_obj.n_features_, 3)):
         decision_tree_data_from_estimator(est_class, feature_names=["First", "Second", "Third"])
 
-    features_array = np.array([f'Testing_{col_}' for col_ in range(est_class._component_obj.n_features_)])
-    formatted_ = decision_tree_data_from_estimator(est_class, feature_names=list(features_array))
+    features_array = tuple([f'Testing_{col_}' for col_ in range(est_class._component_obj.n_features_)])
+    formatted_ = decision_tree_data_from_estimator(est_class, feature_names=features_array)
     assert isinstance(formatted_, OrderedDict)
 
 
