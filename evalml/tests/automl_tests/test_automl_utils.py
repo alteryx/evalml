533db989c2651fea495771b966509fb560441b49
Dylan Sherry <sharshofski@gmail.com>
2020-12-16 22:32:11 -0500

    Initial impl

---
+++ evalml/tests/automl_tests/test_automl_utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import KFold, StratifiedKFold
+
+from automl.utils import make_data_splitter
+
+from evalml import AutoMLSearch
+from evalml.automl.data_splitters import (
+    TimeSeriesSplit,
+    TrainingValidationSplit
+)
+
+
+def test_make_data_splitter():
+    X = pd.DataFrame({'col_0': list(range(10)),
+                      'target': list(range(10))})
+    y = X.pop('target')
+    data_splitter = make_data_splitter(X, y, 'regression')
+    assert isinstance(data_splitter, KFold)
+    assert data_splitter.n_splits == 3
