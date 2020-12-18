bda095926474f29a2d49bf2b778479dab7057377
christopherbunn <chris.bunn@alteryx.com>
2020-12-18 17:45:59 -0500

    Fixed init.py and updated Dask engine to not use evaluate_pipeline

--- evalml/automl/engines/dask_engine.py
+++ evalml/automl/engines/dask_engine.py
@@ -1,5 +1,6 @@
-from evalml.automl.engines import EngineBase
 from dask.distributed import Client, as_completed
+
+from evalml.automl.engines import EngineBase, EngineResult
 
 
 class DaskEngine(EngineBase):
@@ -13,14 +14,33 @@ class DaskEngine(EngineBase):
         self.y_future = self.client.scatter(y)
         super().load_data(X, y)
 
-    def evaluate_batch(self, pipeline_batch, result_callback=None, log_pipeline=False):
-        fitted_pipelines = []
-        evaluation_results = []
+    def evaluate_batch(self, pipeline_batch, result_callback=None, log_pipeline=False, ignore_stopping_condition=False):
+        """ Evaluates a batch of pipelines in order.
+
+        Arguments:
+            pipeline_batch (list(PipelineBase)): A batch of pipelines to be fitted and evaluated.
+            result_callback (callable): Function called once the pipeline is finished evaluating to store the results. If None, results will only be returned.
+            log_pipeline (bool): If True, log the pipeline and relevant information before evaluation.
+            ignore_stopping_condition (bool): If True, will add pipelines regardless of stopping condition.
+                If False, calls `_check_stopping_condition` to determine if additional pipelines should be evaluated. Default is False.
+
+        Returns:
+            EngineResult: An engine result object with completed pipelines, results, and unprocessed pipelines.
+        """
+        super().evaluate_batch()
+        result = EngineResult()
+        if not ignore_stopping_condition and not self.automl._check_stopping_condition(self.automl._start):
+            result.set_early_stop(None, pipeline_batch)
+            return result
         if self.automl.start_iteration_callback:
             for pipeline in pipeline_batch:
                 self.automl.start_iteration_callback(pipeline.__class__, pipeline.parameters, self.automl)
-
-        pipeline_futures = self.client.map(self._compute_cv_scores, pipeline_batch, automl=self.automl, X=self.X_future, y=self.y_future)
+        
+        dask_pipelines = []
+        while len(pipeline_batch) > 0:
+            dask_pipelines.append(pipeline_batch.pop())
+        
+        pipeline_futures = self.client.map(self._compute_cv_scores, dask_pipelines, automl=self.automl, X=self.X_future, y=self.y_future)
 
         for future in as_completed(pipeline_futures):
             pipeline, evaluation_result = future.result()
@@ -28,22 +48,5 @@ class DaskEngine(EngineBase):
                 self.log_pipeline(pipeline)
             if result_callback:
                 self._add_result_callback(result_callback, pipeline, evaluation_result)
-            fitted_pipelines.append(pipeline)
-            evaluation_results.append(evaluation_result)
-
-        return fitted_pipelines, evaluation_results, []
-
-    def evaluate_pipeline(self, pipeline, result_callback=None, log_pipeline=False):
-        try:
-            if log_pipeline:
-                self.log_pipeline(pipeline)
-            if self.automl.start_iteration_callback:
-                self.automl.start_iteration_callback(pipeline.__class__, pipeline.parameters, self)
-            pipeline, evaluation_result = self.client.submit(self._compute_cv_scores, pipeline, automl=self.automl, X=self.X_future, y=self.y_future)
-            if result_callback:
-                self._add_result_callback(result_callback, pipeline, evaluation_result)
-            return pipeline, evaluation_result
-        except KeyboardInterrupt:
-            pipeline_batch = self.automl._handle_keyboard_interrupt([], pipeline)
-            if pipeline_batch == []:
-                return pipeline_batch, []
+            result.add_result(pipeline, evaluation_result)
+        return result
