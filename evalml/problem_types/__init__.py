19a7345953abbedd8970c60ef1b62a5152e4b57f
Dylan Sherry <sharshofski@gmail.com>
2020-12-22 16:27:23 -0500

    Add is_regression, is_classification, is_timeseries prob type helpers

--- evalml/problem_types/__init__.py
+++ evalml/problem_types/__init__.py
@@ -1,2 +1,2 @@
 from .problem_types import ProblemTypes
-from .utils import handle_problem_types, detect_problem_type
+from .utils import handle_problem_types, detect_problem_type, is_regression, is_classification, is_time_series
