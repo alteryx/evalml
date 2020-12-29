6c30734815f267e784a1ff8764f424aa954d09c9
Angela Lin <angela97lin@gmail.com>
2020-12-29 15:13:22 -0500

    lint

--- evalml/automl/automl_search.py
+++ evalml/automl/automl_search.py
@@ -7,7 +7,7 @@ from collections import OrderedDict, defaultdict
 import cloudpickle
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import BaseCrossValidator, train_test_split
+from sklearn.model_selection import BaseCrossValidator
 
 from .pipeline_search_plots import PipelineSearchPlots
 
