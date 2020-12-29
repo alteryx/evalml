fc4051f32a907501d46ad2c64e7a716dac1e7995
bchen1116 <bchen1116@gmail.com>
2020-12-29 10:46:20 -0500

    fix test

--- evalml/data_checks/target_leakage_data_check.py
+++ evalml/data_checks/target_leakage_data_check.py
@@ -65,7 +65,7 @@ class TargetLeakageDataCheck(DataCheck):
         combined = X.copy()
         combined['target'] = y
         combined = _convert_to_woodwork_structure(combined)
-        mutual_info = combined.mutual_information(num_bins=0)
+        mutual_info = combined.mutual_information()
         if len(mutual_info) == 0:
             return messages
         corr_df = mutual_info[(mutual_info['mutual_info'] >= self.pct_corr_threshold) & ((mutual_info['column_1'] == 'target') | (mutual_info['column_2'] == 'target'))]
