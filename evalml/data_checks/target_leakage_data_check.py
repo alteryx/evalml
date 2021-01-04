0c22a3f804d8ab155a244d7002b5977eb7c807f8
bchen1116 <bchen1116@gmail.com>
2021-01-04 14:32:17 -0500

    update pearson implementation

--- evalml/data_checks/target_leakage_data_check.py
+++ evalml/data_checks/target_leakage_data_check.py
@@ -17,6 +17,7 @@ class TargetLeakageDataCheck(DataCheck):
         """Check if any of the features are highly correlated with the target.
 
         If `pearson_corr=False`, supports all target and feature types. Otherwise, only supports binary with numeric and boolean dtypes.
+        Pearson correlation returns a value in [-1, 1], while mutual information returns a value in [0, 1].
 
         Arguments:
             pct_corr_threshold (float): The correlation threshold to be considered leakage. Defaults to 0.95.
@@ -28,10 +29,33 @@ class TargetLeakageDataCheck(DataCheck):
         self.pct_corr_threshold = pct_corr_threshold
         self.pearson = pearson_corr
 
+    def _calculate_pearson(self, X, y, messages):
+        highly_corr_cols = []
+        X = X.select_dtypes(include=numeric_and_boolean_dtypes)
+        if len(X.columns) > 0:
+            highly_corr_cols = [label for label, col in X.iteritems() if abs(y.corr(col)) >= self.pct_corr_threshold]
+        return highly_corr_cols
+
+    def _calculate_mutual_information(self, X, y):
+        highly_corr_cols = []
+        target = 'target'
+        while target in X.columns:
+            target += '0'
+        combined = X.copy()
+        combined[target] = y
+        combined = _convert_to_woodwork_structure(combined)
+        for col in X.columns:
+            sample = combined[[col, target]]
+            mutual_info = sample.mutual_information()
+            if len(mutual_info) > 0 and mutual_info['mutual_info'].iloc[0] > self.pct_corr_threshold:
+                highly_corr_cols.append(col)
+        return highly_corr_cols
+
     def validate(self, X, y):
         """Check if any of the features are highly correlated with the target.
 
         If `pearson_corr=False`, supports all target and feature types. Otherwise, only supports binary with numeric and boolean dtypes.
+        Pearson correlation returns a value in [-1, 1], while mutual information returns a value in [0, 1].
 
         Arguments:
             X (ww.DataTable, pd.DataFrame, np.ndarray): The input features to check
@@ -66,26 +90,11 @@ class TargetLeakageDataCheck(DataCheck):
         X = _convert_woodwork_types_wrapper(X.to_dataframe())
         y = _convert_woodwork_types_wrapper(y.to_series())
         if not self.pearson:
-            highly_corr_cols = []
-            target = 'target'
-            while target in X.columns:
-                target += '0'
-            combined = X.copy()
-            combined[target] = y
-            combined = _convert_to_woodwork_structure(combined)
-            for col in X.columns:
-                sample = combined[[col, target]]
-                mutual_info = sample.mutual_information()
-                if len(mutual_info) > 0 and mutual_info['mutual_info'].iloc[0] > self.pct_corr_threshold:
-                    highly_corr_cols.append(col)
-
+            highly_corr_cols = self._calculate_mutual_information(X, y)
         else:
             if y.dtype not in numeric_and_boolean_dtypes:
                 return messages
-            X = X.select_dtypes(include=numeric_and_boolean_dtypes)
-            if len(X.columns) == 0:
-                return messages
-            highly_corr_cols = [label for label, col in X.iteritems() if abs(y.corr(col)) >= self.pct_corr_threshold]
+            highly_corr_cols = self._calculate_pearson(X, y, messages)
 
         warning_msg = "Column '{}' is {}% or more correlated with the target"
         messages["warnings"].extend([DataCheckWarning(message=warning_msg.format(col_name, self.pct_corr_threshold * 100),
