93dcc796cf2b875d2a12bb2fa3e8980887542d45
Angela Lin <angela97lin@gmail.com>
2021-01-13 14:26:28 -0500

    fix baseline classification test

--- evalml/tests/pipeline_tests/classification_pipeline_tests/test_baseline_classification.py
+++ evalml/tests/pipeline_tests/classification_pipeline_tests/test_baseline_classification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from evalml.pipelines import BaselineBinaryPipeline, BaselineMulticlassPipeline
 from evalml.utils import get_random_state
@@ -15,11 +16,14 @@ def test_baseline_binary_random(X_y_binary):
     }
     clf = BaselineBinaryPipeline(parameters=parameters)
     clf.fit(X, y)
-    predicted_proba = clf.predict_proba(X)
+    expected_predicted = pd.Series(get_random_state(0).choice(np.unique(y), len(X)), dtype="Int64")
+    assert_series_equal(expected_predicted, clf.predict(X).to_series())
 
-    np.testing.assert_allclose(clf.predict(X), get_random_state(0).choice(np.unique(y), len(X)))
+    predicted_proba = clf.predict_proba(X)
     assert predicted_proba.shape == (len(X), 2)
-    np.testing.assert_allclose(predicted_proba, np.array([[0.5 for i in range(len(values))]] * len(X)))
+    expected_predicted_proba = pd.DataFrame(np.array([[0.5 for i in range(len(values))]] * len(X)))
+    assert_frame_equal(expected_predicted_proba, predicted_proba.to_dataframe())
+
     np.testing.assert_allclose(clf.feature_importance.iloc[:, 1], np.array([0.0] * X.shape[1]))
 
 
@@ -36,11 +40,15 @@ def test_baseline_binary_random_weighted(X_y_binary):
     }
     clf = BaselineBinaryPipeline(parameters=parameters)
     clf.fit(X, y)
-    predicted_proba = clf.predict_proba(X)
 
-    np.testing.assert_allclose(clf.predict(X), get_random_state(0).choice(np.unique(y), len(X), p=percent_freq))
+    expected_predicted = pd.Series(get_random_state(0).choice(np.unique(y), len(X), p=percent_freq), dtype="Int64")
+    assert_series_equal(expected_predicted, clf.predict(X).to_series())
+
+    expected_predicted_proba = pd.DataFrame(np.array([[percent_freq[i] for i in range(len(values))]] * len(X)))
+    predicted_proba = clf.predict_proba(X)
     assert predicted_proba.shape == (len(X), 2)
-    np.testing.assert_allclose(predicted_proba, np.array([[percent_freq[i] for i in range(len(values))]] * len(X)))
+    assert_frame_equal(expected_predicted_proba, predicted_proba.to_dataframe())
+
     np.testing.assert_allclose(clf.feature_importance.iloc[:, 1], np.array([0.0] * X.shape[1]))
 
 
@@ -54,11 +62,15 @@ def test_baseline_binary_mode():
     }
     clf = BaselineBinaryPipeline(parameters=parameters)
     clf.fit(X, y)
-    np.testing.assert_allclose(clf.predict(X), np.array([10] * len(X)))
+
+    expected_predicted = pd.Series(np.array([10] * len(X)), dtype="Int64")
+    assert_series_equal(expected_predicted, clf.predict(X).to_series())
+
     predicted_proba = clf.predict_proba(X)
     assert predicted_proba.shape == (len(X), 2)
     expected_predicted_proba = pd.DataFrame({10: [1., 1., 1., 1.], 11: [0., 0., 0., 0.]})
-    pd.testing.assert_frame_equal(expected_predicted_proba, predicted_proba)
+    assert_frame_equal(expected_predicted_proba, predicted_proba.to_dataframe())
+
     np.testing.assert_allclose(clf.feature_importance.iloc[:, 1], np.array([0.0] * X.shape[1]))
 
 
@@ -72,11 +84,14 @@ def test_baseline_multi_random(X_y_multi):
     }
     clf = BaselineMulticlassPipeline(parameters=parameters)
     clf.fit(X, y)
-    predicted_proba = clf.predict_proba(X)
 
-    np.testing.assert_allclose(clf.predict(X), get_random_state(0).choice(np.unique(y), len(X)))
+    expected_predicted = pd.Series(get_random_state(0).choice(np.unique(y), len(X)), dtype="Int64")
+    assert_series_equal(expected_predicted, clf.predict(X).to_series())
+
+    predicted_proba = clf.predict_proba(X)
     assert predicted_proba.shape == (len(X), 3)
-    np.testing.assert_allclose(predicted_proba, np.array([[1. / 3 for i in range(len(values))]] * len(X)))
+    expected_predicted_proba = pd.DataFrame(np.array([[1. / 3 for i in range(len(values))]] * len(X)))
+    assert_frame_equal(expected_predicted_proba, predicted_proba.to_dataframe())
     np.testing.assert_allclose(clf.feature_importance.iloc[:, 1], np.array([0.0] * X.shape[1]))
 
 
@@ -93,11 +108,15 @@ def test_baseline_multi_random_weighted(X_y_multi):
     }
     clf = BaselineMulticlassPipeline(parameters=parameters)
     clf.fit(X, y)
-    predicted_proba = clf.predict_proba(X)
 
-    np.testing.assert_allclose(clf.predict(X), get_random_state(0).choice(np.unique(y), len(X), p=percent_freq))
+    expected_predicted = pd.Series(get_random_state(0).choice(np.unique(y), len(X), p=percent_freq), dtype="Int64")
+    assert_series_equal(expected_predicted, clf.predict(X).to_series())
+
+    predicted_proba = clf.predict_proba(X)
     assert predicted_proba.shape == (len(X), 3)
-    np.testing.assert_allclose(predicted_proba, np.array([[percent_freq[i] for i in range(len(values))]] * len(X)))
+    expected_predicted_proba = pd.DataFrame(np.array([[percent_freq[i] for i in range(len(values))]] * len(X)))
+    assert_frame_equal(expected_predicted_proba, predicted_proba.to_dataframe())
+
     np.testing.assert_allclose(clf.feature_importance.iloc[:, 1], np.array([0.0] * X.shape[1]))
 
 
@@ -111,9 +130,12 @@ def test_baseline_multi_mode():
     }
     clf = BaselineMulticlassPipeline(parameters=parameters)
     clf.fit(X, y)
-    np.testing.assert_allclose(clf.predict(X), np.array([11] * len(X)))
+    expected_predicted = pd.Series(np.array([11] * len(X)), dtype="Int64")
+    assert_series_equal(expected_predicted, clf.predict(X).to_series())
+
     predicted_proba = clf.predict_proba(X)
     assert predicted_proba.shape == (len(X), 3)
     expected_predicted_proba = pd.DataFrame({10: [0., 0., 0., 0.], 11: [1., 1., 1., 1.], 12: [0., 0., 0., 0.]})
-    pd.testing.assert_frame_equal(expected_predicted_proba, predicted_proba)
+    assert_frame_equal(expected_predicted_proba, predicted_proba.to_dataframe())
+
     np.testing.assert_allclose(clf.feature_importance.iloc[:, 1], np.array([0.0] * X.shape[1]))
