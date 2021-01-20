dd23321117f39084d06761b449ab1c45b23e6aa3
bchen1116 <bchen1116@gmail.com>
2021-01-20 11:41:32 -0500

    remove unecessary files

--- .ipython/profile_default/ipython_kernel_config.py
+++
@@ -1,3 +0,0 @@
-c.InlineBackend.figure_formats = {'svg', 'pdf'}
-c.InlineBackend.rc = {'figure.dpi': 96}
-
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,10 +42,7 @@ release = evalml.__version__
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
-nbsphinx_execute_arguments = [
-    "--InlineBackend.figure_formats={'svg', 'pdf'}",
-    "--InlineBackend.rc={'figure.dpi': 96}",
-]
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
