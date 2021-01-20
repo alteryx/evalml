871de4a3e985f489d5d153bd7310571b88b58dae
Jeremy Shih <jeremyliweishih@gmail.com>
2021-01-20 13:31:37 -0500

    add more coverage

--- evalml/tests/component_tests/test_prophet_regressor.py
+++ evalml/tests/component_tests/test_prophet_regressor.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pytest
 from pytest import importorskip
 
 from evalml.model_family import ModelFamily
@@ -21,22 +21,12 @@ def test_fit_predict_ts(ts_data):
     X, y = ts_data
 
     def build_prophet_df(X, y=None):
-        # check for datetime column
-        if 'ds' in X.columns:
-            date_col = X['ds']
-        elif isinstance(X.index, pd.DatetimeIndex):
-            date_col = X.reset_index()
-            date_col = date_col['index']
-        else:
-            date_col = X.select_dtypes(include='datetime')
-            if date_col.shape[1] == 0:
-                raise ValueError('Prophet estimator requires input data X to have a datetime column')
-
+        date_col = X.reset_index()
+        date_col = date_col['index']
         date_col = date_col.rename('ds')
         prophet_df = date_col.to_frame()
-        if y is not None:
-            y.index = prophet_df.index
-            prophet_df['y'] = y
+        y.index = prophet_df.index
+        prophet_df['y'] = y
         return prophet_df
 
     p_clf = prophet.Prophet()
@@ -51,3 +41,39 @@ def test_fit_predict_ts(ts_data):
     y_pred = clf.predict(X)
 
     assert (y_pred == y_pred_p).all()
+
+
+def test_fit_predict_ds_col(ts_data):
+    X, y = ts_data
+
+    def build_prophet_df(X, y=None):
+        date_col = X.reset_index()
+        date_col = date_col['index']
+        date_col = date_col.rename('ds')
+        prophet_df = date_col.to_frame()
+        y.index = prophet_df.index
+        prophet_df['y'] = y
+        return prophet_df
+
+    p_clf = prophet.Prophet()
+    prophet_df = build_prophet_df(X, y)
+
+    with suppress_stdout_stderr():
+        p_clf.fit(prophet_df)
+    y_pred_p = p_clf.predict(prophet_df)['yhat']
+
+    X = X.reset_index()
+    X = X['index'].rename('ds').to_frame()
+    clf = ProphetRegressor()
+    clf.fit(X, y)
+    y_pred = clf.predict(X)
+
+    assert (y_pred == y_pred_p).all()
+
+
+def test_fit_predict_no_ds(X_y_binary):
+    X, y = X_y_binary
+
+    clf = ProphetRegressor()
+    with pytest.raises(ValueError):
+        clf.fit(X, y)
