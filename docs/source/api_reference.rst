35414ac4a39b932fc252218361d67e1953f257eb
bchen1116 <bchen1116@gmail.com>
2021-01-21 17:18:05 -0500

    update api ref

--- docs/source/api_reference.rst
+++ docs/source/api_reference.rst
@@ -133,6 +133,12 @@ Pipeline Base Classes
     TimeSeriesBinaryClassificationPipeline
     TimeSeriesMulticlassClassificationPipeline
     TimeSeriesRegressionPipeline
+    GeneratedPipelineBinary
+    GeneratedPipelineMulticlass
+    GeneratedPipelineRegression
+    GeneratedPipelineTimeSeriesBinary
+    GeneratedPipelineTimeSeriesMulticlass
+    GeneratedPipelineTimeSeriesRegression
 
 Classification Pipelines
 ~~~~~~~~~~~~~~~~~~~~~~~~
--- evalml/pipelines/generate_pipeline.py
+++ evalml/pipelines/generate_pipeline.py
@@ -11,36 +11,48 @@ from .time_series_regression_pipeline import TimeSeriesRegressionPipeline
 
 
 class GeneratedPipelineBinary(BinaryClassificationPipeline):
+    """Generated Pipeline class for Binary Classification Pipelines
+    """
     custom_name = ''
     component_graph = []
     custom_hyperparameters = None
 
 
 class GeneratedPipelineMulticlass(MulticlassClassificationPipeline):
+    """Generated Pipeline class for Multiclass Classification Pipelines
+    """
     custom_name = ''
     component_graph = []
     custom_hyperparameters = None
 
 
 class GeneratedPipelineRegression(RegressionPipeline):
+    """Generated Pipeline class for Regression Pipelines
+    """
     custom_name = ''
     component_graph = []
     custom_hyperparameters = None
 
 
 class GeneratedPipelineTimeSeriesBinary(TimeSeriesBinaryClassificationPipeline):
+    """Generated Pipeline class for Time Series Binary Classification Pipelines
+    """
     custom_name = ''
     component_graph = []
     custom_hyperparameters = None
 
 
 class GeneratedPipelineTimeSeriesMulticlass(TimeSeriesMulticlassClassificationPipeline):
+    """Generated Pipeline class for Time Series Multiclass Classification Pipelines
+    """
     custom_name = ''
     component_graph = []
     custom_hyperparameters = None
 
 
 class GeneratedPipelineTimeSeriesRegression(TimeSeriesRegressionPipeline):
+    """Generated Pipeline class for Time Series Regression Pipelines
+    """
     custom_name = ''
     component_graph = []
     custom_hyperparameters = None
