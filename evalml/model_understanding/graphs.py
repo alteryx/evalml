0d95829299817e5a464a79260e51ce56e157f320
Karsten Chu <karsten.chu@alteryx.com>
2021-01-29 11:26:28 -0500

    Lint fix.

--- evalml/model_understanding/graphs.py
+++ evalml/model_understanding/graphs.py
@@ -477,7 +477,7 @@ def partial_dependence(pipeline, X, features, grid_resolution=100):
     X = _convert_to_woodwork_structure(X)
     # Dynamically set the grid resolution to the maximum number of categories
     # in the categorical variables if there are more categories than resolution cells
-    X_cats =  X.select("categorical")
+    X_cats = X.select("categorical")
     if X_cats.shape[1] != 0:
         max_num_cats = max(X_cats.describe().loc["nunique"])
         grid_resolution = max([max_num_cats + 1, grid_resolution])
