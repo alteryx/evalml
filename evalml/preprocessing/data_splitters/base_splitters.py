08a14ca54a9a83814913cb5f72206e23301e13bf
bchen1116 <bchen1116@gmail.com>
2021-02-09 10:58:16 -0500

    update docs

--- evalml/preprocessing/data_splitters/base_splitters.py
+++ evalml/preprocessing/data_splitters/base_splitters.py
@@ -28,7 +28,7 @@ class BaseTVSplit(BaseCrossValidator):
                 y (ww.DataTable): DataColumn of points to split
 
         Returns:
-            tuple(ww.DataTable, ww.DataColumn): A tuple containing the resulting [(X_train, y_train), (X_test, y_test)] post-transformation.
+            tuple(ww.DataTable, ww.DataColumn): An iterator containing the resulting ((X_train, y_train), (X_test, y_test)) post-transformation.
         """
         X, y = _convert_numeric_dataset(X, y)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=self.test_size, random_state=self.random_state)
@@ -67,7 +67,7 @@ class BaseCVSplit(StratifiedKFold):
                 y (ww.DataTable): DataColumn of points to split
 
         Returns:
-            tuple(ww.DataTable, ww.DataColumn): A tuple containing the resulting [(X_train, y_train), (X_test, y_test)] post-transformation.
+            tuple((ww.DataTable, ww.DataColumn), (ww.DataTable, ww.DataColumn)): An iterator containing the resulting ((X_train, y_train), (X_test, y_test)) post-transformation.
         """
         X, y = _convert_numeric_dataset(X, y)
         for i, (train_indices, test_indices) in enumerate(super().split(X, y)):
