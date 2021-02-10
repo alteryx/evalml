26b0af874c71076ff75a81c8a303a4ff726138c0
Jack Carroll <jpcarrrolll@gmail.com>
2021-02-10 16:43:26 -0500

    fixed spacing

--- evalml/data_checks/class_imbalance_data_check.py
+++ evalml/data_checks/class_imbalance_data_check.py
@@ -49,15 +49,15 @@ class ClassImbalanceDataCheck(DataCheck):
             >>> y = pd.Series([0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
             >>> target_check = ClassImbalanceDataCheck(threshold=0.10)
             >>> assert target_check.validate(X, y) == {"errors": [{"message": "The number of instances of these targets is less than 2 * the number of cross folds = 6 instances: [0]",\
-                                                                "data_check_name": "ClassImbalanceDataCheck",\
-                                                                "level": "error",\
-                                                                "code": "CLASS_IMBALANCE_BELOW_FOLDS",\
-                                                                "details": {"target_values": [0]}}],\
-                                                      "warnings": [{"message": "The following labels fall below 10% of the target: [0]",\
-                                                                "data_check_name": "ClassImbalanceDataCheck",\
-                                                                "level": "warning",\
-                                                                "code": "CLASS_IMBALANCE_BELOW_THRESHOLD",\
-                                                                "details": {"target_values": [0]}}]}
+                                                                   "data_check_name": "ClassImbalanceDataCheck",\
+                                                                   "level": "error",\
+                                                                   "code": "CLASS_IMBALANCE_BELOW_FOLDS",\
+                                                                   "details": {"target_values": [0]}}],\
+                                                     "warnings": [{"message": "The following labels fall below 10% of the target: [0]",\
+                                                                   "data_check_name": "ClassImbalanceDataCheck",\
+                                                                   "level": "warning",\
+                                                                   "code": "CLASS_IMBALANCE_BELOW_THRESHOLD",\
+                                                                   "details": {"target_values": [0]}}]}
         """
         messages = {
             "warnings": [],
