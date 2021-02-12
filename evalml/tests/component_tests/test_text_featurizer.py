2f99bcb795d23ac9d5f9984ca852a9917c58e850
Angela Lin <angela97lin@gmail.com>
2021-02-12 00:32:44 -0500

    actually linting

--- evalml/tests/component_tests/test_text_featurizer.py
+++ evalml/tests/component_tests/test_text_featurizer.py
@@ -3,12 +3,7 @@ import pandas as pd
 import pytest
 import woodwork as ww
 from pandas.testing import assert_frame_equal, assert_series_equal
-from woodwork.logical_types import (
-    Boolean,
-    Categorical,
-    Double,
-    Integer
-)
+from woodwork.logical_types import Boolean, Categorical, Double, Integer
 
 from evalml.pipelines.components import TextFeaturizer
 from evalml.utils import infer_feature_types
