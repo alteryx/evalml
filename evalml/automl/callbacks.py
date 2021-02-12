cd08d7fc5228695ece7d8f3d2f62ff1a49eb00c3
bchen1116 <bchen1116@gmail.com>
2021-02-12 14:05:25 -0500

    fix linting

--- evalml/automl/callbacks.py
+++ evalml/automl/callbacks.py
@@ -36,14 +36,15 @@ def log_error_callback(exception, traceback, automl, **kwargs):
     """Logs the exception thrown as an error. Will not throw. This is the default behavior for AutoMLSearch."""
     fold_num = kwargs.get('fold_num')
     pipeline = kwargs.get('pipeline')
+    trace = '\n'.join(traceback)
     if isinstance(exception, PipelineScoreError):
         logger.info(f"\t\t\tFold {fold_num}: Encountered an error scoring the following objectives: {', '.join(exception.exceptions)}.")
         logger.info(f"\t\t\tFold {fold_num}: The scores for these objectives will be replaced with nan.")
+        trace += f"\n{exception.message}"
     else:
         logger.info(f"\t\t\tFold {fold_num}: Encountered an error.")
         logger.info(f"\t\t\tFold {fold_num}: All scores will be replaced with nan.")
     logger.info(f"\t\t\tFold {fold_num}: Please check {logger.handlers[1].baseFilename} for the current hyperparameters and stack trace.")
-    logger.debug(f"\t\t\tFold {fold_num}: Hyperparameters:\n\t{pipeline.hyperparameters}")
-    trace = '\n'.join(traceback)
-    logger.debug(f"\t\t\tFold {fold_num}: Traceback:\n{trace}")
     logger.info(f"\t\t\tFold {fold_num}: Exception during automl search: {str(exception)}")
+    logger.debug(f"\t\t\tFold {fold_num}: Hyperparameters:\n\t{pipeline.hyperparameters}")
+    logger.debug(f"\t\t\tFold {fold_num}: Traceback:\n{trace}")
