637c52bec192ed862edbd97cd74b56f9cc2b4fd4
Dylan Sherry <sharshofski@gmail.com>
2021-02-15 23:29:36 -0500

    Lint

--- evalml/tests/automl_tests/engine_tests/test_engine_base.py
+++ evalml/tests/automl_tests/engine_tests/test_engine_base.py
@@ -38,7 +38,7 @@ def test_train_and_score_pipelines(mock_fit, mock_score, dummy_binary_pipeline_c
     assert mock_score.call_count == automl.data_splitter.get_n_splits()
     assert evaluation_result.get('training_time') is not None
     assert evaluation_result.get('cv_score_mean') == 0.42
-    pd.testing.assert_series_equal(evaluation_result.get('cv_scores'), pd.Series([0.42]*3))
+    pd.testing.assert_series_equal(evaluation_result.get('cv_scores'), pd.Series([0.42] * 3))
     for i in range(automl.data_splitter.get_n_splits()):
         assert evaluation_result['cv_data'][i]['all_objective_scores']['Log Loss Binary'] == 0.42
 
@@ -56,7 +56,7 @@ def test_train_and_score_pipelines_error(mock_fit, mock_score, dummy_binary_pipe
     assert mock_score.call_count == automl.data_splitter.get_n_splits()
     assert evaluation_result.get('training_time') is not None
     assert np.isnan(evaluation_result.get('cv_score_mean'))
-    pd.testing.assert_series_equal(evaluation_result.get('cv_scores'), pd.Series([np.nan]*3))
+    pd.testing.assert_series_equal(evaluation_result.get('cv_scores'), pd.Series([np.nan] * 3))
     for i in range(automl.data_splitter.get_n_splits()):
         assert np.isnan(evaluation_result['cv_data'][i]['all_objective_scores']['Log Loss Binary'])
     assert 'yeet' in caplog.text
