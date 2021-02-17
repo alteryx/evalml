80ce70cfba92f54c8f99bd5954e3c083c185759b
bchen1116 <bchen1116@gmail.com>
2021-02-17 16:50:32 -0500

    working ontesting

--- evalml/preprocessing/data_splitters/__init__.py
+++ evalml/preprocessing/data_splitters/__init__.py
@@ -1,2 +1,3 @@
 from .training_validation_split import TrainingValidationSplit
 from .time_series_split import TimeSeriesSplit
+from .balanced_classification_splitter import BalancedClassificationSampler
