85ffbfe6bc6533cac0b50c58b39227aaacdfafe8
Angela Lin <angela97lin@gmail.com>
2021-02-24 14:03:51 -0500

    cleanup

--- evalml/pipelines/pipeline_meta.py
+++ evalml/pipelines/pipeline_meta.py
@@ -6,6 +6,27 @@ from evalml.exceptions import PipelineNotYetFittedError
 from evalml.utils.base_meta import BaseMeta
 
 
+# class PipelineBaseMeta(BaseMeta):
+#     """Metaclass that overrides creating a new pipeline by wrapping methods with validators and setters"""
+
+#     @classmethod
+#     def check_for_fit(cls, method):
+#         """`check_for_fit` wraps a method that validates if `self._is_fitted` is `True`.
+#             It raises an exception if `False` and calls and returns the wrapped method if `True`.
+#         """
+#         @wraps(method)
+#         def _check_for_fit(self, X=None, y=None, objective=None):
+#             klass = type(self).__name__
+#             if not self._is_fitted:
+#                 raise PipelineNotYetFittedError(f'This {klass} is not fitted yet. You must fit {klass} before calling {method.__name__}.')
+#             elif X is None and y is None:
+#                 return method(self)
+#             elif y is None:
+#                 return method(self, X)
+#             else:
+#                 return method(self, X, y)
+#         return _check_for_fit
+
 class PipelineBaseMeta(BaseMeta):
     """Metaclass that overrides creating a new pipeline by wrapping methods with validators and setters"""
 
@@ -15,16 +36,16 @@ class PipelineBaseMeta(BaseMeta):
             It raises an exception if `False` and calls and returns the wrapped method if `True`.
         """
         @wraps(method)
-        def _check_for_fit(self, X=None, y=None, objective=None):
+        def _check_for_fit(self, X=None, objective=None):
             klass = type(self).__name__
             if not self._is_fitted:
                 raise PipelineNotYetFittedError(f'This {klass} is not fitted yet. You must fit {klass} before calling {method.__name__}.')
-            elif X is None and y is None:
-                return method(self)
-            elif y is None:
+            if method.__name__ == 'predict_proba':
                 return method(self, X)
+            elif method.__name__ == 'predict':
+                return method(self, X, objective)
             else:
-                return method(self, X, y)
+                return method(self)
         return _check_for_fit
 
 
@@ -41,13 +62,11 @@ class TimeSeriesPipelineBaseMeta(PipelineBaseMeta):
             klass = type(self).__name__
             if not self._is_fitted:
                 raise PipelineNotYetFittedError(f'This {klass} is not fitted yet. You must fit {klass} before calling {method.__name__}.')
-            elif X is None and y is None:
-                return method(self)
-            elif y is None:
-                return method(self, X)
-            # For time series classification pipelines, predict will take X, y, objective
+            if method.__name__ == 'predict_proba':
+                return method(self, X, y)
             elif method.__name__ == 'predict':
                 return method(self, X, y, objective)
             else:
-                return method(self, X, y)
+                return method(self)
         return _check_for_fit
+
