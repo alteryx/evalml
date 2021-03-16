9bf61303b98a44e8d6a90a5664682f597d9570ae
Karsten Chu <karsten.chu@alteryx.com>
2021-03-16 12:27:10 -0400

    Updates.

--- evalml/tests/automl_tests/test_dask_engine.py
+++ evalml/tests/automl_tests/test_dask_engine.py
@@ -3,28 +3,44 @@ import time
 from collections import namedtuple
 from dask.distributed import Client
 from sklearn import datasets
+from unittest.mock import MagicMock, patch
 
+import woodwork as ww
 from evalml.objectives.utils import get_objective
 from evalml.pipelines import BinaryClassificationPipeline
+from evalml.automl.engine.engine_base import evaluate_pipeline
 from evalml.automl.engine.dask_engine import DaskEngine
 from evalml.automl.engine.sequential_engine import SequentialEngine
-
-# client = Client()
+from evalml.preprocessing.data_splitters import RandomUnderSamplerCVSplit
 
 AutoMLSearchStruct = namedtuple("AutoML",
-                                "data_splitter problem_type objective additional_objectives optimize_thresholds error_callback random_seed")
+                                "data_splitter problem_type objective additional_objectives optimize_thresholds error_callback random_seed ensembling_indices")
+data_splitter = "K Fold"
+problem_type = "binary"
+objective = get_objective("Log Loss Binary")
+additional_objectives = []
+optimize_thresholds = False
+error_callback = lambda x: 1
+random_seed = 0
+ensembling_indices = [0]
+automl_data = AutoMLSearchStruct(data_splitter, problem_type, objective, additional_objectives,
+                                 optimize_thresholds, error_callback, random_seed, ensembling_indices)
 
 
-def test_automl_data():
-    data_splitter = "Stratified K Fold"
+class TestBinaryClassificationPipeline(BinaryClassificationPipeline):
+    component_graph = ["Logistic Regression Classifier"]
+
+def mock_automl_data():
+    data_splitter = MagicMock()
     problem_type = "binary"
     objective = get_objective("Log Loss Binary")
     additional_objectives = []
     optimize_thresholds = False
     error_callback = lambda x: 1
     random_seed = 0
+    ensembling_indices = [0]
     automl_data = AutoMLSearchStruct(data_splitter, problem_type, objective, additional_objectives,
-                              optimize_thresholds, error_callback, random_seed)
+                              optimize_thresholds, error_callback, random_seed, ensembling_indices)
     return automl_data
 
 def test_init():
@@ -36,64 +52,75 @@ def test_init():
         DaskEngine(client="Client")
 
 
-def test_submit_single_training_job(X_y_binary):
+def test_submit_training_job_single(X_y_binary):
     X,y = X_y_binary
-    automl_data = test_automl_data()
+    automl_data = mock_automl_data()
 
     class TestBinaryClassificationPipeline(BinaryClassificationPipeline):
         component_graph = ["Logistic Regression Classifier"]
+    pipeline = TestBinaryClassificationPipeline({})
+
+    # Client needs to be instantiated inside the test or it hangs.
+    client = Client()
+    engine = DaskEngine(client=client)
+    pipeline_future = engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline)
+    pipeline_fitted = pipeline_future.get_result()
+    assert pipeline_fitted._is_fitted
+    client.close()
+
+
+def test_submit_training_jobs_multiple(X_y_binary):
+    X, y = datasets.make_classification(n_samples=40000, n_features=20,
+                                        n_informative=2, n_redundant=2, random_state=0)
+
+    automl_data = mock_automl_data()
+
+    class TestBinaryClassificationPipeline(BinaryClassificationPipeline):
+        component_graph = ["Logistic Regression Classifier"]
+
+    def fit_pipelines(engine):
+        num_pipelines = 4
+        pipelines = []
+        for i in range(num_pipelines):
+            pipelines.append(TestBinaryClassificationPipeline({}))
+
+        time_start = time.time()
+        futures = []
+        for pipeline in pipelines:
+            futures.append(engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline))
+        results = [f.get_result() for f in futures]
+        time_taken = time.time()-time_start
+        return results, time_taken
+
+    # Client needs to be instantiated inside the test or it hangs.
+    client = Client()
+    par_pipelines, par_time_taken = fit_pipelines(DaskEngine(client=client))
+    for pipeline in par_pipelines:
+        assert pipeline._is_fitted
+    client.close()
+
+    seq_pipelines, seq_time_taken = fit_pipelines(SequentialEngine())
+    for pipeline in seq_pipelines:
+        assert pipeline._is_fitted
+
+    print(seq_time_taken, par_time_taken)
+
+@patch(evaluate_pipeline)
+def test_submit_evaluate_job_single(mock_evaluate_pipeline, X_y_binary):
+    mock_evaluate_pipeline.return_value = 1
+
+    X,y = X_y_binary
+    # automl_data = mock_automl_data()
 
     pipeline = TestBinaryClassificationPipeline({})
 
     # Client needs to be instantiated inside the test or it hangs.
     client = Client()
     engine = DaskEngine(client=client)
+    pipeline_future = engine.submit_evaluation_job(X=ww.DataTable(X), y=ww.DataColumn(y),
+                                                   automl_data=automl_data, pipeline=pipeline)
+    pipeline_fitted = pipeline_future.get_result()
 
-    future = engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline)
-
-    ans = future.get_result()
-    print(ans)
     client.close()
-
-
-def test_submit_multiple_training_jobs(X_y_binary):
-    X, y = datasets.make_classification(n_samples=40000, n_features=20,
-                                        n_informative=2, n_redundant=2, random_state=0)
-
-    automl_data = test_automl_data()
-
-    class TestBinaryClassificationPipeline(BinaryClassificationPipeline):
-        component_graph = ["Logistic Regression Classifier"]
-
-    num_pipelines = 8
-    pipelines = []
-    for i in range(num_pipelines):
-        pipelines.append(TestBinaryClassificationPipeline({}))
-
-    # Client needs to be instantiated inside the test or it hangs.
-
-    parallel_time_start = time.time()
-    client = Client()
-    engine = DaskEngine(client=client)
-    futures = []
-    for pipeline in pipelines:
-        futures.append(engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline))
-    results = [f.get_result() for f in futures]
-    parallel_time_taken = time.time()-parallel_time_start
-    client.close()
-    print(f"Parallel_time_taken: {parallel_time_taken}")
-    print(results)
-
-    sequential_time_start = time.time()
-    engine = SequentialEngine()
-    futures = []
-    for pipeline in pipelines:
-        futures.append(engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline))
-    results = [f.get_result() for f in futures]
-    sequential_time_taken = time.time()-sequential_time_start
-    print(f"Sequential_time_taken: {sequential_time_taken}")
-    print(results)
-
-    print(sequential_time_taken, parallel_time_taken)
     import pdb; pdb.set_trace()
 
