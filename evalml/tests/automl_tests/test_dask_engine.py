ce95139577000793c2e32978592775b2674a00df
Karsten Chu <karsten.chu@alteryx.com>
2021-03-18 16:17:00 -0400

    Updated tests.

--- evalml/tests/automl_tests/test_dask_engine.py
+++ evalml/tests/automl_tests/test_dask_engine.py
@@ -2,10 +2,14 @@ import time
 import unittest
 import pytest
 import numpy as np
+import pandas as pd
 from distributed import Client
 
 import woodwork as ww
 
+from evalml.pipelines.pipeline_base import PipelineBase
+from evalml.automl.engine.engine_base import JobLogger
+from evalml.automl.engine.engine_base import train_pipeline, evaluate_pipeline
 from evalml.automl.engine.dask_engine import DaskComputation, DaskEngine
 from evalml.automl.engine.sequential_engine import SequentialEngine
 from evalml.tests.automl_tests.dask_testing import (
@@ -14,6 +18,10 @@ from evalml.tests.automl_tests.dask_testing import (
     TestSVMPipeline,
     automl_data
 )
+
+from evalml.automl.engine.engine_base import train_pipeline, evaluate_pipeline, train_and_score_pipeline
+def score_pipeline(pipeline, X, y, objectives):
+    return pipeline.score(X,y,objectives)
 
 
 @pytest.mark.usefixtures("X_y_binary_cls")
@@ -31,35 +39,44 @@ class TestDaskEngine(unittest.TestCase):
             DaskEngine(client="Client")
 
     def test_submit_training_job_single(self):
+        """ Test that training a single pipeline using the parallel engine produces the
+        same results as simply running the train_pipeline function. """
         X, y = self.X_y_binary
         pipeline = TestLRCPipeline({})
-
         engine = DaskEngine(client=self.client)
+
+        # Verify that engine fits a pipeline
         pipeline_future = engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline)
         pipeline_fitted = pipeline_future.get_result()
         assert pipeline_fitted._is_fitted
 
+        # Verify parallelization has no affect on output of function
+        original_pipeline_fitted = train_pipeline(pipeline, X, y, optimize_thresholds=automl_data.optimize_thresholds,
+                                                  objective=automl_data.objective)
+        assert pipeline_fitted == original_pipeline_fitted
+
     def test_submit_training_jobs_multiple(self):
+        """ Test that training multiple pipelines using the parallel engine produces the
+        same results as the sequential engine. """
         X, y = self.X_y_binary
         pipelines = [TestLRCPipeline({}),
                      TestCBPipeline({}),
                      TestSVMPipeline({})]
 
         def fit_pipelines(pipelines, engine):
-            time_start = time.time()
             futures = []
             for pipeline in pipelines:
                 futures.append(engine.submit_training_job(X=X, y=y, automl_data=automl_data, pipeline=pipeline))
             results = [f.get_result() for f in futures]
-            time_taken = time.time() - time_start
-            return results, time_taken
+            return results
 
-        seq_pipelines, seq_time_taken = fit_pipelines(pipelines, SequentialEngine())
+        # Verify all pipelines are trained and fitted.
+        seq_pipelines= fit_pipelines(pipelines, SequentialEngine())
         for pipeline in seq_pipelines:
             assert pipeline._is_fitted
 
-        # Check that pipelines are fitted
-        par_pipelines, par_time_taken = fit_pipelines(pipelines, DaskEngine(client=self.client))
+        # Verify all pipelines are trained and fitted.
+        par_pipelines= fit_pipelines(pipelines, DaskEngine(client=self.client))
         for pipeline in par_pipelines:
             assert pipeline._is_fitted
 
@@ -68,71 +85,110 @@ class TestDaskEngine(unittest.TestCase):
         for par_pipeline in par_pipelines:
             assert par_pipeline in seq_pipelines
 
-        print(seq_time_taken, par_time_taken)
-
     def test_submit_evaluate_job_single(self):
+        """ Test that evaluating a single pipeline using the parallel engine produces the
+        same results as simply running the evaluate_pipeline function. """
         X, y = self.X_y_binary
+        X = ww.DataTable(X)
+        y = ww.DataColumn(y)
         pipeline = TestLRCPipeline({})
         engine = DaskEngine(client=self.client)
 
-        pipeline_future = engine.submit_evaluation_job(X=ww.DataTable(X), y=ww.DataColumn(y),
+        # Verify that engine evaluates a pipeline
+        pipeline_future = engine.submit_evaluation_job(X=X, y=y,
                                                        automl_data=automl_data, pipeline=pipeline)
         assert isinstance(pipeline_future, DaskComputation)
-        pipeline_results = pipeline_future.get_result()
+
+        par_eval_results = pipeline_future.get_result()
+
+        original_eval_results = evaluate_pipeline(pipeline, automl_data=automl_data, X=X, y=y, logger=JobLogger())
+
+        # Ensure we get back the same output as the parallelized function.
+        assert len(par_eval_results) == 3
+
+        # Compare cross validation information except training time.
+        assert isinstance(par_eval_results[0], dict)
+        assert par_eval_results[0]["cv_data"] == original_eval_results[0]["cv_data"]
+        assert all(par_eval_results[0]["cv_scores"] == original_eval_results[0]["cv_scores"])
+        assert par_eval_results[0]["cv_score_mean"] == original_eval_results[0]["cv_score_mean"]
+
+        # Make sure the resulting pipelines are the same.
+        assert isinstance(par_eval_results[1], PipelineBase)
+        assert par_eval_results[1] == original_eval_results[1]
+
+        # Make sure a properly filled logger comes back.
+        assert isinstance(par_eval_results[2], JobLogger)
+        assert par_eval_results[2].logs == original_eval_results[2].logs
 
     def test_submit_evaluate_jobs_multiple(self):
+        """ Test that evaluating multiple pipelines using the parallel engine produces the
+        same results as the sequential engine. """
         X, y = self.X_y_binary
         pipelines = [TestLRCPipeline({}),
                      TestCBPipeline({}),
                      TestSVMPipeline({})]
 
         def eval_pipelines(pipelines, engine):
-            time_start = time.time()
             futures = []
             for pipeline in pipelines:
                 futures.append(engine.submit_evaluation_job(X=ww.DataTable(X), y=ww.DataColumn(y),
                                                             automl_data=automl_data, pipeline=pipeline))
             results = [f.get_result() for f in futures]
-            time_taken = time.time() - time_start
-            return results, time_taken
+            return results
 
-        par_eval_results, par_time_taken = eval_pipelines(pipelines, DaskEngine(client=self.client))
-
-        # Extract result information from parallel evaluation
+        par_eval_results = eval_pipelines(pipelines, DaskEngine(client=self.client))
         par_dicts = [s[0] for s in par_eval_results]
         par_scores = [s["cv_data"][0]["score"] for s in par_dicts]
         par_pipelines = [s[1] for s in par_eval_results]
+        
+        seq_eval_results = eval_pipelines(pipelines, SequentialEngine())
+        seq_dicts = [s[0] for s in seq_eval_results]
+        seq_scores = [s["cv_data"][0]["score"] for s in seq_dicts]
+        seq_pipelines = [s[1] for s in seq_eval_results]
 
-        # Check all pipelines are fitted, there are the proper number of pipelines and all their scores are reasonable.
+        # Ensure all pipelines are fitted.
         assert all([s._is_fitted for s in par_pipelines])
-        assert len(par_eval_results) == len(pipelines)
+
+        # Ensure the scores in parallel and sequence are same
+        assert set(par_scores) == set(seq_scores)
         assert not any([np.isnan(s) for s in par_scores])
 
+        # Ensure the parallel and sequence pipelines match
+        assert len(par_pipelines) == len(seq_pipelines)
+        for par_pipeline in par_pipelines:
+            assert par_pipeline in seq_pipelines
+
     def test_submit_scoring_job_single(self):
+        """ Test that scoring a single pipeline using the parallel engine produces the
+        same results as simply running the score_pipeline function. """
         X, y = self.X_y_binary
         pipeline = TestLRCPipeline({})
         engine = DaskEngine(client=self.client)
+        objectives = [automl_data.objective]
 
         pipeline_future = engine.submit_training_job(X=ww.DataTable(X), y=ww.DataColumn(y),
                                                      automl_data=automl_data, pipeline=pipeline)
-        assert isinstance(pipeline_future, DaskComputation)
         pipeline = pipeline_future.get_result()
-        pipeline_future = engine.submit_scoring_job(X=ww.DataTable(X), y=ww.DataColumn(y),
+        pipeline_score_future = engine.submit_scoring_job(X=ww.DataTable(X), y=ww.DataColumn(y),
                                                     automl_data=automl_data, pipeline=pipeline,
-                                                    objectives=[automl_data.objective])
-        assert isinstance(pipeline_future, DaskComputation)
-        pipeline_results = pipeline_future.get_result()
+                                                    objectives=objectives)
+        assert isinstance(pipeline_score_future, DaskComputation)
+        pipeline_score = pipeline_score_future.get_result()
 
-        assert not np.isnan(pipeline_results["Log Loss Binary"])
+        original_pipeline_score = pipeline.score(X=X, y=y, objectives=objectives)
+
+        assert not np.isnan(pipeline_score["Log Loss Binary"])
+        assert pipeline_score == original_pipeline_score
 
     def test_submit_scoring_jobs_multiple(self):
+        """ Test that scoring multiple pipelines using the parallel engine produces the
+        same results as the sequential engine. """
         X, y = self.X_y_binary
         pipelines = [TestLRCPipeline({}),
                      TestCBPipeline({}),
                      TestSVMPipeline({})]
 
         def score_pipelines(pipelines, engine):
-            time_start = time.time()
             futures = []
             for pipeline in pipelines:
                 futures.append(engine.submit_training_job(X=ww.DataTable(X), y=ww.DataColumn(y),
@@ -144,18 +200,40 @@ class TestDaskEngine(unittest.TestCase):
                                                          automl_data=automl_data, pipeline=pipeline,
                                                          objectives=[automl_data.objective]))
             results = [f.get_result() for f in futures]
-            time_taken = time.time() - time_start
-            return results, time_taken
+            return results
 
-        par_eval_results, par_time_taken = score_pipelines(pipelines, DaskEngine(client=self.client))
+        par_eval_results = score_pipelines(pipelines, DaskEngine(client=self.client))
         par_scores = [s["Log Loss Binary"] for s in par_eval_results]
 
-        seq_eval_results, seq_time_taken = score_pipelines(pipelines, SequentialEngine())
+        seq_eval_results = score_pipelines(pipelines, SequentialEngine())
         seq_scores = [s["Log Loss Binary"] for s in seq_eval_results]
 
         # Check there are the proper number of pipelines and all their scores are same.
         assert len(par_eval_results) == len(pipelines)
         assert set(par_scores) == set(seq_scores)
+
+    def test_freddy(self):
+        X, y = self.X_y_binary
+        pipelines = [TestLRCPipeline({}),
+                     TestCBPipeline({}),
+                     TestSVMPipeline({})]
+
+        batch_futures = []
+        for pipeline in pipelines:
+            p_f = self.client.submit(train_pipeline, pipeline=pipeline,
+                                     X=X, y=y, optimize_thresholds=True,
+                                     objective=automl_data.objective)
+            p_f = self.client.submit(score_pipeline, pipeline=p_f, X=X, y=y,
+                                     objectives=[automl_data.objective])
+            batch_futures.append(p_f)
+        batch_scores = self.client.gather(batch_futures)
+        """
+        assert batch_scores == [OrderedDict([('Log Loss Binary', 0.17764440547651003)]),
+                               OrderedDict([('Log Loss Binary', 0.4841126635831677)]),
+                               OrderedDict([('Log Loss Binary', 0.11584614593690136)])
+        """
+        # import pdb; pdb.set_trace()
+
 
     @classmethod
     def tearDownClass(cls) -> None:
