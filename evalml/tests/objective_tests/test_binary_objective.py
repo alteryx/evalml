d10c941781cacc26222c67a34fe7c4dd02aa390d
Kelsey Skvoretz <skvorekn@miamioh.edu>
2021-03-20 14:04:58 -0400

    feat: binary objective test base class

---
+++ evalml/tests/objective_tests/test_binary_objective.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod, ABCMeta
+import pytest
+
+from evalml import AutoMLSearch
+
+class TestBinaryObjective(metaclass=ABCMeta):
+    __test__ = False
+
+    def assign_problem_type(self):
+        self.problem_type = 'binary'
+
+    def get_data(self, X_y_binary):
+        self.X, self.y = X_y_binary
+
+    @abstractmethod
+    def assign_objective(self, **kwargs):
+        """Get objective object using specified parameters
+        """
+
+    def run_pipeline(self, **kwargs):
+        automl = AutoMLSearch(X_train=self.X, y_train=self.y, problem_type=self.problem_type, objective=self.objective, max_iterations=1)
+        automl.search()
+
+        pipeline = automl.best_pipeline
+        pipeline.fit(self.X, self.y)
+        pipeline.predict(self.X, self.objective)
+        pipeline.predict_proba(self.X)
+        pipeline.score(self.X, self.y, [self.objective])
+
+    # def test_input_contains_nan():
+
+    # def test_input_contains_inf():
+
+    # def test_input_lengths():
+
+    # def test_zero_input_lengths():
+
+    # def test_binary_more_than_two_unique_values():
+
+    # def test_objective_score():
