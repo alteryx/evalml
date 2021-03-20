f762b68626ade9833d9d4db17b726f692c709b90
Kelsey Skvoretz <skvorekn@miamioh.edu>
2021-03-20 14:05:21 -0400

    test: sla using base class

---
+++ evalml/tests/objective_tests/test_sla.py
@@ -0,0 +1,33 @@
+import pytest
+
+from evalml import AutoMLSearch
+from evalml.objectives import SLA
+from evalml.tests.objective_tests.test_binary_objective import TestBinaryObjective
+
+class TestSLA(TestBinaryObjective):
+    __test__ = True
+
+    def assign_objective(self, alert_rate):
+        self.objective = SLA(alert_rate)
+
+    def test_sla_objective(self, X_y_binary):
+        self.assign_problem_type()
+        self.assign_objective(0.1)
+        self.get_data(X_y_binary)
+        self.run_pipeline()
+
+    @pytest.mark.parametrize("alert_rate", [0.01, 0.99])
+    def test_valid_alert_rate(self, alert_rate):
+        object = SLA(alert_rate)
+        assert object.alert_rate == alert_rate
+
+    @pytest.mark.parametrize("alert_rate", [-1, 1.5])
+    def test_invalid_alert_rate(self, alert_rate):
+        with pytest.raises(Exception):
+            SLA(alert_rate)
+
+    # def test_single_pred_proba():
+
+    # def test_extreme_threshold():
+
+    # def test_sla_score():
