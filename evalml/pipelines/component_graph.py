8080d41ae93eb4f1945da76a92c2b0d8ef69463f
bchen1116 <bchen1116@gmail.com>
2021-03-22 13:55:36 -0400

    lint

--- evalml/pipelines/component_graph.py
+++ evalml/pipelines/component_graph.py
@@ -54,7 +54,7 @@ class ComponentGraph:
 
             component_dict[component_name] = [component_class]
             if previous_component is not None:
-                    component_dict[component_name].extend([f"{previous_component}.x", f"{previous_component}.y"])
+                component_dict[component_name].extend([f"{previous_component}.x", f"{previous_component}.y"])
                 # component_dict[component_name].append(f"{previous_component}.y")
             previous_component = component_name
         return cls(component_dict, random_seed=random_seed)
