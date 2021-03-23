24de1e0f3c08e44db82e63c483eb1633ea54b6d8
freddyaboulton <alfonsoboulton@gmail.com>
2021-03-23 19:11:04 -0400

    sorting imports

--- evalml/automl/engine/dask_engine.py
+++ evalml/automl/engine/dask_engine.py
@@ -4,8 +4,8 @@ from evalml.automl.engine.engine_base import (
     EngineBase,
     EngineComputation,
     evaluate_pipeline,
-    train_pipeline,
-    score_pipeline
+    score_pipeline,
+    train_pipeline
 )
 
 
