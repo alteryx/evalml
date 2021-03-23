713e9a1108662f4bf2986ac750b4670f0b570617
Jeremy Shih <jeremyliweishih@gmail.com>
2021-03-23 15:03:42 -0400

    Add drop columns component when there are index columns

--- evalml/pipelines/utils.py
+++ evalml/pipelines/utils.py
@@ -66,6 +66,10 @@ def _get_preprocessing_components(X, y, problem_type, estimator_class):
     text_columns = list(X.select('natural_language').columns)
     if len(text_columns) > 0:
         pp_components.append(TextFeaturizer)
+    
+    index_columns = list(X.select('index').columns)
+    if len(index_columns) > 0:
+        pp_components.append(DropColumns)
 
     datetime_cols = X.select(["Datetime"])
     add_datetime_featurizer = len(datetime_cols.columns) > 0
