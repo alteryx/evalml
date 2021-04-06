0b4a64c41682a02c7ae9acf34efe5748e4f7cfbe
bchen1116 <bchen1116@gmail.com>
2021-04-06 12:23:20 -0400

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -266,7 +266,7 @@ def setup(app):
     print(os.path.abspath(os.getcwd()))
     for folder in glob.glob("/home/docs/.ipython/*"):
         print(folder)
-    shutil.copy("disable-warnings.py", "/home/docs/.ipython/profile_default/startup")
+    shutil.copy("disable-warnings.py", "/home/docs/.ipython/profile_default/startup/disable-warnings.py")
     app.add_javascript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js')
     app.add_stylesheet("style.css")
     app.add_autodocumenter(AccessorCallableDocumenter)
