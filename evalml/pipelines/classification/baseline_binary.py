e377212a45f7d2edf1b8f99f10bcc040aebca9d2
Angela Lin <angela97lin@gmail.com>
2021-04-06 13:59:34 -0400

    hmm

--- evalml/pipelines/classification/baseline_binary.py
+++ evalml/pipelines/classification/baseline_binary.py
@@ -4,6 +4,7 @@ from evalml.pipelines import BinaryClassificationPipeline
 class BaselineBinaryPipeline(BinaryClassificationPipeline):
     """Baseline Pipeline for binary classification."""
     custom_name = "Baseline Classification Pipeline"
+    name = "Baseline Classification Pipeline"
     component_graph = ["Baseline Classifier"]
 
     def __init__(self, parameters, random_seed=0):
@@ -12,6 +13,7 @@ class BaselineBinaryPipeline(BinaryClassificationPipeline):
 
 class ModeBaselineBinaryPipeline(BinaryClassificationPipeline):
     """Mode Baseline Pipeline for binary classification."""
+    name = "Mode Baseline Binary Classification Pipeline"
     custom_name = "Mode Baseline Binary Classification Pipeline"
     component_graph = ["Baseline Classifier"]
     custom_hyperparameters = {"strategy": ["mode"]}
