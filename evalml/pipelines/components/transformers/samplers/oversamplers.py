e4fd2d11a1a17076bca7d40a3dfe6c2ef7bd6cb3
bchen1116 <bchen1116@gmail.com>
2021-04-06 17:41:38 -0400

    fiox

--- evalml/pipelines/components/transformers/samplers/oversamplers.py
+++ evalml/pipelines/components/transformers/samplers/oversamplers.py
@@ -13,14 +13,14 @@ class SMOTESampler(BaseSampler):
     """SMOTE Oversampler component. Works on numerical datasets only. This component is only run during training and not during predict."""
     name = "SMOTE Oversampler"
 
-    def __init__(self, sampling_ratio=0.25, sampling_ratio_dict={}, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+    def __init__(self, sampling_ratio=0.25, sampling_ratio_dict=None, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
         """Initialize the SMOTE Oversampler component.
 
         Arguments:
             sampling_ratio (float): This is the goal ratio of the minority to majority class, with range (0, 1]. A value of 0.25 means we want a 1:4 ratio
                 of the minority to majority class after oversampling. If the targets are multiclass, will create a dictionary using this ratio. Defaults to 0.25.
             sampling_ratio_dict (dict): Dictionary which has keys corresponding to each class, and the values are the number of samples we want to oversample to for each class key.
-                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to empty.
+                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to None.
             k_neighbors (int): The number of nearest neighbors to used to construct synthetic samples. Defaults to 5.
             n_jobs (int): The number of CPU cores to use. Defaults to -1.
         """
@@ -46,7 +46,7 @@ class SMOTESampler(BaseSampler):
         """
         super().fit(X, y)
         sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
-        if len(self.parameters['sampling_ratio_dict']):
+        if self.parameters['sampling_ratio_dict'] is not None and len(self.parameters['sampling_ratio_dict']):
             # dictionary provided, which takes priority
             sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
         else:
@@ -82,7 +82,7 @@ class SMOTENCSampler(BaseSampler):
        This component is only run during training and not during predict."""
     name = "SMOTENC Oversampler"
 
-    def __init__(self, categorical_features=[], sampling_ratio=0.25, sampling_ratio_dict={}, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+    def __init__(self, categorical_features=[], sampling_ratio=0.25, sampling_ratio_dict=None, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
         """Initialize the SMOTENC Oversampler component.
 
         Arguments:
@@ -91,7 +91,7 @@ class SMOTENCSampler(BaseSampler):
             sampling_ratio (float): This is the goal ratio of the minority to majority class, with range (0, 1]. A value of 0.25 means we want a 1:4 ratio
                 of the minority to majority class after oversampling. If the targets are multiclass, will create a dictionary using this ratio. Defaults to 0.25.
             sampling_ratio_dict (dict): Dictionary which has keys corresponding to each class, and the values are the number of samples we want to oversample to for each class key.
-                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to empty.
+                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to None.
             k_neighbors (int): The number of nearest neighbors to used to construct synthetic samples. Defaults to 5.
             n_jobs (int): The number of CPU cores to use. Defaults to -1.
         """
@@ -119,7 +119,7 @@ class SMOTENCSampler(BaseSampler):
         """
         super().fit(X, y)
         sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
-        if len(self.parameters['sampling_ratio_dict']):
+        if self.parameters['sampling_ratio_dict'] is not None and len(self.parameters['sampling_ratio_dict']):
             # dictionary provided, which takes priority
             sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
         else:
@@ -148,14 +148,14 @@ class SMOTENSampler(BaseSampler):
        This component is only run during training and not during predict."""
     name = "SMOTEN Oversampler"
 
-    def __init__(self, sampling_ratio=0.25, sampling_ratio_dict={}, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+    def __init__(self, sampling_ratio=0.25, sampling_ratio_dict=None, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
         """Initialize the SMOTEN Oversampler component.
 
         Arguments:
             sampling_ratio (float): This is the goal ratio of the minority to majority class, with range (0, 1]. A value of 0.25 means we want a 1:4 ratio
                 of the minority to majority class after oversampling. If the targets are multiclass, will create a dictionary using this ratio. Defaults to 0.25.
             sampling_ratio_dict (dict): Dictionary which has keys corresponding to each class, and the values are the number of samples we want to oversample to for each class key.
-                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to empty.
+                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to None.
             k_neighbors (int): The number of nearest neighbors to used to construct synthetic samples. Defaults to 5.
             n_jobs (int): The number of CPU cores to use. Defaults to -1.
         """
@@ -182,7 +182,7 @@ class SMOTENSampler(BaseSampler):
         """
         super().fit(X, y)
         sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
-        if len(self.parameters['sampling_ratio_dict']):
+        if self.parameters['sampling_ratio_dict'] is not None and len(self.parameters['sampling_ratio_dict']):
             # dictionary provided, which takes priority
             sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
         else:
