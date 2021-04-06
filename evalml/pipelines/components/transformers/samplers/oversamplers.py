b8fe35b09252d7ff4ef2b116b3350accaf406536
bchen1116 <bchen1116@gmail.com>
2021-04-06 17:39:53 -0400

    support for sampling_ratio and sampling_ratio_dict

--- evalml/pipelines/components/transformers/samplers/oversamplers.py
+++ evalml/pipelines/components/transformers/samplers/oversamplers.py
@@ -1,8 +1,10 @@
+import copy
 from skopt.space import Integer
 
 from evalml.pipelines.components.transformers.samplers.base_sampler import (
     BaseSampler
 )
+from evalml.pipelines.components.utils import make_balancing_dictionary
 from evalml.utils import import_or_raise
 from evalml.utils.woodwork_utils import infer_feature_types
 
@@ -10,32 +12,54 @@ from evalml.utils.woodwork_utils import infer_feature_types
 class SMOTESampler(BaseSampler):
     """SMOTE Oversampler component. Works on numerical datasets only. This component is only run during training and not during predict."""
     name = "SMOTE Oversampler"
-    hyperparameter_ranges = {
-        'k_neighbors': Integer(2, 10)
-    }
 
-    def __init__(self, sampling_strategy='auto', k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+    def __init__(self, sampling_ratio=0.25, sampling_ratio_dict={}, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
         """Initialize the SMOTE Oversampler component.
 
         Arguments:
-            sampling_strategy (float, dictionary, str): If float, this is the goal ratio of the minority to majority class.
-                A value of 0.25 means we want a 1:4 ratio of the minority to majority class after oversampling.
-                If dictionary, the keys correspond to each class, and the values are the number of samples we want to oversample to for each class key.
-                Defaults to 'auto', which oversamples all classes to become equal.
-                Note that float is only compatible with binary classification problems, and dictionary or 'auto' should be used for multiclass.
+            sampling_ratio (float): This is the goal ratio of the minority to majority class, with range (0, 1]. A value of 0.25 means we want a 1:4 ratio
+                of the minority to majority class after oversampling. If the targets are multiclass, will create a dictionary using this ratio. Defaults to 0.25.
+            sampling_ratio_dict (dict): Dictionary which has keys corresponding to each class, and the values are the number of samples we want to oversample to for each class key.
+                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to empty.
             k_neighbors (int): The number of nearest neighbors to used to construct synthetic samples. Defaults to 5.
             n_jobs (int): The number of CPU cores to use. Defaults to -1.
         """
         error_msg = "imbalanced-learn is not installed. Please install using 'pip install imbalanced-learn'"
-        im = import_or_raise("imblearn.over_sampling", error_msg=error_msg)
-        parameters = {"sampling_strategy": sampling_strategy,
+        self.im = import_or_raise("imblearn.over_sampling", error_msg=error_msg)
+        parameters = {"sampling_ratio": sampling_ratio,
+                      "sampling_ratio_dict": sampling_ratio_dict,
                       "k_neighbors": k_neighbors,
                       "n_jobs": n_jobs}
         parameters.update(kwargs)
-        sampler = im.SMOTE(**parameters, random_state=random_seed)
         super().__init__(parameters=parameters,
-                         component_obj=sampler,
+                         component_obj=None,
                          random_seed=random_seed)
+    def fit(self, X, y):
+        """Fits the sampler to the data.
+
+        Arguments:
+            X (ww.DataFrame): Training features
+            y (ww.DataColumn): Target features
+
+        Returns:
+            self
+        """
+        super().fit(X, y)
+        sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
+        if len(self.parameters['sampling_ratio_dict']):
+            # dictionary provided, which takes priority
+            sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
+        else:
+            sampling_ratio = self.parameters['sampling_ratio']
+            # no dictionary provided. We pass the float if we have a binary situation
+            if len(y.value_counts()) == 2:
+                sampler_params['sampling_strategy'] = sampling_ratio if sampling_ratio != 1 else 'auto'
+            else:
+                # otherwise, we make the dictionary
+                dic = make_balancing_dictionary(y, sampling_ratio)
+                sampler_params['sampling_strategy'] = dic
+        sampler = self.im.SMOTE(**sampler_params, random_state=self.random_seed)
+        self._component_obj = sampler
 
     def fit_transform(self, X, y):
         """Fit and transform the data using the undersampler. Used during training of the pipeline
@@ -48,6 +72,7 @@ class SMOTESampler(BaseSampler):
             ww.DataTable, ww.DataColumn: Undersampled X and y data
         """
         _, _, X_pd, y_pd = self._prepare_data(X, y)
+        self.fit(X_pd, y_pd)
         X_new, y_new = self._component_obj.fit_resample(X_pd, y_pd)
         return infer_feature_types(X_new), infer_feature_types(y_new)
 
@@ -56,40 +81,64 @@ class SMOTENCSampler(BaseSampler):
     """SMOTENC Oversampler component. Uses SMOTENC to generate synthetic samples. Works on a mix of nomerical and categorical columns.
        This component is only run during training and not during predict."""
     name = "SMOTENC Oversampler"
-    hyperparameter_ranges = {
-        'k_neighbors': Integer(2, 10)
-    }
 
-    def __init__(self, categorical_features=[], sampling_strategy='auto', k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+    def __init__(self, categorical_features=[], sampling_ratio=0.25, sampling_ratio_dict={}, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
         """Initialize the SMOTENC Oversampler component.
 
         Arguments:
             categorical_features (list): A list of indices of the categorical columns, or a list of booleans for each column,
                 where True represents a categorical column and False represents a numeric. Defaults empty list.
-            sampling_strategy (float, dictionary, str): If float, this is the goal ratio of the minority to majority class.
-                A value of 0.25 means we want a 1:4 ratio of the minority to majority class after oversampling.
-                If dictionary, the keys correspond to each class, and the values are the number of samples we want to oversample to for each class key.
-                Defaults to 'auto', which oversamples all classes to become equal.
-                Note that float is only compatible with binary classification problems, and dictionary or 'auto' should be used for multiclass.
+            sampling_ratio (float): This is the goal ratio of the minority to majority class, with range (0, 1]. A value of 0.25 means we want a 1:4 ratio
+                of the minority to majority class after oversampling. If the targets are multiclass, will create a dictionary using this ratio. Defaults to 0.25.
+            sampling_ratio_dict (dict): Dictionary which has keys corresponding to each class, and the values are the number of samples we want to oversample to for each class key.
+                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to empty.
             k_neighbors (int): The number of nearest neighbors to used to construct synthetic samples. Defaults to 5.
             n_jobs (int): The number of CPU cores to use. Defaults to -1.
         """
         error_msg = "imbalanced-learn is not installed. Please install using 'pip install imbalanced-learn'"
-        im = import_or_raise("imblearn.over_sampling", error_msg=error_msg)
+        self.im = import_or_raise("imblearn.over_sampling", error_msg=error_msg)
         parameters = {"categorical_features": categorical_features,
-                      "sampling_strategy": sampling_strategy,
+                      "sampling_ratio": sampling_ratio,
+                      "sampling_ratio_dict": sampling_ratio_dict,
                       "k_neighbors": k_neighbors,
                       "n_jobs": n_jobs}
         parameters.update(kwargs)
-        sampler = im.SMOTENC(**parameters, random_state=random_seed)
         super().__init__(parameters=parameters,
-                         component_obj=sampler,
+                         component_obj=None,
                          random_seed=random_seed)
+
+    def fit(self, X, y):
+        """Fits the sampler to the data.
+
+        Arguments:
+            X (ww.DataFrame): Training features
+            y (ww.DataColumn): Target features
+
+        Returns:
+            self
+        """
+        super().fit(X, y)
+        sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
+        if len(self.parameters['sampling_ratio_dict']):
+            # dictionary provided, which takes priority
+            sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
+        else:
+            sampling_ratio = self.parameters['sampling_ratio']
+            # no dictionary provided. We pass the float if we have a binary situation
+            if len(y.value_counts()) == 2:
+                sampler_params['sampling_strategy'] = sampling_ratio if sampling_ratio != 1 else 'auto'
+            else:
+                # otherwise, we make the dictionary
+                dic = make_balancing_dictionary(y, sampling_ratio)
+                sampler_params['sampling_strategy'] = dic
+        sampler = self.im.SMOTENC(**sampler_params, random_state=self.random_seed)
+        self._component_obj = sampler
 
     def fit_transform(self, X, y):
         """Resample the data
         """
         _, _, X_pd, y_pd = self._prepare_data(X, y)
+        self.fit(X_pd, y_pd)
         X_new, y_new = self._component_obj.fit_resample(X_pd, y_pd)
         return infer_feature_types(X_new), infer_feature_types(y_new)
 
@@ -98,36 +147,60 @@ class SMOTENSampler(BaseSampler):
     """SMOTEN Oversampler component. Uses SMOTEN to generate synthetic samples. Works for purely categorical datasets.
        This component is only run during training and not during predict."""
     name = "SMOTEN Oversampler"
-    hyperparameter_ranges = {
-        'k_neighbors': Integer(2, 10)
-    }
 
-    def __init__(self, sampling_strategy='auto', k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+    def __init__(self, sampling_ratio=0.25, sampling_ratio_dict={}, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
         """Initialize the SMOTEN Oversampler component.
 
         Arguments:
-            sampling_strategy (float, dictionary, str): If float, this is the goal ratio of the minority to majority class.
-                A value of 0.25 means we want a 1:4 ratio of the minority to majority class after oversampling.
-                If dictionary, the keys correspond to each class, and the values are the number of samples we want to oversample to for each class key.
-                Defaults to 'auto', which oversamples all classes to become equal.
-                Note that float is only compatible with binary classification problems, and dictionary or 'auto' should be used for multiclass.
+            sampling_ratio (float): This is the goal ratio of the minority to majority class, with range (0, 1]. A value of 0.25 means we want a 1:4 ratio
+                of the minority to majority class after oversampling. If the targets are multiclass, will create a dictionary using this ratio. Defaults to 0.25.
+            sampling_ratio_dict (dict): Dictionary which has keys corresponding to each class, and the values are the number of samples we want to oversample to for each class key.
+                If this value is provided, it will be used. Otherwise, we opt to use sampling_ratio. Defaults to empty.
             k_neighbors (int): The number of nearest neighbors to used to construct synthetic samples. Defaults to 5.
             n_jobs (int): The number of CPU cores to use. Defaults to -1.
         """
         error_msg = "imbalanced-learn is not installed. Please install using 'pip install imbalanced-learn'"
-        im = import_or_raise("imblearn.over_sampling", error_msg=error_msg)
-        parameters = {"sampling_strategy": sampling_strategy,
+        self.im = import_or_raise("imblearn.over_sampling", error_msg=error_msg)
+        parameters = {"sampling_ratio": sampling_ratio,
+                      "sampling_ratio_dict": sampling_ratio_dict,
                       "k_neighbors": k_neighbors,
                       "n_jobs": n_jobs}
         parameters.update(kwargs)
-        sampler = im.SMOTEN(**parameters, random_state=random_seed)
         super().__init__(parameters=parameters,
-                         component_obj=sampler,
+                         component_obj=None,
                          random_seed=random_seed)
+
+    def fit(self, X, y):
+        """Fits the sampler to the data.
+
+        Arguments:
+            X (ww.DataFrame): Training features
+            y (ww.DataColumn): Target features
+
+        Returns:
+            self
+        """
+        super().fit(X, y)
+        sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
+        if len(self.parameters['sampling_ratio_dict']):
+            # dictionary provided, which takes priority
+            sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
+        else:
+            sampling_ratio = self.parameters['sampling_ratio']
+            # no dictionary provided. We pass the float if we have a binary situation
+            if len(y.value_counts()) == 2:
+                sampler_params['sampling_strategy'] = sampling_ratio if sampling_ratio != 1 else 'auto'
+            else:
+                # otherwise, we make the dictionary
+                dic = make_balancing_dictionary(y, sampling_ratio)
+                sampler_params['sampling_strategy'] = dic
+        sampler = self.im.SMOTEN(**sampler_params, random_state=self.random_seed)
+        self._component_obj = sampler
 
     def fit_transform(self, X, y):
         """Resample the data
         """
         _, _, X_pd, y_pd = self._prepare_data(X, y)
+        self.fit(X_pd, y_pd)
         X_new, y_new = self._component_obj.fit_resample(X_pd, y_pd)
         return infer_feature_types(X_new), infer_feature_types(y_new)
--- evalml/tests/component_tests/test_oversamplers.py
+++ evalml/tests/component_tests/test_oversamplers.py
@@ -7,14 +7,34 @@ from evalml.pipelines.components import (
     SMOTENSampler,
     SMOTESampler
 )
+from evalml.pipelines.components.utils import make_balancing_dictionary
 
 pytest.importorskip('imblearn.over_sampling', reason='Skipping test because imbalanced-learn not installed')
+
+
+binary = pd.Series([0] * 800 + [1] * 200)
+multiclass = pd.Series([0] * 800 + [1] * 150 + [2] * 50)
+
+@pytest.mark.parametrize("y,sampling_ratio,result", [
+                                                     (binary, 1, {0:800, 1:800}),
+                                                     (binary, 0.5, {0:800, 1:400}),
+                                                     (binary, 0.25, {0:800, 1:200}),
+                                                     (binary, 0.1, {0:800, 1:200}),
+                                                     (multiclass, 1, {0:800, 1:800, 2:800}),
+                                                     (multiclass, 0.5, {0:800, 1:400, 2:400}),
+                                                     (multiclass, 0.25, {0: 800, 1:200, 2:200}),
+                                                     (multiclass, 0.1, {0:800, 1: 150, 2: 80})
+                                                     ])
+def test_make_balancing_dictionary(y, sampling_ratio, result):
+    dic = make_balancing_dictionary(y, sampling_ratio)
+    assert dic == result
 
 
 @pytest.mark.parametrize("sampler", [SMOTESampler, SMOTENCSampler, SMOTENSampler])
 def test_init(sampler):
     parameters = {
-        "sampling_strategy": 1.0,
+        "sampling_ratio": 0.5,
+        "sampling_ratio_dict": {},
         "k_neighbors": 2,
         "n_jobs": -1
     }
@@ -24,7 +44,9 @@ def test_init(sampler):
     assert oversampler.parameters == parameters
 
 
-@pytest.mark.parametrize("sampler", [SMOTESampler(), SMOTENCSampler(categorical_features=[0]), SMOTENSampler()])
+@pytest.mark.parametrize("sampler", [SMOTESampler(sampling_ratio=1),
+                                     SMOTENCSampler(sampling_ratio=1, categorical_features=[0]),
+                                     SMOTENSampler(sampling_ratio=1)])
 def test_none_y(sampler):
     X = pd.DataFrame([[i] for i in range(5)])
     oversampler = sampler
@@ -36,7 +58,9 @@ def test_none_y(sampler):
     oversampler.transform(X, None)
 
 
-@pytest.mark.parametrize("sampler", [SMOTESampler(), SMOTENCSampler(categorical_features=[0]), SMOTENSampler()])
+@pytest.mark.parametrize("sampler", [SMOTESampler(sampling_ratio=1),
+                                     SMOTENCSampler(sampling_ratio=1, categorical_features=[0]),
+                                     SMOTENSampler(sampling_ratio=1)])
 @pytest.mark.parametrize("data_type", ["np", "pd", "ww"])
 def test_no_oversample(data_type, sampler, make_data_type, X_y_binary):
     X, y = X_y_binary
@@ -57,7 +81,9 @@ def test_no_oversample(data_type, sampler, make_data_type, X_y_binary):
     np.testing.assert_equal(y, new_y.to_series().values)
 
 
-@pytest.mark.parametrize("sampler", [SMOTESampler(), SMOTENCSampler(categorical_features=[1]), SMOTENSampler()])
+@pytest.mark.parametrize("sampler", [SMOTESampler(sampling_ratio=1),
+                                     SMOTENCSampler(sampling_ratio=1, categorical_features=[1]),
+                                     SMOTENSampler(sampling_ratio=1)])
 @pytest.mark.parametrize("data_type", ["np", "pd", "ww"])
 def test_oversample_imbalanced_binary(data_type, sampler, make_data_type):
     X = np.array([[i for i in range(1000)],
@@ -90,10 +116,10 @@ def test_oversample_imbalanced_binary(data_type, sampler, make_data_type):
     np.testing.assert_equal(y, transform_y.to_series().values)
 
 
-@pytest.mark.parametrize("sampling_strategy", ['auto', {0: 800, 1: 300, 2: 300}])
+@pytest.mark.parametrize("sampling_ratio_dict", [{}, {0: 800, 1: 300, 2: 300}])
 @pytest.mark.parametrize("sampler", [SMOTESampler, SMOTENCSampler, SMOTENSampler])
 @pytest.mark.parametrize("data_type", ["np", "pd", "ww"])
-def test_oversample_imbalanced_multiclass(data_type, sampler, sampling_strategy, make_data_type):
+def test_oversample_imbalanced_multiclass(data_type, sampler, sampling_ratio_dict, make_data_type):
     X = np.array([[i for i in range(1000)],
                   [i % 7 for i in range(1000)],
                   [0.3 * (i % 3) for i in range(1000)]]).T
@@ -101,17 +127,17 @@ def test_oversample_imbalanced_multiclass(data_type, sampler, sampling_strategy,
     X = make_data_type(data_type, X)
     y = make_data_type(data_type, y)
 
-    oversampler = sampler(sampling_strategy=sampling_strategy)
+    oversampler = sampler(sampling_ratio_dict=sampling_ratio_dict)
     if 'NC' in sampler.name:
-        oversampler = sampler(categorical_features=[1], sampling_strategy=sampling_strategy)
+        oversampler = sampler(categorical_features=[1], sampling_ratio_dict=sampling_ratio_dict)
 
     new_X, new_y = oversampler.fit_transform(X, y)
 
-    if sampling_strategy == 'auto':
-        new_length = 2400
-        num_samples = [800, 800, 800]
+    if sampling_ratio_dict == {}:
+        new_length = 1200
+        num_samples = [800, 200, 200]
     else:
-        new_length = sum(sampling_strategy.values())
+        new_length = sum(sampling_ratio_dict.values())
         num_samples = [800, 300, 300]
     # check the lengths and sampled values are as we expect
     assert len(new_X) == new_length
@@ -131,3 +157,49 @@ def test_oversample_imbalanced_multiclass(data_type, sampler, sampling_strategy,
 
     np.testing.assert_equal(X, transform_X.to_dataframe().values)
     np.testing.assert_equal(y, transform_y.to_series().values)
+
+
+@pytest.mark.parametrize("sampler", [SMOTESampler, SMOTENCSampler, SMOTENSampler])
+def test_oversample_seed_same_outputs(sampler, X_y_binary):
+    X, y = X_y_binary
+    X = pd.DataFrame(X)
+    y = pd.Series([0] * 90 + [1] * 10)
+
+    samplers = []
+    for seed in [0, 0, 1]:
+        oversampler = sampler(sampling_ratio=1, random_seed=seed)
+        if 'NC' in sampler.name:
+            oversampler = sampler(categorical_features=[1], sampling_ratio=1, random_seed=seed)
+        samplers.append(oversampler)
+    
+    # iterate through different indices in samplers
+    # in group 1, first two oversamplers in samplers should be equal
+    # in group 2, calling same oversamplers twice should be equal
+    # in group 3, last two oversamplers in samplers should be different
+    for s1, s2 in [[0, 1], [1, 1], [1, 2]]:
+        X1, y1 = samplers[s1].fit_transform(X, y)
+        X2, y2 = samplers[s2].fit_transform(X, y)
+        if s2 == 2 and sampler != SMOTENSampler:
+            # group 3, SMOTENSampler performance doesn't change with different random states
+            with pytest.raises(AssertionError):
+                pd.testing.assert_frame_equal(X1.to_dataframe(), X2.to_dataframe())
+        else:
+            pd.testing.assert_frame_equal(X1.to_dataframe(), X2.to_dataframe())
+        pd.testing.assert_series_equal(y1.to_series(), y2.to_series())
+
+
+@pytest.mark.parametrize("sampler", [SMOTESampler, SMOTENCSampler, SMOTENSampler])
+def test_sampler_dic_overrides_ratio(sampler):
+    X = np.array([[i for i in range(1000)],
+                  [i % 7 for i in range(1000)],
+                  [0.3 * (i % 3) for i in range(1000)]]).T
+    y = np.array([0] * 800 + [1] * 100 + [2] * 100)
+
+    goal_value_dic = {0: 800, 1: 800, 2: 600}
+    oversampler = sampler(sampling_ratio=1, sampling_ratio_dict=goal_value_dic)
+    if 'NC' in sampler.name:
+        oversampler = sampler(categorical_features=[1], sampling_ratio=1, sampling_ratio_dict=goal_value_dic)
+    
+    new_X, new_y = oversampler.fit_transform(X, y)
+    assert len(new_X) == sum(goal_value_dic.values())
+    assert len(new_y) == sum(goal_value_dic.values())
