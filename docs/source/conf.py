f62d468b5af99bbed0c9abec3b6bd952dee7c61a
bchen1116 <bchen1116@gmail.com>
2021-04-07 11:34:58 -0400

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -263,9 +263,9 @@ class AccessorMethodDocumenter(AccessorLevelDocumenter, MethodDocumenter):
 
 
 def setup(app):
-    if not os.path.isdir("/home/docs/.ipython/profile_default/startup"):
-        os.makedirs("/home/docs/.ipython/profile_default/startup")
-    shutil.copy("disable-warnings.py", "/home/docs/.ipython/profile_default/startup/")
+    # if not os.path.isdir("/home/docs/.ipython/profile_default/startup"):
+    #     os.makedirs("/home/docs/.ipython/profile_default/startup")
+    # shutil.copy("disable-warnings.py", "/home/docs/.ipython/profile_default/startup/")
     app.add_javascript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js')
     app.add_stylesheet("style.css")
     app.add_autodocumenter(AccessorCallableDocumenter)
