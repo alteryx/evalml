1a18c2f1c922cdb7d1adba6ff1e1383f3c667be5
Dylan Sherry <sharshofski@gmail.com>
2021-04-06 21:10:09 -0400

    Update metaclasses to support kwargs

--- evalml/pipelines/components/component_base_meta.py
+++ evalml/pipelines/components/component_base_meta.py
@@ -15,16 +15,16 @@ class ComponentBaseMeta(BaseMeta):
             It raises an exception if `False` and calls and returns the wrapped method if `True`.
         """
         @wraps(method)
-        def _check_for_fit(self, X=None, y=None):
+        def _check_for_fit(self, X=None, y=None, **kwargs):
             klass = type(self).__name__
             if not self._is_fitted and self.needs_fitting:
                 raise ComponentNotYetFittedError(f'This {klass} is not fitted yet. You must fit {klass} before calling {method.__name__}.')
             elif method.__name__ == 'inverse_transform':
-                return method(self, X, y)
+                return method(self, X, y, **kwargs)
             elif X is None and y is None:
-                return method(self)
+                return method(self, **kwargs)
             elif y is None:
-                return method(self, X)
+                return method(self, X, **kwargs)
             else:
-                return method(self, X, y)
+                return method(self, X, y, **kwargs)
         return _check_for_fit
