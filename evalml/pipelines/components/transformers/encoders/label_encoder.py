87c57216a2c393b81c28aac89d54c88c0a3ce777
Dylan Sherry <sharshofski@gmail.com>
2021-04-06 22:48:00 -0400

    Record dtype in label encoder, and convert target to that type in label decoder

--- evalml/pipelines/components/transformers/encoders/label_encoder.py
+++ evalml/pipelines/components/transformers/encoders/label_encoder.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder as SkLabelEncoder
+import woodwork as ww
 
 from evalml.pipelines.components.transformers import Transformer
 from evalml.utils import _convert_woodwork_types_wrapper, _retain_custom_types_and_initalize_woodwork, infer_feature_types
@@ -11,6 +12,7 @@ class LabelEncoder(Transformer):
     hyperparameter_ranges = {}
 
     def __init__(self, random_seed=0):
+        self._y_logical_type = None
         super().__init__(parameters={}, component_obj=None, random_seed=random_seed)
 
     def fit(self, X, y):
@@ -19,6 +21,7 @@ class LabelEncoder(Transformer):
         if y is None:
             return X, None
         y_ww = infer_feature_types(y)
+        self._y_logical_type = y_ww.logical_type
 
         self._encoder = SkLabelEncoder()
         y_pd = _convert_woodwork_types_wrapper(y_ww.to_series())
@@ -69,7 +72,8 @@ class LabelDecoder(Transformer):
         y_pd = _convert_woodwork_types_wrapper(y_ww.to_series())
         y_decoded = label_encoder._encoder.inverse_transform(y_pd.astype(int))
         y_t = pd.Series(y_decoded, index=y_pd.index)
-        return X, _retain_custom_types_and_initalize_woodwork(y_ww, y_t)
+        y_t_ww = ww.DataColumn(y_t, logical_type=label_encoder._y_logical_type)
+        return X, y_t_ww
 
     def fit_transform(self, X, y, dependent_components=None):
         if dependent_components is None:
