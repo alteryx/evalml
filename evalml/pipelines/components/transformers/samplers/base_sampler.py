d143a33af8e74f121ba52ee35339fe67ddc52dba
bchen1116 <bchen1116@gmail.com>
2021-04-07 14:06:26 -0400

    fix comments

--- evalml/pipelines/components/transformers/samplers/base_sampler.py
+++ evalml/pipelines/components/transformers/samplers/base_sampler.py
@@ -32,28 +32,25 @@ class BaseSampler(Transformer):
             Arguments:
             X (ww.DataFrame): Training features
             y (ww.DataColumn): Target features
-            sampler_class (Sampler): The sampler we want to initialize with
-
-        Returns:
-            self
+            sampler_class (Sampler): The sampler we want to initialize
         """
         _, _, _, y_pd = self._prepare_data(X, y)
         sampler_params = {k: v for k, v in copy.copy(self.parameters).items() if k not in ['sampling_ratio', 'sampling_ratio_dict']}
         if self.parameters['sampling_ratio_dict'] is not None and len(self.parameters['sampling_ratio_dict']):
-            # dictionary provided, which takes priority
+            # dictionary provided and takes priority
             sampler_params['sampling_strategy'] = self.parameters['sampling_ratio_dict']
         else:
-            sampling_ratio = self.parameters['sampling_ratio']
             # no dictionary provided. We pass the float if we have a binary situation
+            sampling_ratio = self.parameters['sampling_ratio']
             if len(y_pd.value_counts()) == 2:
+                # imblearn has strange behavior when we set sampling_ratio = 1, so we choose 'auto' instead in that scenario
                 sampler_params['sampling_strategy'] = sampling_ratio if sampling_ratio != 1 else 'auto'
             else:
-                # otherwise, we make the dictionary
+                # otherwise, we make the dictionary using the sampling_ratio
                 dic = make_balancing_dictionary(y_pd, sampling_ratio)
                 sampler_params['sampling_strategy'] = dic
         sampler = sampler_class(**sampler_params, random_state=self.random_seed)
         self._component_obj = sampler
-        return self
 
     def _prepare_data(self, X, y):
         """Transforms the input data to pandas data structure that our sampler can ingest.
