a0f0efe5cfdc5dcccec1d23ed16583f269e8b17a
Jeremy Shih <jeremyliweishih@gmail.com>
2021-04-09 17:14:14 -0400

    Add rest of unit tests

--- evalml/tests/data_checks_tests/test_natural_language_nan_data_check.py
+++ evalml/tests/data_checks_tests/test_natural_language_nan_data_check.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import woodwork as ww
 
 from evalml.data_checks import (
     DataCheckError,
@@ -7,7 +9,7 @@ from evalml.data_checks import (
 )
 
 
-def test_datetime_nan_data_check_error():
+def test_nl_nan_data_check_error():
     data = pd.DataFrame({'natural_language': [None, "string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language"]})
     nl_nan_check = NaturalLanguageNaNDataCheck()
     assert nl_nan_check.validate(data) == {
@@ -20,93 +22,91 @@ def test_datetime_nan_data_check_error():
     }
 
 
-# def test_datetime_nan_data_check_error_numeric_columns_no_null():
-#     dt_nan_check = DateTimeNaNDataCheck()
-#     assert dt_nan_check.validate(pd.DataFrame(np.random.randint(0, 10, size=(10, 4)))) == {
-#         "warnings": [],
-#         "actions": [],
-#         "errors": []
-#     }
+def test_nl_nan_data_check_error_no_nan():
+    nl_nan_check = NaturalLanguageNaNDataCheck()
+    assert nl_nan_check.validate(pd.DataFrame({'natural_language': ["string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language"]})) == {
+        "warnings": [],
+        "actions": [],
+        "errors": []
+    }
 
 
-# def test_datetime_nan_data_check_error_numeric_null_columns():
-#     data = pd.DataFrame(np.random.randint(0, 10, size=(10, 4)))
-#     data = data.replace(data.iloc[0][0], None)
-#     data = data.replace(data.iloc[1][1], None)
-#     dt_nan_check = DateTimeNaNDataCheck()
-#     assert dt_nan_check.validate(data) == {
-#         "warnings": [],
-#         "actions": [],
-#         "errors": []
-#     }
+def test_nl_nan_data_check_error_other_cols_with_nan():
+    data = pd.DataFrame(np.random.randint(0, 10, size=(10, 4)))
+    data = data.replace(data.iloc[0][0], None)
+    data = data.replace(data.iloc[1][1], None)
+    nl_nan_check = NaturalLanguageNaNDataCheck()
+    assert nl_nan_check.validate(data) == {
+        "warnings": [],
+        "actions": [],
+        "errors": []
+    }
 
 
-# def test_datetime_nan_data_check_multiple_dt_no_nan():
-#     data = pd.DataFrame()
-#     data['A'] = pd.Series(pd.date_range('20200101', periods=3))
-#     data['B'] = pd.Series(pd.date_range('20200101', periods=3))
-#     data['C'] = np.random.randint(0, 5, size=len(data))
+def test_nl_nan_data_check_error_multiple_nl_no_nan():
+    data = pd.DataFrame()
+    data['A'] = ['string_that_is_long_enough_for_natural_language', 'string_that_is_long_enough_for_natural_language']
+    data['B'] = ['string_that_is_long_enough_for_natural_language', 'string_that_is_long_enough_for_natural_language']
+    data['C'] = np.random.randint(0, 3, size=len(data))
 
-#     dt_nan_check = DateTimeNaNDataCheck()
-#     assert dt_nan_check.validate(data) == {
-#         "warnings": [],
-#         "actions": [],
-#         "errors": []
-#     }
-
-
-# def test_datetime_nan_data_check_multiple_nan_dt():
-#     data = pd.DataFrame()
-#     data['A'] = pd.Series(pd.date_range('20200101', periods=3))
-#     data.loc[0][0] = None
-#     data['B'] = pd.Series(pd.date_range('20200101', periods=3))
-#     data.loc[0][1] = None
-#     data['C'] = np.random.randint(0, 5, size=len(data))
-
-#     dt_nan_check = DateTimeNaNDataCheck()
-#     assert dt_nan_check.validate(data) == {
-#         "warnings": [],
-#         "actions": [],
-#         "errors": [DataCheckError(message='Input datetime column(s) (A, B) contains NaN values. Please impute NaN values or drop these rows or columns.',
-#                                   data_check_name=DateTimeNaNDataCheck.name,
-#                                   message_code=DataCheckMessageCode.DATETIME_HAS_NAN,
-#                                   details={"columns": 'A, B'}).to_dict()]
-#     }
+    nl_nan_check = NaturalLanguageNaNDataCheck()
+    assert nl_nan_check.validate(data) == {
+        "warnings": [],
+        "actions": [],
+        "errors": []
+    }
 
 
-# def test_datetime_nan_check_input_formats():
-#     dt_nan_check = DateTimeNaNDataCheck()
+def test_nl_nan_data_check_error_multiple_nl_nan():
+    data = pd.DataFrame()
+    data['A'] = pd.Series([None, "string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language"])
+    data['B'] = pd.Series([None, "string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language"])
+    data['C'] = np.random.randint(0, 5, size=len(data))
 
-#     # test empty pd.DataFrame
-#     assert dt_nan_check.validate(pd.DataFrame()) == {"warnings": [], "errors": [], "actions": []}
+    nl_nan_check = NaturalLanguageNaNDataCheck()
+    assert nl_nan_check.validate(data) == {
+        "warnings": [],
+        "actions": [],
+        "errors": [DataCheckError(message='Input natural language column(s) (A, B) contains NaN values. Please impute NaN values or drop these rows or columns.',
+                                  data_check_name=NaturalLanguageNaNDataCheck.name,
+                                  message_code=DataCheckMessageCode.NATURAL_LANGUAGE_HAS_NAN,
+                                  details={"columns": 'A, B'}).to_dict()]
+    }
 
-#     expected = {
-#         "warnings": [],
-#         "actions": [],
-#         "errors": [DataCheckError(message='Input datetime column(s) (index) contains NaN values. Please impute NaN values or drop these rows or columns.',
-#                                   data_check_name=DateTimeNaNDataCheck.name,
-#                                   message_code=DataCheckMessageCode.DATETIME_HAS_NAN,
-#                                   details={"columns": 'index'}).to_dict()]
-#     }
 
-#     dates = np.arange(np.datetime64('2017-01-01'), np.datetime64('2017-01-08'))
-#     dates[0] = np.datetime64('NaT')
+def test_nl_nan_check_input_formats():
+    nl_nan_check = NaturalLanguageNaNDataCheck()
 
-#     #  test Woodwork
-#     ww_input = ww.DataTable(pd.DataFrame(dates, columns=['index']))
-#     assert dt_nan_check.validate(ww_input) == expected
+    # test empty pd.DataFrame
+    assert nl_nan_check.validate(pd.DataFrame()) == {"warnings": [], "errors": [], "actions": []}
 
-#     expected = {
-#         "warnings": [],
-#         "actions": [],
-#         "errors": [DataCheckError(message='Input datetime column(s) (0) contains NaN values. Please impute NaN values or drop these rows or columns.',
-#                                   data_check_name=DateTimeNaNDataCheck.name,
-#                                   message_code=DataCheckMessageCode.DATETIME_HAS_NAN,
-#                                   details={'columns': '0'}).to_dict()]
-#     }
+    expected = {
+        "warnings": [],
+        "actions": [],
+        "errors": [DataCheckError(message='Input natural language column(s) (nl) contains NaN values. Please impute NaN values or drop these rows or columns.',
+                                  data_check_name=NaturalLanguageNaNDataCheck.name,
+                                  message_code=DataCheckMessageCode.NATURAL_LANGUAGE_HAS_NAN,
+                                  details={"columns": 'nl'}).to_dict()]
+    }
 
-#     #  test 2D list
-#     assert dt_nan_check.validate([dates, np.arange(np.datetime64('2017-01-01'), np.datetime64('2017-01-08'))]) == expected
+    nl_col = [None, "string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language"]
 
-#     # test np.array
-#     assert dt_nan_check.validate(np.array([dates, np.arange(np.datetime64('2017-01-01'), np.datetime64('2017-01-08'))])) == expected
+    #  test Woodwork
+    ww_input = ww.DataTable(pd.DataFrame(nl_col, columns=['nl']))
+    assert nl_nan_check.validate(ww_input) == expected
+
+    expected = {
+        "warnings": [],
+        "actions": [],
+        "errors": [DataCheckError(message='Input natural language column(s) (0) contains NaN values. Please impute NaN values or drop these rows or columns.',
+                                  data_check_name=NaturalLanguageNaNDataCheck.name,
+                                  message_code=DataCheckMessageCode.NATURAL_LANGUAGE_HAS_NAN,
+                                  details={'columns': '0'}).to_dict()]
+    }
+
+    #  test 2D list
+    nl_col_without_nan = ["string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language", "string_that_is_long_enough_for_natural_language"]
+    assert nl_nan_check.validate([nl_col, nl_col_without_nan]) == expected
+
+    # test np.array
+    assert nl_nan_check.validate(np.array([nl_col, nl_col_without_nan])) == expected
