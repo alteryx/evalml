5ebdcad94615214540ffd5305eff2586927114ee
bchen1116 <bchen1116@gmail.com>
2021-04-22 14:46:53 -0400

    linting

--- evalml/tests/automl_tests/test_automl_search_classification.py
+++ evalml/tests/automl_tests/test_automl_search_classification.py
@@ -3,7 +3,6 @@ from unittest.mock import MagicMock, patch
 import numpy as np
 import pandas as pd
 import pytest
-import woodwork as ww
 from skopt.space import Categorical
 
 from evalml import AutoMLSearch
