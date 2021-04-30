432305972a01d51123cf24c6eb986e6473b49a9b
Dylan Sherry <sharshofski@gmail.com>
2021-04-30 01:58:49 -0400

    Simplify oversampler code. Same change to fit_transform as with undersampler

--- evalml/pipelines/components/transformers/samplers/oversamplers.py
+++ evalml/pipelines/components/transformers/samplers/oversamplers.py
@@ -1,33 +1,38 @@
 from evalml.pipelines.components.transformers.samplers.base_sampler import (
-    BaseOverSampler
+    BaseOversampler
 )
+from evalml.utils import import_or_raise
 from evalml.utils.woodwork_utils import infer_feature_types
 
+_ERROR_MSG = "imbalanced-learn is not installed. Please install using 'pip install imbalanced-learn'"
 
-class SMOTESampler(BaseOverSampler):
+
+class SMOTEOversampler(BaseOversampler):
     """SMOTE Oversampler component. Works on numerical datasets only. This component is only run during training and not during predict."""
     name = "SMOTE Oversampler"
     hyperparameter_ranges = {}
 
     def __init__(self, sampling_ratio=0.25, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
-        super().__init__("SMOTE",
-                         sampling_ratio=sampling_ratio,
+        im = import_or_raise("imblearn.over_sampling", error_msg=_ERROR_MSG)
+        self.sampler_class = im.SMOTE
+        super().__init__(sampling_ratio=sampling_ratio,
                          k_neighbors=k_neighbors,
                          n_jobs=n_jobs,
                          random_seed=random_seed,
                          **kwargs)
 
 
-class SMOTENCSampler(BaseOverSampler):
+class SMOTENCOversampler(BaseOversampler):
     """SMOTENC Oversampler component. Uses SMOTENC to generate synthetic samples. Works on a mix of nomerical and categorical columns.
        Input data must be Woodwork type, and this component is only run during training and not during predict."""
     name = "SMOTENC Oversampler"
     hyperparameter_ranges = {}
 
     def __init__(self, sampling_ratio=0.25, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
+        im = import_or_raise("imblearn.over_sampling", error_msg=_ERROR_MSG)
+        self.sampler_class = im.SMOTENC
         self.categorical_features = None
-        super().__init__("SMOTENC",
-                         sampling_ratio=sampling_ratio,
+        super().__init__(sampling_ratio=sampling_ratio,
                          k_neighbors=k_neighbors,
                          n_jobs=n_jobs,
                          random_seed=random_seed,
@@ -44,15 +49,16 @@ class SMOTENCSampler(BaseOverSampler):
         super().fit(X, y)
 
 
-class SMOTENSampler(BaseOverSampler):
+class SMOTENOversampler(BaseOversampler):
     """SMOTEN Oversampler component. Uses SMOTEN to generate synthetic samples. Works for purely categorical datasets.
        This component is only run during training and not during predict."""
     name = "SMOTEN Oversampler"
     hyperparameter_ranges = {}
 
     def __init__(self, sampling_ratio=0.25, k_neighbors=5, n_jobs=-1, random_seed=0, **kwargs):
-        super().__init__("SMOTEN",
-                         sampling_ratio=sampling_ratio,
+        im = import_or_raise("imblearn.over_sampling", error_msg=_ERROR_MSG)
+        self.sampler_class = im.SMOTEN
+        super().__init__(sampling_ratio=sampling_ratio,
                          k_neighbors=k_neighbors,
                          n_jobs=n_jobs,
                          random_seed=random_seed,
