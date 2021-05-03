684e41972a94eae125614cb10891d03f01e89a89
Jeremy Shih <jeremyliweishih@gmail.com>
2021-05-03 16:19:33 -0400

    Add highly null row data check

---
+++ evalml/data_checks/highly_null_rows_data_check.py
@@ -0,0 +1,83 @@
+from evalml.data_checks import (
+    DataCheck,
+    DataCheckAction,
+    DataCheckActionCode,
+    DataCheckMessageCode,
+    DataCheckWarning
+)
+from evalml.utils import _convert_woodwork_types_wrapper, infer_feature_types
+
+
+class HighlyNullRowsDataCheck(DataCheck):
+    """Checks if there are any highly-null rows in the input."""
+
+    def __init__(self, pct_null_threshold=0.95):
+        """Checks if there are any highly-null rows in the input.
+
+        Arguments:
+            pct_null_threshold(float): If the percentage of NaN values in an input feature exceeds this amount,
+                that feature will be considered highly-null. Defaults to 0.95.
+
+        """
+        if pct_null_threshold < 0 or pct_null_threshold > 1:
+            raise ValueError("pct_null_threshold must be a float between 0 and 1, inclusive.")
+        self.pct_null_threshold = pct_null_threshold
+
+    def validate(self, X, y=None):
+        """Checks if there are any highly-null rows in the input.
+
+        Arguments:
+            X (ww.DataTable, pd.DataFrame, np.ndarray): Features
+            y (ww.DataColumn, pd.Series, np.ndarray): Ignored.
+
+        Returns:
+            dict: dict with a DataCheckWarning if there are any highly-null rows.
+
+        Example:
+            >>> import pandas as pd
+            >>> df = pd.DataFrame({
+            ...    'lots_of_null': [None, None, None, None, 5],
+            ...    'no_null': [1, 2, 3, 4, 5]
+            ... })
+            >>> null_check = HighlyNullRowsDataCheck(pct_null_threshold=0.8)
+            >>> assert null_check.validate(df) == {"errors": [],\
+                                                   "warnings": [{"message": "Row 'lots_of_null' is 80.0% or more null",\
+                                                                 "data_check_name": "HighlyNullRowsDataCheck",\
+                                                                 "level": "warning",\
+                                                                 "code": "HIGHLY_NULL",\
+                                                                 "details": {"column": "lots_of_null", "pct_null_rows": 0.8}}],\
+                                                    "actions": [{"code": "DROP_COL",\
+                                                                 "metadata": {"column": "lots_of_null"}}]}
+        """
+        results = {
+            "warnings": [],
+            "errors": [],
+            "actions": []
+        }
+
+        X = infer_feature_types(X)
+        X = _convert_woodwork_types_wrapper(X.to_dataframe())
+
+        percent_null = (X.isnull().mean(axis=1)).to_dict()
+        highly_null_rows = []
+        if self.pct_null_threshold == 0.0:
+            highly_null_rows = {key: value for key, value in percent_null.items() if value > 0.0}
+            warning_msg = "Row '{}' is more than 0% null"
+            results["warnings"].extend([DataCheckWarning(message=warning_msg.format(row_name),
+                                                         data_check_name=self.name,
+                                                         message_code=DataCheckMessageCode.HIGHLY_NULL_ROWS,
+                                                         details={"row": row_name, "pct_null_cols": highly_null_rows[row_name]}).to_dict()
+                                        for row_name in highly_null_rows])
+        else:
+            highly_null_rows = {key: value for key, value in percent_null.items() if value >= self.pct_null_threshold}
+            warning_msg = "Row '{}' is {}% or more null"
+            results["warnings"].extend([DataCheckWarning(message=warning_msg.format(row_name, self.pct_null_threshold * 100),
+                                                         data_check_name=self.name,
+                                                         message_code=DataCheckMessageCode.HIGHLY_NULL_ROWS,
+                                                         details={"row": row_name, "pct_null_cols": highly_null_rows[row_name]}).to_dict()
+                                        for row_name in highly_null_rows])
+
+        results["actions"].extend([DataCheckAction(DataCheckActionCode.DROP_ROW,
+                                                   metadata={"row": row_name}).to_dict()
+                                   for row_name in highly_null_rows])
+        return results
