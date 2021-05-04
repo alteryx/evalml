32fce22fec77d5bf52e2fab1eb76875ba0a34989
Dylan Sherry <sharshofski@gmail.com>
2021-05-03 22:35:16 -0400

    Prediction explanations: multiclass catboost support

--- evalml/model_understanding/prediction_explanations/_algorithms.py
+++ evalml/model_understanding/prediction_explanations/_algorithms.py
@@ -57,10 +57,6 @@ def _compute_shap_values(pipeline, features, training_data=None):
         features = check_array(features.values)
 
     if estimator.model_family.is_tree_estimator():
-        # Because of this issue: https://github.com/slundberg/shap/issues/1215
-        if estimator.model_family == ModelFamily.CATBOOST and is_multiclass(pipeline.problem_type):
-            # Will randomly segfault
-            raise NotImplementedError("SHAP values cannot currently be computed for catboost models for multiclass problems.")
         # Use tree_path_dependent to avoid linear runtime with dataset size
         with warnings.catch_warnings(record=True) as ws:
             explainer = shap.TreeExplainer(estimator._component_obj, feature_perturbation="tree_path_dependent")
