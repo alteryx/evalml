c3e2fa93b7b3891608c5eaed44e970b51c9a8b72
Angela Lin <angela97lin@gmail.com>
2021-05-05 03:24:47 -0400

    begin to fix tests

--- evalml/pipelines/pipeline_base.py
+++ evalml/pipelines/pipeline_base.py
@@ -526,13 +526,12 @@ class PipelineBase(ABC, metaclass=PipelineBaseMeta):
         component_graph_str = f"[{component_graph_repr}]"
         parameters_repr = ', '.join([f"'{component}':{{{repr_component(parameters)}}}" for component, parameters in self.parameters.items()])
         custom_hyperparameters_repr = ', '.join([f"'{component}':{{{repr_component(hyperparameters)}}}," for component, hyperparameters in self.custom_hyperparameters.items()]) if self.custom_hyperparameters else None
-        custom_hyperparmeter_str = f"custom_hyperparameters={{{custom_hyperparameters_repr}}}," if custom_hyperparameters_repr else ''
-        custom_name_repr = f"custom_name='{self.custom_name}'," if self.custom_name else ''
+        custom_hyperparmeter_str = f"custom_hyperparameters={{{custom_hyperparameters_repr}}}," if custom_hyperparameters_repr else None
+        custom_name_repr = f"custom_name='{self.custom_name}'" if self.custom_name else None
         parameters_str = f"parameters={{{parameters_repr}}}"
         random_seed_str = f"random_seed={self.random_seed}"
-
-        return f'pipeline = {(type(self).__name__)}(component_graph={component_graph_str}, {parameters_str}, {custom_hyperparmeter_str} {custom_name_repr} {random_seed_str})'
-        return f'pipeline = {(type(self).__name__)}(component_graph={component_graph_str})'
+        additional_args_str = ", ".join([arg for arg in [parameters_str, custom_hyperparmeter_str, custom_name_repr, random_seed_str] if arg is not None])
+        return f'pipeline = {(type(self).__name__)}(component_graph={component_graph_str}, {additional_args_str})'
 
     def __iter__(self):
         return self
