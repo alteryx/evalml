57b285e72dd28ff9ca28f6ed8d05ef68fa2a5b34
Bryan Chen <bchen1116@gmail.com>
2021-05-12 17:28:59 -0400

    Address Sphinx 4.0 upgrade (#2244)
    * fix sphinx
    
    * fix release notes
    
    * capping sphinx
    
    * fix release notes
    
    * pin nbsphinx:
    
    * pin to 0.8.5
    
    * fix docs
    
    * update markupsafe
    
    * fix graphviz
    
    * remove markupsafe
    
    * add markupspace
    
    * update markupsafe
    
    * fix test
    
    Co-authored-by: Gaurav Sheni <gvsheni@gmail.com>

--- evalml/tests/utils_tests/test_dependencies.py
+++ evalml/tests/utils_tests/test_dependencies.py
@@ -5,12 +5,20 @@ import pytest
 import requirements
 
 
+def _get_req_name(name):
+    if name == 'imbalanced-learn':
+        return 'imblearn'
+    elif name == 'MarkupSafe':
+        return 'markupsafe'
+    return name
+
+
 def test_has_minimal_deps(has_minimal_dependencies, is_running_py_39_or_above, is_using_conda):
     reqs_path = pathlib.Path(__file__).absolute().parents[3].joinpath('requirements.txt')
     lines = open(reqs_path, 'r').readlines()
     lines = [line for line in lines if '-r ' not in line]
     reqs = requirements.parse(''.join(lines))
-    extra_deps = [req.name if req.name != 'imbalanced-learn' else 'imblearn' for req in reqs]
+    extra_deps = [_get_req_name(req.name) for req in reqs]
     extra_deps += ['plotly.graph_objects']
     for module in extra_deps:
         # We don't expect to install sktime in python 3.9. Let's verify it's not present:
