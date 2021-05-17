1e30ba2ffd16fdb2d9361b88e09a8666fee03d88
Parthiv Naresh <parthiv.naresh@alteryx.com>
2021-05-17 17:09:59 -0400

    simplify test

--- evalml/tests/utils_tests/test_logger.py
+++ evalml/tests/utils_tests/test_logger.py
@@ -213,7 +213,7 @@ def test_time_elapsed(mock_time, time_passed, answer):
     assert time == answer
 
 
-@pytest.mark.parametrize("type_, allowed_families, number_", [("binary", None, 8), ("multiclass", 3, 3), ("regression", 4, 4)])
+@pytest.mark.parametrize("type_, allowed_families, number_", [("binary", None, 8), ("multiclass", 2, 2), ("regression", 3, 3)])
 def test_pipeline_output(type_, allowed_families, number_, X_y_binary, X_y_multi, X_y_regression, caplog):
     if type_ == 'binary':
         X, y = X_y_binary
@@ -224,8 +224,8 @@ def test_pipeline_output(type_, allowed_families, number_, X_y_binary, X_y_multi
     if not allowed_families:
         _ = AutoMLSearch(X_train=X, y_train=y, problem_type=type_)
     else:
-        if allowed_families == 3:
-            _ = AutoMLSearch(X_train=X, y_train=y, problem_type=type_, allowed_model_families=['xgboost', 'lightgbm', 'decision_tree'])
-        elif allowed_families == 4:
-            _ = AutoMLSearch(X_train=X, y_train=y, problem_type=type_, allowed_model_families=['xgboost', 'lightgbm', 'decision_tree', 'catboost'])
+        if allowed_families == 2:
+            _ = AutoMLSearch(X_train=X, y_train=y, problem_type=type_, allowed_model_families=['lightgbm', 'decision_tree'])
+        elif allowed_families == 3:
+            _ = AutoMLSearch(X_train=X, y_train=y, problem_type=type_, allowed_model_families=['lightgbm', 'decision_tree', 'catboost'])
     assert f"{number_} pipelines ready for search" in caplog.text
