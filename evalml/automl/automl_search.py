7f2f81d28f6b4b5b8b8a5d60a5e7cdf23ef512b1
Parthiv Naresh <parthiv.naresh@alteryx.com>
2021-06-01 09:35:31 -0400

    docs update

--- evalml/automl/automl_search.py
+++ evalml/automl/automl_search.py
@@ -232,7 +232,7 @@ class AutoMLSearch:
                     }
 
             custom_hyperparameters (dict): A dict of the hyperparameter ranges used to iterate over during search.
-                Keys should consist of the component names and values should specify an skopt.Space.
+                Keys should consist of the component names and values should specify a singular value or skopt.Space.
                 e.g. custom_hyperparameters = {
                         'Imputer' : {
                             'numeric_impute_strategy': Categorical(['most_frequent', 'median'])
