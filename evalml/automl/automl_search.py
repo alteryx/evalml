e2de86cd379b83b7a9b5a36751a94c1051efc6e4
bchen1116 <bchen1116@gmail.com>
2021-06-07 17:52:46 -0400

    linting

--- evalml/automl/automl_search.py
+++ evalml/automl/automl_search.py
@@ -289,7 +289,9 @@ class AutoMLSearch:
             thresholding_objective, return_instance=True
         )
         if self.thresholding_objective.score_needs_proba:
-            raise ValueError("Thresholding objective must be thresholdable and cannot need probabilities!")
+            raise ValueError(
+                "Thresholding objective must be thresholdable and cannot need probabilities!"
+            )
         if self.data_splitter is not None and not issubclass(
             self.data_splitter.__class__, BaseCrossValidator
         ):
