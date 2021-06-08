df2f6e7e1b6828fd08b0aa154d1cc620596434f6
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2021-06-08 12:51:33 -0500

    lint fix

--- evalml/update_checker.py
+++ evalml/update_checker.py
@@ -1,9 +1,9 @@
 from pkg_resources import iter_entry_points
 
-for entry_point in iter_entry_points('alteryx_open_src_initialize'):
+for entry_point in iter_entry_points("alteryx_open_src_initialize"):
     try:
         method = entry_point.load()
         if callable(method):
-            method('composeml')
+            method("composeml")
     except Exception:
         pass
