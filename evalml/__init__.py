6602643a33c423b4c935dfbc9b109a96a66c1491
Karsten Chu <karsten.chu@alteryx.com>
2021-06-08 20:04:43 -0400

    Initial.

--- evalml/__init__.py
+++ evalml/__init__.py
@@ -21,4 +21,4 @@ with warnings.catch_warnings():
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-__version__ = "0.25.0"
+__version__ = "0.26.0"
