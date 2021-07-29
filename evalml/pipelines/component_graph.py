f3a33522af1af57ecf6bf1aad10f3f885a3e93a8
Angela Lin <angela97lin@gmail.com>
2021-07-29 10:39:52 -0400

    oops remove pdb

--- evalml/pipelines/component_graph.py
+++ evalml/pipelines/component_graph.py
@@ -244,9 +244,6 @@ class ComponentGraph:
             y_input = None
 
             for parent_input in self.get_inputs(component_name):
-                import pdb
-
-                pdb.set_trace()
                 if parent_input[-2:] == ".y" or parent_input == "y":
                     if y_input is not None:
                         raise ValueError(
