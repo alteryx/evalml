d0635570a4182eca23626f994a92114b962a2da1
Angela Lin <angela97lin@gmail.com>
2021-08-24 16:36:35 -0400

    add tests and cleanup

--- evalml/pipelines/components/transformers/preprocessing/drop_rows_transformer.py
+++ evalml/pipelines/components/transformers/preprocessing/drop_rows_transformer.py
@@ -3,7 +3,12 @@ from evalml.utils import infer_feature_types
 
 
 class DropRowsTransformer(Transformer):
-    """Transformer to drop rows specified by row indices."""
+    """Transformer to drop rows specified by row indices.
+
+
+    Arguments:
+        indices_to_drop (list): List of indices to drop in the input data,
+    """
 
     name = "Drop Rows Transformer"
     hyperparameter_ranges = {}
@@ -15,21 +20,26 @@ class DropRowsTransformer(Transformer):
 
     def fit(self, X, y=None):
         X_t = infer_feature_types(X)
-        # check if all indices exist. If no, error
+        y_t = infer_feature_types(y) if y is not None else None
+
         if self.indices_to_drop is not None:
-            for index in self.indices_to_drop:
-                if index not in X_t.index:
-                    raise ValueError("Index does not exist in input DataFrame")
+            indices_to_drop_set = set(self.indices_to_drop)
+            if not indices_to_drop_set.issubset(X_t.index):
+                raise ValueError("Index does not exist in input features.")
+            elif y_t is not None and not indices_to_drop_set.issubset(y_t.index):
+                raise ValueError("Index does not exist in input target.")
         return self
 
     def transform(self, X, y=None):
         X_t = infer_feature_types(X)
-        X_t
-        if y is not None:
-            y_t = infer_feature_types(y)
-        # check if y is None
-        if len(self.indices_to_drop) == 0:
-            return X_t
-        rows_dropped = X_t.drop(self.indices_to_drop, axis=0)
-        rows_dropped.ww.init()
-        return rows_dropped
+        y_t = infer_feature_types(y) if y is not None else None
+        if self.indices_to_drop is None or len(self.indices_to_drop) == 0:
+            return X_t, y_t
+
+        X_t = X_t.drop(self.indices_to_drop, axis=0)
+        X_t.ww.init()
+
+        if y_t is not None:
+            y_t = y_t.drop(self.indices_to_drop)
+            y_t.ww.init()
+        return X_t, y_t
