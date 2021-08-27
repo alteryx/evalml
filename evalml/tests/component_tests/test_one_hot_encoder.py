708ea7bf8194c5352c3434b84fe6c4169e4f77da
Parthiv Naresh <parthiv.naresh@alteryx.com>
2021-08-27 12:22:16 +0100

    one hot encoder updates

--- evalml/tests/component_tests/test_one_hot_encoder.py
+++ evalml/tests/component_tests/test_one_hot_encoder.py
@@ -166,6 +166,7 @@ def test_drop_first():
             "col_3": ["a", "a", "a", "a", "a"],
         }
     )
+    X.ww.init(logical_types={"col_1": "categorical", "col_2": "categorical", "col_3": "categorical"})
     encoder = OneHotEncoder(top_n=None, drop="first", handle_unknown="error")
     encoder.fit(X)
     X_t = encoder.transform(X)
@@ -216,6 +217,7 @@ def test_drop_binary_and_top_n_2():
             "col_3": ["a", "a", "a", "a", "a"],
         }
     )
+    X.ww.init(logical_types={"col_1": "categorical", "col_2": "categorical", "col_3": "categorical"})
     encoder = OneHotEncoder(top_n=2, drop="if_binary")
     encoder.fit(X)
     X_t = encoder.transform(X)
@@ -233,7 +235,7 @@ def test_handle_unknown():
             "col_4": [2, 0, 1, 3, 0, 1, 2],
         }
     )
-
+    X.ww.init(logical_types={"col_1": "categorical", "col_2": "categorical", "col_3": "categorical"})
     encoder = OneHotEncoder(handle_unknown="error")
     encoder.fit(X)
     assert isinstance(encoder.transform(X), pd.DataFrame)
@@ -299,6 +301,7 @@ def test_categories():
             "col_4": [2, 0, 1, 3, 0, 1, 2],
         }
     )
+    X.ww.init(logical_types={"col_1": "categorical", "col_2": "categorical", "col_3": "categorical"})
 
     categories = [["a", "b", "c", "d"], ["a", "b", "c"], ["a", "b"]]
 
@@ -373,6 +376,7 @@ def test_more_top_n_unique_values():
             "col_4": [2, 0, 1, 3, 0, 1, 2],
         }
     )
+    X.ww.init(logical_types={"col_1": "categorical", "col_2": "categorical", "col_3": "categorical"})
 
     random_seed = 2
 
@@ -521,21 +525,24 @@ def test_large_number_of_categories():
 @pytest.mark.parametrize("data_type", ["list", "np", "pd_no_index", "pd_index", "ww"])
 def test_data_types(data_type):
     if data_type == "list":
-        X = [["a"], ["b"], ["c"]]
+        X = [["a"], ["b"], ["c"]]*5
     elif data_type == "np":
-        X = np.array([["a"], ["b"], ["c"]])
+        X = np.array([["a"], ["b"], ["c"]]*5)
     elif data_type == "pd_no_index":
-        X = pd.DataFrame(["a", "b", "c"])
+        X = pd.DataFrame(["a", "b", "c"]*5)
     elif data_type == "pd_index":
-        X = pd.DataFrame(["a", "b", "c"], columns=["0"])
+        X = pd.DataFrame(["a", "b", "c"]*5, columns=["0"])
     elif data_type == "ww":
-        X = pd.DataFrame(["a", "b", "c"])
+        X = pd.DataFrame(["a", "b", "c"]*5)
         X.ww.init()
     encoder = OneHotEncoder()
     encoder.fit(X)
     X_t = encoder.transform(X)
     assert list(X_t.columns) == ["0_a", "0_b", "0_c"]
-    np.testing.assert_array_equal(X_t.to_numpy(), np.identity(3))
+    mask = np.identity(3)
+    for _ in range(4):
+        mask = np.vstack((mask, np.identity(3)))
+    np.testing.assert_array_equal(X_t.to_numpy(), mask)
 
 
 @pytest.mark.parametrize(
@@ -563,6 +570,7 @@ def test_ohe_categories():
     X = pd.DataFrame(
         {"col_1": ["a"] * 10, "col_2": ["a"] * 3 + ["b"] * 3 + ["c"] * 2 + ["d"] * 2}
     )
+    X.ww.init(logical_types={"col_2": "categorical"})
     ohe = OneHotEncoder(top_n=2)
     with pytest.raises(
         ComponentNotYetFittedError,
@@ -584,6 +592,7 @@ def test_ohe_get_feature_names():
     X = pd.DataFrame(
         {"col_1": ["a"] * 10, "col_2": ["a"] * 3 + ["b"] * 3 + ["c"] * 2 + ["d"] * 2}
     )
+    X.ww.init(logical_types={"col_2": "categorical"})
     ohe = OneHotEncoder(top_n=2)
     with pytest.raises(
         ComponentNotYetFittedError,
@@ -671,6 +680,7 @@ def test_ohe_top_n_categories_always_the_same():
 
 def test_ohe_column_names_unique():
     df = pd.DataFrame({"A": ["x_y"], "A_x": ["y"]})
+    df.ww.init(logical_types={"A": "categorical", "A_x": "categorical"})
     df_transformed = OneHotEncoder().fit_transform(df)
     assert set(df_transformed.columns) == {"A_x_y", "A_x_y_1"}
 
@@ -685,6 +695,7 @@ def test_ohe_column_names_unique():
             "A_x_y": ["1", "y", "y"],
         }
     )
+    df.ww.init(logical_types={"A": "categorical", "A_x": "categorical", "A_x_y": "categorical"})
     df_transformed = OneHotEncoder().fit_transform(df)
     # category y in A_x gets mapped to A_x_y_1 because A_x_y already exists
     # category 1 in A_x_y gets mapped to A_x_y_1_1 because A_x_y_1 already exists
@@ -693,6 +704,7 @@ def test_ohe_column_names_unique():
     df = pd.DataFrame(
         {"A": ["x_y", "z", "a"], "A_x": ["y_1", "y", "b"], "A_x_y": ["1", "y", "c"]}
     )
+    df.ww.init(logical_types={"A": "categorical", "A_x": "categorical", "A_x_y": "categorical"})
     df_transformed = OneHotEncoder().fit_transform(df)
     # category y in A_x gets mapped to A_x_y_1 because A_x_y already exists
     # category y_1 in A_x gets mapped to A_x_y_1_1 because A_x_y_1 already exists
