b0212dd06a302ac04cf1cc6473609b13b978a925
chukarsten <64713315+chukarsten@users.noreply.github.com>
2021-08-31 21:27:17 -0400

    AutoML & CFEngine Convenience (#2667)
    * Initial.
    
    * Release.
    
    * Added convenience to CFEngine.
    
    * Fixed exception test to hopefully pass CI lint.
    
    * Updated docstrings for AutoML and CFEngine.
    
    * Updated the docs to show AutoMLSearch using a string for the engine and the default CFEngine.
    
    * Removed empty space.
    
    * Addressed most of Freddy's comments...
    
    * Removed if statement in favor of a dictionary.
    
    * Lint.
    
    * Removed unused import of import_or_raise.
    
    * Added a context manager to the DaskEngine to cutdown on test times.
    
    * Reduced pipeline sleep to make the test less wasteful and still recreate the testing conditions.
    
    * Big engine update and rewrite of dask_engine and automl_dask tests.
    
    * Docs update.
    
    * Lint.
    
    * Removed some unused imports and added explicit testing for the build_engine_from_str function.
    
    * Expanded test to cover exception.
    
    * Tightened up the engine choice test for coverage.
    
    * Added CF engine shutdown code and testing for close_engine()
    
    * Added explicit closing testing and the ability to close threaded and process clients.
    
    * Addressed some of Angela's comments and removed the process parallelism from the tests.
    
    * Fixed some of the code coverage.
    
    * Added a no-op close to sequential.
    
    * Fixed typo.
    
    * Updated tests.
    
    * Updates to user guide.
    
    * Added link to CF.
    
    * Added docstring for build_engine_from_str function.
    
    * Clarified AutoML docstring for engine param.
    
    * Clarified close_engine() comments.
    
    * Updated CFClient/Engine to return is_closed.
    
    * Added DaskEngine's is_closed property.
    
    * Added testing for closing the Dask Engine in its own file.
    
    * Lint.
    
    * Made dask schema test conform to using the context manager.
    
    * Updated the build_engine_from_str test to include sequential.
    
    * Updated release notes.
    
    * Added Engines to API ref.
    
    * Fixed test coverage and autoapi mistake.
    
    * try
    
    * [DO NOT MERGE] test doc warnings (#2720)
    
    * init
    
    * try
    
    * Closed the engines in test_build_engine.
    
    Co-authored-by: Angela Lin <angela97lin@gmail.com>

--- evalml/tests/conftest.py
+++ evalml/tests/conftest.py
@@ -221,7 +221,7 @@ def X_y_binary():
     return X, y
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def X_y_binary_cls():
     X, y = datasets.make_classification(
         n_samples=100, n_features=20, n_informative=2, n_redundant=2, random_state=0
