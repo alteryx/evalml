f11c1c989568f0bf2d2e19752afe0f8d46a02483
freddyaboulton <alfonsoboulton@gmail.com>
2021-09-01 15:55:10 -0400

    Fix variable names in weather.py

--- evalml/demos/weather.py
+++ evalml/demos/weather.py
@@ -6,10 +6,11 @@ from evalml.utils import infer_feature_types
 
 
 def load_weather():
+    """Load the Australian daily-min-termperatures weather dataset."""
     currdir_path = os.path.dirname(os.path.abspath(__file__))
     data_folder_path = os.path.join(currdir_path, "data")
-    churn_data_path = os.path.join(data_folder_path, "daily-min-temperatures.csv")
-    X = pd.read_csv(churn_data_path)
+    weather_data_path = os.path.join(data_folder_path, "daily-min-temperatures.csv")
+    X = pd.read_csv(weather_data_path)
     X = infer_feature_types(X, feature_types={"Date": "Datetime", "Temp": "Double"})
     y = X.ww.pop("Temp")
     return X, y
