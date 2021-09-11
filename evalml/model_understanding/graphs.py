3c889a8c5a1d69235b9ebf2bab97a8c5064c53aa
Angela Lin <angela97lin@gmail.com>
2021-09-11 02:01:02 -0400

    try

--- evalml/model_understanding/graphs.py
+++ evalml/model_understanding/graphs.py
@@ -1586,6 +1586,7 @@ def t_sne(
         perplexity (float, optional): Related to the number of nearest neighbors that is used in other manifold learning algorithms. Larger datasets usually require a larger perplexity. Consider selecting a value between 5 and 50.
         learning_rate (float, optional): Usually in the range [10.0, 1000.0]. If the cost function gets stuck in a bad local minimum, increasing the learning rate may help.
         metric (str, optional): The metric to use when calculating distance between instances in a feature array.
+        **kwargs: Additional keyword arguments.
 
     Returns:
         np.ndarray (n_samples, n_components): TSNE output.
@@ -1632,6 +1633,7 @@ def graph_t_sne(
         metric (str, optional): The metric to use when calculating distance between instances in a feature array.
         marker_line_width (int, optional): Determines the line width of the marker boundary.
         marker_size (int, optional): Determines the size of the marker.
+        **kwargs: Additional keyword arguments.
 
     Returns:
         plotly.Figure: Figure representing the transformed data.
