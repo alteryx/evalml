a129a0b432d3f1eca17ebc94a7cacdcd4ca7c3c2
Jeremy Shih <jeremyliweishih@gmail.com>
2021-09-20 16:57:53 -0400

    Remove unncessary change

--- evalml/automl/engine/dask_engine.py
+++ evalml/automl/engine/dask_engine.py
@@ -1,6 +1,5 @@
 """A Future-like wrapper around jobs created by the DaskEngine."""
 import joblib
-
 from dask.distributed import Client, LocalCluster
 
 from evalml.automl.engine.engine_base import (
