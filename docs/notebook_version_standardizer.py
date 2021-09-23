3cabb2c2d0ff7c1681f5e13761c437c11c0b026b
Bryan Chen <bchen1116@gmail.com>
2021-09-23 10:52:21 -0400

    Add lint to fix jupyter executed cells (#2829)
    * update to clean juptyer
    
    * updat release note
    
    * fix naming
    
    * change null to none

--- docs/notebook_version_standardizer.py
+++ docs/notebook_version_standardizer.py
@@ -45,6 +45,40 @@ def _standardize_versions(notebooks, desired_version="3.8.6"):
         _standardize_python_version(notebook, desired_version)
 
 
+def _check_execution_and_output(notebook):
+    with open(notebook, "r") as f:
+        source = json.load(f)
+        for cells in source["cells"]:
+            if cells["cell_type"] == "code" and cells["execution_count"] != None:
+                return False
+    return True
+
+
+def _fix_execution_and_output(notebook):
+    with open(notebook, "r") as f:
+        source = json.load(f)
+        for cells in source["cells"]:
+            if cells["cell_type"] == "code" and cells["execution_count"] != None:
+                cells["execution_count"] = None
+                cells["outputs"] = []
+        source["metadata"]["kernelspec"]["display_name"] = "Python 3"
+        source["metadata"]["kernelspec"]["name"] = "python3"
+    json.dump(source, open(notebook, "w"), indent=1)
+
+
+def _get_notebooks_with_executions(notebooks):
+    executed = []
+    for notebook in notebooks:
+        if not _check_execution_and_output(notebook):
+            executed.append(notebook)
+    return executed
+
+
+def _standardize_outputs(notebooks):
+    for notebook in notebooks:
+        _fix_execution_and_output(notebook)
+
+
 @click.group()
 def cli():
     """no-op"""
@@ -81,12 +115,33 @@ def standardize(desired_version):
     different_versions = _get_notebooks_with_different_versions(
         notebooks, desired_version
     )
+    executed_notebooks = _get_notebooks_with_executions(notebooks)
     if different_versions:
         _standardize_versions(different_versions, desired_version)
         different_versions = ["\t" + notebook for notebook in different_versions]
         different_versions = "\n".join(different_versions)
         click.echo(
             f"Set the notebook version to {desired_version} for:\n {different_versions}"
+        )
+    if executed_notebooks:
+        _standardize_outputs(executed_notebooks)
+        executed_notebooks = ["\t" + notebook for notebook in executed_notebooks]
+        executed_notebooks = "\n".join(executed_notebooks)
+        click.echo(
+            f"Removed the outputs for:\n {executed_notebooks}"
+        )
+
+
+@cli.command()
+def check_execution():
+    notebooks = _get_ipython_notebooks(DOCS_PATH)
+    executed_notebooks = _get_notebooks_with_executions(notebooks)
+    if executed_notebooks:
+        executed_notebooks = ["\t" + notebook for notebook in executed_notebooks]
+        executed_notebooks = "\n".join(executed_notebooks)
+        raise SystemExit(
+            f"The following notebooks have executed outputs:\n {executed_notebooks}\n"
+            "Please run make lint-fix to fix this."
         )
 
 
