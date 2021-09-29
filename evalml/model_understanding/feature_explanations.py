81cf0aa097f96f7f269824a995d2c269d1c5fae6
Becca McBrayer <becca.mcbrayer@alteryx.com>
2021-09-29 14:59:28 -0400

    More testing

--- evalml/model_understanding/feature_explanations.py
+++ evalml/model_understanding/feature_explanations.py
@@ -4,6 +4,7 @@ from evalml.model_family import ModelFamily
 from evalml.model_understanding import calculate_permutation_importance
 from evalml.utils import get_logger, infer_feature_types
 
+
 def explain(
     pipeline,
     X=None,
@@ -13,12 +14,12 @@ def explain(
     min_importance_threshold=0.05,
     objective="auto",
 ):
-    """ Outputs a human-readable explanation of trained pipeline behavior.
+    """Outputs a human-readable explanation of trained pipeline behavior.
 
     Args:
         pipeline (PipelineBase): The pipeline to explain.
         X (pd.DataFrame): If importance_method is permutation, the holdout X data to compute importance with. Ignored otherwise.
-        y (pd.Series): The holdout y data, used to obtain the name of the target class. If importance_method is permutation, used to compute importance with. 
+        y (pd.Series): The holdout y data, used to obtain the name of the target class. If importance_method is permutation, used to compute importance with.
         importance_method (str): The method of determining feature importance. One of ["permutation", "feature"]. Defaults to "permutation".
         max_features (int): The maximum number of features to include in an explanation. Defaults to 5.
         min_importance_threshold (float): The minimum percent of total importance a single feature can have to be considered important. Defaults to 0.05.
@@ -27,45 +28,76 @@ def explain(
     """
     logger = get_logger(f"{__name__}.explain")
 
-    if objective == "auto":    
-        objective = {
-            "binary": "Log Loss Binary",
-            "multiclass": "Log Loss Multiclass",
-            "regression": "R2",
-            "time series regression": "R2",
-            "time series binary": "Log Loss Binary",
-            "time series multiclass": "Log Loss Multiclass",
-        }[pipeline.problem_type.value]
+    if not pipeline._is_fitted:
+        raise ValueError(
+            "Pipelines must be fitted in order to run feature explanations."
+        )
+
+    if min_importance_threshold > 1 or min_importance_threshold < 0:
+        raise ValueError(
+            f"The minimum importance threshold must be a percentage value between 0 and 1, not {min_importance_threshold}."
+        )
 
     if importance_method == "permutation":
+
+        if objective == "auto":
+            objective = {
+                "binary": "Log Loss Binary",
+                "multiclass": "Log Loss Multiclass",
+                "regression": "R2",
+                "time series regression": "R2",
+                "time series binary": "Log Loss Binary",
+                "time series multiclass": "Log Loss Multiclass",
+            }[pipeline.problem_type.value]
+
+        if X is None or y is None:
+            raise ValueError(
+                "X and y are required parameters for explaining pipelines with permutation importance."
+            )
+
         X = infer_feature_types(X)
         y = infer_feature_types(y)
         imp_df = calculate_permutation_importance(pipeline, X, y, objective)
     elif importance_method == "feature":
+        objective = None
         imp_df = pipeline.feature_importance
     else:
         raise ValueError(f"Unknown importance method {importance_method}")
 
-    if min_importance_threshold > 1 or min_importance_threshold < 0:
-        raise ValueError(f'The minimum importance threshold must be a percentage value between 0 and 1, not {min_importance_threshold}')
-
     linear_importance = False
-    if pipeline.estimator.model_family == ModelFamily.LINEAR_MODEL and importance_method == "feature":
+    if (
+        pipeline.estimator.model_family == ModelFamily.LINEAR_MODEL
+        and importance_method == "feature"
+    ):
         linear_importance = True
 
-    most_important_features, somewhat_important_features, detrimental_features = get_influential_features(
+    (
+        most_important_features,
+        somewhat_important_features,
+        detrimental_features,
+    ) = get_influential_features(
         imp_df,
         max_features,
         min_importance_threshold,
         linear_importance,
     )
-    explanation = _fill_template(pipeline.estimator, y.name, objective, most_important_features, somewhat_important_features, detrimental_features)
+    target = y if y is None else y.name
+    explanation = _fill_template(
+        pipeline.estimator,
+        target,
+        objective,
+        most_important_features,
+        somewhat_important_features,
+        detrimental_features,
+    )
     logger.info(explanation)
 
 
-def get_influential_features(imp_df, max_features=5, min_importance_threshold=0.05, linear_importance=False):
-    heavy_importance_threshold = max(0.2, min_importance_threshold+0.1)
-    
+def get_influential_features(
+    imp_df, max_features=5, min_importance_threshold=0.05, linear_importance=False
+):
+    heavy_importance_threshold = max(0.2, min_importance_threshold + 0.1)
+
     # Separate negative and positive features, if situation calls
     if linear_importance:
         pos_imp_df = imp_df
@@ -76,18 +108,32 @@ def get_influential_features(imp_df, max_features=5, min_importance_threshold=0.
         pos_imp_df = imp_df[imp_df["importance"] >= 0]
 
     # Normalize the positive features to sum to 1
-    pos_imp_df["importance"] = pos_imp_df["importance"]/sum(pos_imp_df["importance"])
+    pos_imp_df["importance"] = pos_imp_df["importance"] / sum(pos_imp_df["importance"])
 
     num_feats = min(len(pos_imp_df), max_features)
     imp_features = pos_imp_df[:num_feats]
     return (
-      list(imp_features[imp_features["importance"]>=heavy_importance_threshold]["feature"]),
-      list(imp_features[imp_features["importance"].between(min_importance_threshold, heavy_importance_threshold)]["feature"]),
-      list(neg_imp_df["feature"])
-      )
+        list(
+            imp_features[imp_features["importance"] >= heavy_importance_threshold][
+                "feature"
+            ]
+        ),
+        list(
+            imp_features[
+                imp_features["importance"].between(
+                    min_importance_threshold,
+                    heavy_importance_threshold,
+                    inclusive="left",
+                )
+            ]["feature"]
+        ),
+        list(neg_imp_df["feature"]),
+    )
 
 
-def _fill_template(estimator, target, objective, most_important, somewhat_important, detrimental_feats):
+def _fill_template(
+    estimator, target, objective, most_important, somewhat_important, detrimental_feats
+):
     if objective is not None:
         if target is not None:
             beginning = f"{estimator}: The performance of predicting {target} as measured by {objective.lower()}"
@@ -102,7 +148,7 @@ def _fill_template(estimator, target, objective, most_important, somewhat_import
     def enumerate_features(feature_list):
         text = "" if len(feature_list) == 2 else ","
         for i in range(1, len(feature_list)):
-            if i == len(feature_list)-1:
+            if i == len(feature_list) - 1:
                 text = text + f" and {feature_list[i]}"
             else:
                 text = text + f" {feature_list[i]},"
@@ -141,6 +187,10 @@ def _fill_template(estimator, target, objective, most_important, somewhat_import
             detrimental = f" The features {detrimental_feats[0]}"
             detrimental = detrimental + enumerate_features(detrimental_feats)
             tag = "these features."
-        detrimental = detrimental + " detracted from model performance. We suggest removing " + tag
+        detrimental = (
+            detrimental
+            + " detracted from model performance. We suggest removing "
+            + tag
+        )
 
     return beginning + heavy + somewhat + neither + detrimental
--- evalml/tests/model_understanding_tests/test_feature_explanations.py
+++ evalml/tests/model_understanding_tests/test_feature_explanations.py
@@ -1,32 +1,235 @@
+from unittest.mock import patch
+
+import numpy as np
 import pandas as pd
+import pytest
 
 from evalml.model_understanding import explain, get_influential_features
+from evalml.pipelines import BinaryClassificationPipeline
+
+xgboost_component_graph = [
+    "Imputer",
+    "One Hot Encoder",
+    "DateTime Featurization Component",
+    "XGBoost Classifier",
+]
+
+elasticnet_component_graph = [
+    "Imputer",
+    "One Hot Encoder",
+    "DateTime Featurization Component",
+    "Elastic Net Classifier",
+]
+
 
 def test_get_influential_features():
-    importance_df = pd.DataFrame({'feature': ['heavy influence', 'somewhat influence', 'zero influence', 'negative influence'],
-                                  'importance': [0.6, 0.1, 0.0, -0.1]})
-    heavy, somewhat, negative = get_influential_features(importance_df, max_features=5)
-    assert heavy == ['heavy influence']
-    assert somewhat == ['somewhat influence']
-    assert negative == ['negative influence']
+    importance_df = pd.DataFrame(
+        {
+            "feature": [
+                "heavy influence",
+                "somewhat influence",
+                "zero influence",
+                "negative influence",
+            ],
+            "importance": [0.6, 0.1, 0.0, -0.1],
+        }
+    )
+    heavy, somewhat, negative = get_influential_features(importance_df)
+    assert heavy == ["heavy influence"]
+    assert somewhat == ["somewhat influence"]
+    assert negative == ["negative influence"]
+
 
 def test_get_influential_features_max_features():
-    importance_df = pd.DataFrame({'feature': ['heavy 1', 'heavy 2', 'heavy 3', 'somewhat 1', 'somewhat 2'],
-                                  'importance': [0.35, 0.3, 0.23, 0.15, 0.07]})
+    importance_df = pd.DataFrame(
+        {
+            "feature": ["heavy 1", "heavy 2", "heavy 3", "somewhat 1", "somewhat 2"],
+            "importance": [0.35, 0.3, 0.23, 0.15, 0.07],
+        }
+    )
     heavy, somewhat, negative = get_influential_features(importance_df, max_features=2)
-    assert heavy == ['heavy 1', 'heavy 2']
+    assert heavy == ["heavy 1", "heavy 2"]
     assert somewhat == []
     assert negative == []
 
     heavy, somewhat, negative = get_influential_features(importance_df, max_features=4)
-    assert heavy == ['heavy 1', 'heavy 2', 'heavy 3']
-    assert somewhat == ['somewhat 1']
+    assert heavy == ["heavy 1", "heavy 2", "heavy 3"]
+    assert somewhat == ["somewhat 1"]
     assert negative == []
 
+
 def test_get_influential_features_max_features_ignore_negative():
-    importance_df = pd.DataFrame({'feature': ['heavy 1', 'heavy 2', 'heavy 3', 'neg 1', 'neg 2', 'neg 3'],
-                                  'importance': [0.35, 0.3, 0.23, -0.15, -0.17, -0.43]})
+    importance_df = pd.DataFrame(
+        {
+            "feature": ["heavy 1", "heavy 2", "heavy 3", "neg 1", "neg 2", "neg 3"],
+            "importance": [0.35, 0.3, 0.23, -0.15, -0.17, -0.43],
+        }
+    )
     heavy, somewhat, negative = get_influential_features(importance_df, max_features=2)
-    assert heavy == ['heavy 1', 'heavy 2']
+    assert heavy == ["heavy 1", "heavy 2"]
     assert somewhat == []
-    assert negative == ['neg 1', 'neg 2', 'neg 3']
+    assert negative == ["neg 1", "neg 2", "neg 3"]
+
+
+def test_get_influential_features_lower_min_importance_threshold():
+    importance_df = pd.DataFrame(
+        {"feature": np.arange(100), "importance": [0.1] * 25 + [0.05] * 75}
+    )
+    heavy, somewhat, negative = get_influential_features(importance_df)
+    assert heavy == []
+    assert somewhat == []
+    assert negative == []
+
+    heavy, somewhat, negative = get_influential_features(
+        importance_df, min_importance_threshold=0.01
+    )
+    assert heavy == []
+    assert somewhat == [0, 1, 2, 3, 4]
+    assert negative == []
+
+
+def test_get_influential_features_higher_min_importance_threshold():
+    importance_df = pd.DataFrame(
+        {"feature": np.arange(5), "importance": [0.55, 0.201, 0.15, 0.075, 0.024]}
+    )
+    heavy, somewhat, negative = get_influential_features(importance_df)
+    assert heavy == [0, 1]
+    assert somewhat == [2, 3]
+    assert negative == []
+
+    heavy, somewhat, negative = get_influential_features(
+        importance_df, min_importance_threshold=0.15
+    )
+    assert heavy == [0]
+    assert somewhat == [1, 2]
+    assert negative == []
+
+
+def test_get_influential_features_linear_importance():
+    importance_df = pd.DataFrame(
+        {
+            "feature": [
+                "heavy influence",
+                "negative influence",
+                "somewhat influence",
+                "zero influence",
+            ],
+            "importance": [0.6, -0.2, 0.1, 0.0],
+        }
+    )
+    heavy, somewhat, negative = get_influential_features(
+        importance_df, linear_importance=True
+    )
+    assert heavy == ["heavy influence", "negative influence"]
+    assert somewhat == ["somewhat influence"]
+    assert negative == []
+
+
+def test_explain_pipeline_not_fitted(fraud_100):
+    pipeline = BinaryClassificationPipeline(xgboost_component_graph)
+    X, y = fraud_100
+
+    with pytest.raises(
+        ValueError,
+        match="Pipelines must be fitted in order to run feature explanations",
+    ):
+        explain(pipeline, X, y)
+
+    with pytest.raises(
+        ValueError,
+        match="Pipelines must be fitted in order to run feature explanations",
+    ):
+        explain(pipeline, importance_method="feature")
+
+
+def test_explain_pipeline_missing_X_y(fraud_100):
+    pipeline = BinaryClassificationPipeline(xgboost_component_graph)
+    X, y = fraud_100
+    pipeline._is_fitted = True
+
+    with pytest.raises(
+        ValueError, match="required parameters for explaining pipelines"
+    ):
+        explain(pipeline)
+
+    with pytest.raises(
+        ValueError, match="required parameters for explaining pipelines"
+    ):
+        explain(pipeline, X)
+
+    with pytest.raises(
+        ValueError, match="required parameters for explaining pipelines"
+    ):
+        explain(pipeline, y=y)
+
+
+def test_explain_pipeline_invalid_importance(fraud_100):
+    pipeline = BinaryClassificationPipeline(xgboost_component_graph)
+    X, y = fraud_100
+    pipeline._is_fitted = True
+
+    with pytest.raises(ValueError, match="Unknown importance method"):
+        explain(pipeline, importance_method="fake")
+
+
+def test_explain_pipeline_invalid_min_threshold(fraud_100):
+    pipeline = BinaryClassificationPipeline(xgboost_component_graph)
+    X, y = fraud_100
+    pipeline._is_fitted = True
+
+    with pytest.raises(
+        ValueError, match="minimum importance threshold must be a percentage"
+    ):
+        explain(pipeline, X, y, min_importance_threshold=-1)
+
+    with pytest.raises(
+        ValueError, match="minimum importance threshold must be a percentage"
+    ):
+        explain(pipeline, X, y, min_importance_threshold=2)
+
+
+def test_explain_pipeline_permutation(caplog, fraud_100):
+    pipeline = BinaryClassificationPipeline(elasticnet_component_graph)
+    X, y = fraud_100
+    pipeline.fit(X, y)
+
+    explain(pipeline, X, y, importance_method="permutation")
+
+    out = caplog.text
+    expected_influence = "Elastic Net Classifier: The performance of predicting fraud as measured by log loss binary is heavily influenced by lng."
+    expected_negative = "The features customer_present, lat, amount, card_id, and store_id detracted from model performance. We suggest removing these features."
+
+    assert expected_influence in out
+    assert expected_negative in out
+    caplog.clear()
+
+
+def test_explain_pipeline_different_objective(caplog, fraud_100):
+    pipeline = BinaryClassificationPipeline(xgboost_component_graph)
+    X, y = fraud_100
+    pipeline.fit(X, y)
+
+    explain(pipeline, X, y, objective="precision")
+
+    out = caplog.text
+    expected_output = "XGBoost Classifier: The performance of predicting fraud as measured by precision is heavily influenced by amount"
+
+    assert expected_output in out
+    caplog.clear()
+
+
+def test_explain_pipeline_feature(caplog, fraud_100):
+    pipeline = BinaryClassificationPipeline(elasticnet_component_graph)
+    X, y = fraud_100
+    pipeline.fit(X, y)
+
+    explain(pipeline, importance_method="feature")
+
+    out = caplog.text
+    expected_heavy = "Elastic Net Classifier: The output is heavily influenced by datetime_year and store_id"
+    expected_somewhat = "somewhat influenced by card_id"
+
+    assert expected_heavy in out
+    assert expected_somewhat in out
+    assert "detracted from model performance" not in out
+    caplog.clear()
