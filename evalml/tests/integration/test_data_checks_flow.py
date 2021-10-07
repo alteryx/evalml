8ac51dcb9046aaddec677eba8184e66b22e5a00d
Angela Lin <angela97lin@gmail.com>
2021-10-07 14:28:09 -0400

    init

---
+++ evalml/tests/integration/test_data_checks_flow.py
@@ -0,0 +1,23 @@
+## Test data checks flow:
+## User runs data checks. Gets actions as a result. Uses it to create components. (Eventually pass to AutoML.)
+
+
+def test_empty_checks():
+    # Checks do not return any error.
+    pass
+
+
+def test_return_row_removal():
+    pass
+
+
+def test_impute_col():
+    pass
+
+
+def test_transform_target():
+    pass
+
+
+def test_drop_col():
+    pass
