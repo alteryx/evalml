547e57f07ba50dfd043ed8145ccaea37d7598104
Angela Lin <angela97lin@gmail.com>
2021-10-22 17:20:56 -0400

    add cmdstan builder

--- evalml/tests/dependency_update_check/make_deps_diff.sh
+++ evalml/tests/dependency_update_check/make_deps_diff.sh
@@ -1,3 +1,5 @@
 allow_list=$(cat core-requirements.txt requirements.txt | grep -oE "^[a-zA-Z0-9]+[a-zA-Z0-9_\-]*" | paste -d "|" -s -)
+cmd_builder_pkg="cmdstan-builder"
+allow_list="${allow_list}|${cmd_builder_pkg}"
 echo "Allow list: ${allow_list}"
 pip freeze | grep -v "evalml.git" | grep -E "${allow_list}" > "${DEPENDENCY_FILE_PATH}"
