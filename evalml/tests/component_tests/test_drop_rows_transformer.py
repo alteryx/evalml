33fa48e9df8871801d934e8b8fd6cd132f486da3
Angela Lin <angela97lin@gmail.com>
2021-10-27 17:32:10 -0400

    linting

--- evalml/tests/component_tests/test_drop_rows_transformer.py
+++ evalml/tests/component_tests/test_drop_rows_transformer.py
@@ -15,6 +15,7 @@ def test_drop_rows_transformer_init():
     assert drop_rows_transformer.indices_to_drop == [0, 1]
     assert drop_rows_transformer.parameters == {"indices_to_drop": [0, 1]}
 
+
 def test_drop_rows_transformer_init_with_duplicate_indices():
     with pytest.raises(ValueError, match="All input indices must be unique."):
         DropRowsTransformer(indices_to_drop=[0, 0])
