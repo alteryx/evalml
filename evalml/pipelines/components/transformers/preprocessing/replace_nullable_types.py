388ba7768b021e285e575dbad65dd265df97aad5
Karsten Chu <karsten.chu@alteryx.com>
2021-11-23 14:23:19 -0500

    Got target impl written and tested.

--- evalml/pipelines/components/transformers/preprocessing/replace_nullable_types.py
+++ evalml/pipelines/components/transformers/preprocessing/replace_nullable_types.py
@@ -1,23 +1,27 @@
 """Transformer to replace features with the new nullable dtypes with a dtype that is compatible in EvalML."""
 from pandas.core import arrays as pca
+from woodwork import init_series
+from woodwork.exceptions import WoodworkNotInitError
 from woodwork.logical_types import BooleanNullable, IntegerNullable
 
 from evalml.pipelines.components.transformers import Transformer
 from evalml.utils import infer_feature_types
 
 
-def is_nullable_int(df, col):
+def is_nullable_int(series):
     """Function to determine whether a column in a dataframe is a pandas NullableInteger type.
 
     Args:
-        df: Input dataframe to check.
-        col: Column of the input dataframe to check.
+        series: The series to detect NullableIntegers in.
 
     Returns:
         boolean: Whether the column was of NullableInteger type.
     """
-    is_pandas_nullable = isinstance(df.dtypes[col], pca.integer.Int64Dtype)
-    is_ww_nullable = isinstance(df.ww.logical_types[col], IntegerNullable)
+    is_pandas_nullable = isinstance(series.dtype, pca.integer.Int64Dtype)
+    try:
+        is_ww_nullable = isinstance(series.ww.logical_type, IntegerNullable)
+    except WoodworkNotInitError:
+        is_ww_nullable = False
     return is_ww_nullable or is_pandas_nullable
 
 
@@ -34,18 +38,20 @@ def replace_nullable_int(series):
     return series.astype("float64")
 
 
-def is_nullable_bool(df, col):
+def is_nullable_bool(series):
     """Function to determine whether a column in a dataframe is a pandas NullableBoolean type.
 
     Args:
-        df: Input dataframe to check.
-        col: Column of the input dataframe to check.
+        series: The series to detect NullableBooleans in.
 
     Returns:
         boolean: Whether the column was of NullableBoolean type.
     """
-    is_pandas_nullable = isinstance(df.dtypes[col], pca.boolean.BooleanDtype)
-    is_ww_nullable = isinstance(df.ww.logical_types[col], BooleanNullable)
+    is_pandas_nullable = isinstance(series.dtype, pca.boolean.BooleanDtype)
+    try:
+        is_ww_nullable = isinstance(series.ww.logical_type, BooleanNullable)
+    except WoodworkNotInitError:
+        is_ww_nullable = False
     return is_ww_nullable or is_pandas_nullable
 
 
@@ -75,6 +81,7 @@ class ReplaceNullableTypes(Transformer):
 
         self._nullable_int_cols = []
         self._nullable_bool_cols = []
+        self._nullable_target = None
         super().__init__(
             parameters=parameters, component_obj=None, random_seed=random_seed
         )
@@ -91,11 +98,17 @@ class ReplaceNullableTypes(Transformer):
         """
         X_t = infer_feature_types(X, ignore_nullable_types=True)
         for col in X_t.columns:
-            if is_nullable_int(X_t, col):
+            if is_nullable_int(X_t[col]):
                 self._nullable_int_cols.append(col)
-            elif is_nullable_bool(X_t, col):
+            elif is_nullable_bool(X_t[col]):
                 self._nullable_bool_cols.append(col)
-                
+
+        if y is None:
+            self._nullable_target = None
+        elif is_nullable_int(y):
+            self._nullable_target = "nullable_int"
+        elif is_nullable_bool(y):
+            self._nullable_target = "nullable_bool"
         return self
 
     def transform(self, X, y=None):
@@ -114,4 +127,13 @@ class ReplaceNullableTypes(Transformer):
             X_t[col] = replace_nullable_int(X_t[col])
         for col in self._nullable_bool_cols:
             X_t[col] = replace_nullable_bool(X_t[col])
-        return X_t
+
+        y_t = y
+        if y is not None:
+            y_t = init_series(y)
+            if self._nullable_target is not None:
+                if self._nullable_target == "nullable_int":
+                    y_t = replace_nullable_int(y_t)
+                elif self._nullable_target == "nullable_bool":
+                    y_t = replace_nullable_bool(y_t)
+        return X_t, y_t
