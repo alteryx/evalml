dc8d97134b2e605582707d8553be7d53e35d7300
freddyaboulton <alfonsoboulton@gmail.com>
2021-12-06 16:05:59 -0500

    Update comments in bash script

--- import_or_skip.sh
+++ import_or_skip.sh
@@ -1,7 +1,6 @@
 # if there are more than 0 lines with importorskip, go into the if branch
-# In bash, 0 is true so negate the expression
 if [ `grep -nR "importorskip" --include \*.py evalml/tests | wc -l | xargs` -ge 1 ]; then
-    echo The following test files use importorskip. Refactor them to skip if minimal_dependencies_is_true
+    echo The following test files use importorskip. Refactor them to skip if has_minimal_dependencies is true
     echo or add the pytest.mark.noncore_dependency decorator.
     grep -nR "importorskip" --include \*.py evalml/tests
     exit 1
