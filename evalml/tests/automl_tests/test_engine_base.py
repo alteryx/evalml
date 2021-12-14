7c55715c90275391187cc795c7dc11184eadd0be
Parthiv Naresh <parthiv.naresh@alteryx.com>
2021-12-14 17:49:24 +0100

    no message

--- evalml/tests/automl_tests/test_engine_base.py
+++ evalml/tests/automl_tests/test_engine_base.py
@@ -140,18 +140,15 @@ def test_train_pipeline_trains_and_tunes_threshold(
     mock_split_data.assert_called_once()
 
 
-@patch("evalml.automl.engine.engine_base.split_data")
 def test_train_pipeline_trains_and_tunes_threshold_ts(
-    mock_split_data,
     ts_data,
-    AutoMLTestEnv,
     dummy_ts_binary_pipeline_class,
 ):
     X, y = ts_data
     params = {"gap": 1, "max_delay": 1, "forecast_horizon": 1, "time_index": "date"}
     ts_binary = dummy_ts_binary_pipeline_class(parameters={"pipeline": params})
-    mock_split_data.return_value = split_data(
-        X, y, "time series binary", test_size=params["forecast_horizon"] / len(X)
+    X, X_threshold_tuning, y, y_threshold_tuning = split_data(
+        X, y, "time series binary", test_size=0.2
     )
     automl_config = AutoMLConfig(
         None, "time series binary", LogLossBinary(), [], F1(), True, None, 0, None, None
