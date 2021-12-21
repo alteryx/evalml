0a79113d24de07807d4a76c7844d23ccd04b3aa5
bchen1116 <bchen1116@gmail.com>
2021-12-21 16:50:33 -0500

    initial impl

---
+++ evalml/tests/data_checks_tests/test_utils.py
@@ -0,0 +1,34 @@
+import pytest
+
+from evalml.data_checks import DataCheckActionCode
+from evalml.data_checks.utils import handle_data_check_action_code
+from evalml.problem_types import ProblemTypes
+
+
+def test_handle_action_code_errors():
+    with pytest.raises(KeyError, match="Action code 'dropping cols' does not"):
+        handle_data_check_action_code("dropping cols")
+
+    with pytest.raises(
+        ValueError,
+        match="`handle_data_check_action_code` was not passed a str or DataCheckActionCode object",
+    ):
+        handle_data_check_action_code(None)
+
+    with pytest.raises(
+        ValueError,
+        match="`handle_data_check_action_code` was not passed a str or DataCheckActionCode object",
+    ):
+        handle_data_check_action_code(ProblemTypes.BINARY)
+
+
+@pytest.mark.parametrize(
+    "action_code,expected_code",
+    [
+        ("drop_rows", DataCheckActionCode.DROP_ROWS),
+        ("Drop_col", DataCheckActionCode.DROP_COL),
+        ("TRANSFORM_TARGET", DataCheckActionCode.TRANSFORM_TARGET),
+    ],
+)
+def test_handle_action_code(action_code, expected_code):
+    assert handle_data_check_action_code(action_code) == expected_code
