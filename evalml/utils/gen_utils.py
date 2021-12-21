7205a1b1d6d644ef07f38cd82f56aac59b613a72
Becca McBrayer <becca.mcbrayer@alteryx.com>
2021-12-21 09:31:02 -0500

    Add ExponentialSmoothing to automl for perf tests (oops)

--- evalml/utils/gen_utils.py
+++ evalml/utils/gen_utils.py
@@ -202,7 +202,7 @@ def _get_subclasses(base_class):
 _not_used_in_automl = {
     "BaselineClassifier",
     "BaselineRegressor",
-    "ExponentialSmoothingRegressor",
+    #"ExponentialSmoothingRegressor",
     "TimeSeriesBaselineEstimator",
     "StackedEnsembleClassifier",
     "StackedEnsembleRegressor",
