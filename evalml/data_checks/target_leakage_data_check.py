6b252108166f9c2edfbacce76626959c69e81a6a
Angela Lin <angela97lin@gmail.com>
2022-01-04 15:29:02 -0500

    update target leakage data check docstring for consistency

--- evalml/data_checks/target_leakage_data_check.py
+++ evalml/data_checks/target_leakage_data_check.py
@@ -82,9 +82,9 @@ class TargetLeakageDataCheck(DataCheck):
             data leakage.
 
             >>> X = pd.DataFrame({
-            ...    'leak': [10, 42, 31, 51, 61],
-            ...    'x': [42, 54, 12, 64, 12],
-            ...    'y': [13, 5, 13, 74, 24],
+            ...    "leak": [10, 42, 31, 51, 61],
+            ...    "x": [42, 54, 12, 64, 12],
+            ...    "y": [13, 5, 13, 74, 24],
             ... })
             >>> y = pd.Series([10, 42, 31, 51, 40])
             ...
@@ -102,18 +102,18 @@ class TargetLeakageDataCheck(DataCheck):
 
             The default method can be changed to pearson from mutual information.
 
-            >>> X['x'] = y / 2
-            >>> target_leakage_check = TargetLeakageDataCheck(pct_corr_threshold=0.8, method='pearson')
+            >>> X["x"] = y / 2
+            >>> target_leakage_check = TargetLeakageDataCheck(pct_corr_threshold=0.8, method="pearson")
             >>> assert target_leakage_check.validate(X, y) == {
-            ...     "warnings": [{'message': "Columns 'leak', 'x' are 80.0% or more correlated with the target",
-            ...                   'data_check_name': 'TargetLeakageDataCheck',
-            ...                   'level': 'warning',
-            ...                   'details': {'columns': ['leak', 'x'], 'rows': None},
-            ...                   'code': 'TARGET_LEAKAGE'}],
+            ...     "warnings": [{"message": "Columns 'leak', 'x' are 80.0% or more correlated with the target",
+            ...                   "data_check_name": "TargetLeakageDataCheck",
+            ...                   "level": "warning",
+            ...                   "details": {"columns": ["leak", "x"], "rows": None},
+            ...                   "code": "TARGET_LEAKAGE"}],
             ...     "errors": [],
-            ...     "actions": {"action_list": [{'code': 'DROP_COL',
+            ...     "actions": {"action_list": [{"code": "DROP_COL",
             ...                  "data_check_name": "TargetLeakageDataCheck",
-            ...                  'metadata': {'columns': ['leak', 'x'], 'rows': None}}], "default_action": None}}
+            ...                  "metadata": {"columns": ["leak", "x"], "rows": None}}], "default_action": None}}
         """
         results = {
             "warnings": [],
