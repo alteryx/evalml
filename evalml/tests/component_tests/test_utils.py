25a1596449fa06432e887adbf422206214b40e56
freddyaboulton <alfonsoboulton@gmail.com>
2022-01-06 11:02:25 -0500

    Lint

--- evalml/tests/component_tests/test_utils.py
+++ evalml/tests/component_tests/test_utils.py
@@ -112,7 +112,6 @@ not_supported_in_windows_py39 = set(
         "ARIMA Regressor",
         "Polynomial Detrender",
         "Prophet Regressor",
-        "Exponential Smoothing Regressor",
     ]
 )
 not_supported_in_linux_py39 = set(
