09ce87ec8ec5f0d018e7b973d0f49327dcf21e39
Becca McBrayer <becca.mcbrayer@alteryx.com>
2022-03-10 11:20:23 -0500

    Small test fix

--- evalml/utils/gen_utils.py
+++ evalml/utils/gen_utils.py
@@ -244,14 +244,7 @@ def get_importable_subclasses(base_class, used_in_automl=True):
     if used_in_automl:
         from evalml.problem_types import ProblemTypes
 
-        classes = [
-            cls
-            for cls in classes
-            if (
-                cls.__name__ not in _not_used_in_automl
-                and ProblemTypes.CLUSTERING not in cls.supported_problem_types
-            )
-        ]
+        classes = [cls for cls in classes if cls.__name__ not in _not_used_in_automl]
 
     return classes
 
