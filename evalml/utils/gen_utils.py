99f6399160207f3a3ad010d7f35b5da3cfd1b204
Becca McBrayer <becca.mcbrayer@alteryx.com>
2022-03-21 10:39:52 -0400

    Remove unnecessary import statements

--- evalml/utils/gen_utils.py
+++ evalml/utils/gen_utils.py
@@ -246,9 +246,6 @@ def get_importable_subclasses(base_class, used_in_automl=True):
                 f"Could not import class {cls.__name__} in get_importable_subclasses"
             )
     if used_in_automl:
-        from evalml.pipelines import Transformer
-        from evalml.problem_types import ProblemTypes
-
         classes = [cls for cls in classes if (cls.__name__ not in _not_used_in_automl)]
 
     return classes
