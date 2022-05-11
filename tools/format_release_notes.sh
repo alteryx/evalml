9db1a4bdb8a985087d16dcb0dc6dc75d9cd448f8
Becca McBrayer <becca.mcbrayer@alteryx.com>
2022-05-11 14:04:39 -0400

    Let's get some checks green plz

--- tools/format_release_notes.sh
+++ tools/format_release_notes.sh
@@ -9,7 +9,7 @@ Usage: tools/format_release_notes.sh
 Note: this was written to work properly on mac. It should work on linux if -r is used instead of -E for sed.'
     exit 0
 fi
-evalml_version=$(sed -n -E "s/.*version=\'([0-9A-Za-z\.\_]+)\'.*/\1/p" setup.py)
+evalml_version=$(python -c "import evalml; print(evalml.__version__)")
 content_raw=$(sed -n -E '/^\*\*v[A-Za-z0-9\., ]+\*\*$/,$p' docs/source/release_notes.rst | tail -n +2 | sed -E '/\*\*v[A-Za-z0-9\., ]+\*\*$/q' | sed '$ d' | awk 'NF')
 date_formatted=$(date '+%b. %-d, %Y')
 content_markdown=$(echo -n "${content_raw}" | grep -v ".. warning::" | sed 's/^        \* /- /g' | sed 's/^    \* /### /g' | sed 's/    \*\*Breaking Changes\*\*/### Breaking Changes/g' | sed -E "s/:pr:\`([0-9]+)\`/#\1/g")
