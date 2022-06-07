1fb35ca2ff7d53d23837b4e65795082aa0e34c76
Karsten Chu <karsten.chu@alteryx.com>
2022-06-07 09:52:29 -0700

    Forgot a comma in the component graph doctest.

--- evalml/pipelines/component_graph.py
+++ evalml/pipelines/component_graph.py
@@ -58,7 +58,7 @@ class ComponentGraph:
         ...                 'numeric_impute_strategy': 'mean',
         ...                 'boolean_impute_strategy': 'most_frequent',
         ...                 'categorical_fill_value': None,
-        ...                 'numeric_fill_value': None
+        ...                 'numeric_fill_value': None,
         ...                 'boolean_fill_value': None},
         ...     'One Hot Encoder': {'top_n': 10,
         ...                         'features_to_encode': None,
