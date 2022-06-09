5d2c8fd6aa154c64ab907f7caaa0e834be4a72f5
Karsten Chu <karsten.chu@alteryx.com>
2022-06-09 16:40:00 -0700

    Missed a refactor to use the is_categorical_actually_boolean function.

--- evalml/pipelines/components/transformers/imputers/simple_imputer.py
+++ evalml/pipelines/components/transformers/imputers/simple_imputer.py
@@ -142,9 +142,7 @@ class SimpleImputer(Transformer):
         cat_cols = X.ww.select(["Categorical"], return_schema=True)
         cat_cols = [x for x in cat_cols.columns.keys()]
         for col in cat_cols:
-            if {True, False}.issubset(set(X[col].unique())) and any(
-                isinstance(x, bool) for x in X[col].unique()
-            ):
+            if is_categorical_actually_boolean(X, col):
                 X_t[col] = X_t[col].astype(bool)
 
         # Convert Nullable Integers to Doubles for the "mean" and "median" strategies
