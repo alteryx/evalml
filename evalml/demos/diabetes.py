af7f153207bea21ae4a982ae9320cd8069b433e5
Michael Fu <michael.fu@alteryx.com>
2022-06-28 14:37:03 -0700

    Mispelled a word oops

--- evalml/demos/diabetes.py
+++ evalml/demos/diabetes.py
@@ -23,7 +23,7 @@ def load_diabetes():
     y = y.astype(float)
     numpy_of_X = X.to_numpy()
     numpy_of_X = scale(numpy_of_X, copy=False)
-    umpy_of_X /= numpy_of_X.shape[0] ** 0.5
+    numpy_of_X /= numpy_of_X.shape[0] ** 0.5
     X = pd.DataFrame(numpy_of_X, columns=X.columns)
     X.ww.init()
     y = ww.init_series(y)
