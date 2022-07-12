2a80c56c44da67ccdc8f3a52b48646baf31b2be2
Michael Fu <michael.fu@alteryx.com>
2022-07-12 13:00:30 -0700

    fixed docstring example

--- evalml/data_checks/unknown_type_data_check.py
+++ evalml/data_checks/unknown_type_data_check.py
@@ -46,16 +46,16 @@ class UnknownTypeDataCheck(DataCheck):
             >>> unknown_type_dc = UnknownTypeDataCheck()
             >>> assert unknown_type_dc.validate(df) == [
             ...     {
-            ...         "message": "2 out of 4 rows are unknown type, meaning the number of rows that are unknown is more than 50.0%."
-            ...         "data_check_name": "UnknownTypeDataCheck"
+            ...         "message": "2 out of 4 rows are unknown type, meaning the number of rows that are unknown is more than 50.0%.",
+            ...         "data_check_name": "UnknownTypeDataCheck",
             ...         "level": "warning",
             ...         "details": {
-            ...             "columns": ['all_null", "literally_all_null"],
+            ...             "columns": ["all_null", "literally_all_null"],
             ...             "rows": None,
             ...         },
             ...         "code": "HIGH_NUMBER_OF_UNKNOWN_TYPE",
-            ...         "action_options": []
-            ...      }    
+            ...         "action_options": [],
+            ...      }
             ... ]
         """
         messages = []
