d7a4de6b5667441a6731fd6c2fcd8ac7903492d3
Jeremy Shih <jeremyliweishih@gmail.com>
2022-08-01 15:05:02 -0400

    Add sleep for test case

--- evalml/tests/automl_tests/test_progress.py
+++ evalml/tests/automl_tests/test_progress.py
@@ -54,6 +54,8 @@ def test_progress_return_progress(X_y_binary, logistic_regression_binary_pipelin
 
     assert p.should_continue(mock_results)
 
+    time.sleep(1)
+
     progress_dict = p.return_progress()
     for progress in progress_dict:
         if progress["stopping_criteria"] == "max_time":
