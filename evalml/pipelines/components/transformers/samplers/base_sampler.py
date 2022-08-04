33b8a9e56052c881ee11f2fe1192e02bd3ea3e2e
Karsten Chu <karsten.chu@alteryx.com>
2022-08-04 11:08:47 -0400

    Changes to the standard_scaler and base_sampler to fix test_can_run_automl_for_time_series_known_in_advance.  Requires MR#1485 on Woodwork to be approved, merged and released.

--- evalml/pipelines/components/transformers/samplers/base_sampler.py
+++ evalml/pipelines/components/transformers/samplers/base_sampler.py
@@ -2,6 +2,8 @@
 import copy
 from abc import abstractmethod
 
+from woodwork.logical_types import BooleanNullable, IntegerNullable
+
 from evalml.pipelines.components.transformers import Transformer
 from evalml.utils.woodwork_utils import infer_feature_types
 
@@ -57,7 +59,17 @@ class BaseSampler(Transformer):
         Returns:
             pd.DataFrame, pd.Series: Prepared X and y data as pandas types
         """
+
         X = infer_feature_types(X)
+        try:
+            X = X.astype(
+                {null_col: int for null_col in X.ww.select(IntegerNullable).columns},
+            )
+        except ValueError:
+            X = X.astype(
+                {null_col: float for null_col in X.ww.select(IntegerNullable).columns},
+            )
+        X.ww.init()
         if y is None:
             raise ValueError("y cannot be None")
         y = infer_feature_types(y)
