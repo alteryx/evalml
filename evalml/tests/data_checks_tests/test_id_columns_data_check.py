cb7d74673d43a2497096e6b2fb35ed9c8b37f9c0
simha104 <pranav.simha@alteryx.com>
2022-08-04 10:38:27 -0700

    Test fixes

--- evalml/tests/data_checks_tests/test_id_columns_data_check.py
+++ evalml/tests/data_checks_tests/test_id_columns_data_check.py
@@ -141,46 +141,6 @@ def test_id_columns_strings():
         ).to_dict(),
     ]
 
-    X_dict = {
-        "col_1_id": ["a", "b", "c", "d"],
-        "col_2": ["w", "x", "y", "z"],
-        "col_3_id": [
-            "123456789012345",
-            "234567890123456",
-            "3456789012345678",
-            "45678901234567",
-        ],
-        "Id": ["z", "y", "x", "a"],
-        "col_5": ["0", "0", "1", "2"],
-        "col_6": [0.1, 0.2, 0.3, 0.4],
-    }
-    X = pd.DataFrame.from_dict(X_dict)
-    X.ww.init(
-        logical_types={
-            "col_1_id": "categorical",
-            "col_2": "categorical",
-            "Id": "categorical",
-            "col_5": "categorical",
-        },
-    )
-
-    id_cols_check = IDColumnsDataCheck(id_threshold=0.95)
-    assert id_cols_check.validate(X) == [
-        DataCheckWarning(
-            message="The first column 'col_1_id' is most likely to be the ID column. Columns 'Id', 'col_2', 'col_3_id' are also 95.0% or more likely to be an ID column",
-            data_check_name=id_data_check_name,
-            message_code=DataCheckMessageCode.HAS_ID_FIRST_COLUMN,
-            details={"columns": ["col_1_id", "Id", "col_2", "col_3_id"]},
-            action_options=[
-                DataCheckActionOption(
-                    DataCheckActionCode.SET_FIRST_COL_ID,
-                    data_check_name=id_data_check_name,
-                    metadata={"columns": ["col_1_id", "Id", "col_2", "col_3_id"]},
-                ),
-            ],
-        ).to_dict(),
-    ]
-
 
 def test_id_cols_data_check_input_formats():
     id_cols_check = IDColumnsDataCheck(id_threshold=0.8)
@@ -193,13 +153,13 @@ def test_id_cols_data_check_input_formats():
     ww_input.ww.init()
     assert id_cols_check.validate(ww_input) == [
         DataCheckWarning(
-            message="The first column '0' is most likely to be the ID column. Columns '1' are also 80.0% or more likely to be an ID column",
+            message="Columns '0', '1' are 80.0% or more likely to be an ID column",
             data_check_name=id_data_check_name,
-            message_code=DataCheckMessageCode.HAS_ID_FIRST_COLUMN,
+            message_code=DataCheckMessageCode.HAS_ID_COLUMN,
             details={"columns": [0, 1]},
             action_options=[
                 DataCheckActionOption(
-                    DataCheckActionCode.SET_FIRST_COL_ID,
+                    DataCheckActionCode.DROP_COL,
                     data_check_name=id_data_check_name,
                     metadata={"columns": [0, 1]},
                 ),
@@ -210,13 +170,13 @@ def test_id_cols_data_check_input_formats():
     #  test 2D list
     assert id_cols_check.validate([[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]) == [
         DataCheckWarning(
-            message="The first column '0' is most likely to be the ID column. Columns '1' are also 80.0% or more likely to be an ID column",
+            message="Columns '0', '1' are 80.0% or more likely to be an ID column",
             data_check_name=id_data_check_name,
-            message_code=DataCheckMessageCode.HAS_ID_FIRST_COLUMN,
+            message_code=DataCheckMessageCode.HAS_ID_COLUMN,
             details={"columns": [0, 1]},
             action_options=[
                 DataCheckActionOption(
-                    DataCheckActionCode.SET_FIRST_COL_ID,
+                    DataCheckActionCode.DROP_COL,
                     data_check_name=id_data_check_name,
                     metadata={"columns": [0, 1]},
                 ),
@@ -229,13 +189,13 @@ def test_id_cols_data_check_input_formats():
         np.array([[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]),
     ) == [
         DataCheckWarning(
-            message="The first column '0' is most likely to be the ID column. Columns '1' are also 80.0% or more likely to be an ID column",
+            message="Columns '0', '1' are 80.0% or more likely to be an ID column",
             data_check_name=id_data_check_name,
-            message_code=DataCheckMessageCode.HAS_ID_FIRST_COLUMN,
+            message_code=DataCheckMessageCode.HAS_ID_COLUMN,
             details={"columns": [0, 1]},
             action_options=[
                 DataCheckActionOption(
-                    DataCheckActionCode.SET_FIRST_COL_ID,
+                    DataCheckActionCode.DROP_COL,
                     data_check_name=id_data_check_name,
                     metadata={"columns": [0, 1]},
                 ),
