22e0dda2c4ffdecedaa5590d48f6e0d2d56b8cdd
christopherbunn <chris.bunn@alteryx.com>
2022-09-01 11:47:54 -0400

    Fixed estimator name

--- evalml/tests/component_tests/test_estimators.py
+++ evalml/tests/component_tests/test_estimators.py
@@ -215,7 +215,7 @@ def test_estimator_predict_output_type(X_y_binary, helper_functions):
             predict_output = component.predict(X)
             assert isinstance(predict_output, pd.Series)
             assert len(predict_output) == len(y)
-            if component.name != "ExponentialSmoothing":
+            if component.name != "Exponential Smoothing Regressor":
                 assert predict_output.name is None
 
             if not (
