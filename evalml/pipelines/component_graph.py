737be25c2ad1daf928334c14569d568feb0b55c1
Karsten Chu <karsten.chu@alteryx.com>
2022-09-16 00:40:58 -0400

    Modified the component_graph.py docstring.

--- evalml/pipelines/component_graph.py
+++ evalml/pipelines/component_graph.py
@@ -953,7 +953,7 @@ class ComponentGraph:
     def inverse_transform(self, y):
         """Apply component inverse_transform methods to estimator predictions in reverse order.
 
-        Components that implement inverse_transform are PolynomialDecomposer, LabelEncoder (tbd).
+        Components that implement inverse_transform are PolynomialDecomposer, LogTransformer, LabelEncoder (tbd).
 
         Args:
             y: (pd.Series): Final component features.
