b2a88155056ae03c853b30c95d1fccdedcce1603
Karsten Chu <karsten.chu@alteryx.com>
2022-10-13 13:49:25 -0400

    Removed raise e

--- evalml/automl/engine/engine_base.py
+++ evalml/automl/engine/engine_base.py
@@ -266,7 +266,6 @@ def train_and_score_pipeline(
             score = scores[automl_config.objective.name]
             pipeline_cache[hashes] = fitted_pipeline.component_graph.component_instances
         except Exception as e:
-            raise e
             if automl_config.error_callback is not None:
                 automl_config.error_callback(
                     exception=e,
