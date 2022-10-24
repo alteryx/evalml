6dfd2ea178abb48f3163fbdf499c005a9fa5a368
Jeremy Shih <jeremyliweishih@gmail.com>
2022-10-24 16:03:36 -0400

    Fix output in make_deps_diff.sh

--- evalml/tests/dependency_update_check/make_deps_diff.sh
+++ evalml/tests/dependency_update_check/make_deps_diff.sh
@@ -1,4 +1,4 @@
-reqs_list=$(python -c "import os; from evalml.utils import get_evalml_pip_requirements; print('\n'.join(get_evalml_pip_requirements(os.getcwd())))")
+reqs_list=$(python -c "import os; import sys; import io; save_stdout = sys.stdout; sys.stdout = open('trash', 'w'); from evalml.utils import get_evalml_pip_requirements; sys.stdout = save_stdout; print('\n'.join(get_evalml_pip_requirements(os.getcwd())));")
 allow_list=$(echo ${reqs_list} | grep -oE "[a-zA-Z]+[a-zA-Z_\-]*" | paste -d "|" -s -)
 echo "Allow list: ${allow_list}"
 pip freeze | grep -v "evalml.git" | grep -E "${allow_list}" > "${DEPENDENCY_FILE_PATH}"
