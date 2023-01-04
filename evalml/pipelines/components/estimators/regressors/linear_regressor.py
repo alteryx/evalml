c96a6b6b9c489b84c040406b8b11349e2987cb99
Becca McBrayer <becca.mcbrayer@alteryx.com>
2023-01-04 10:00:54 -0500

    Remove normalize parameter

--- evalml/pipelines/components/estimators/regressors/linear_regressor.py
+++ evalml/pipelines/components/estimators/regressors/linear_regressor.py
@@ -14,18 +14,14 @@ class LinearRegressor(Estimator):
         fit_intercept (boolean): Whether to calculate the intercept for this model.
             If set to False, no intercept will be used in calculations (i.e. data is expected to be centered).
             Defaults to True.
-        normalize (boolean): If True, the regressors will be normalized before regression
-            by subtracting the mean and dividing by the l2-norm.
-            This parameter is ignored when fit_intercept is set to False. Defaults to False.
         n_jobs (int or None): Number of jobs to run in parallel. -1 uses all threads. Defaults to -1.
         random_seed (int): Seed for the random number generator. Defaults to 0.
     """
 
     name = "Linear Regressor"
-    hyperparameter_ranges = {"fit_intercept": [True, False], "normalize": [True, False]}
+    hyperparameter_ranges = {"fit_intercept": [True, False]}
     """{
         "fit_intercept": [True, False],
-        "normalize": [True, False]
     }"""
     model_family = ModelFamily.LINEAR_MODEL
     """ModelFamily.LINEAR_MODEL"""
@@ -38,12 +34,9 @@ class LinearRegressor(Estimator):
         ProblemTypes.TIME_SERIES_REGRESSION,
     ]"""
 
-    def __init__(
-        self, fit_intercept=True, normalize=False, n_jobs=-1, random_seed=0, **kwargs
-    ):
+    def __init__(self, fit_intercept=True, n_jobs=-1, random_seed=0, **kwargs):
         parameters = {
             "fit_intercept": fit_intercept,
-            "normalize": normalize,
             "n_jobs": n_jobs,
         }
         parameters.update(kwargs)
