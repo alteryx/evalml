0f5027dca3d16eb672bbbc115d9058b154cacc40
Becca McBrayer <becca.mcbrayer@alteryx.com>
2023-04-04 15:38:27 -0400

    Make sure indices for residuals match

--- evalml/pipelines/time_series_regression_pipeline.py
+++ evalml/pipelines/time_series_regression_pipeline.py
@@ -252,6 +252,7 @@ class TimeSeriesRegressionPipeline(TimeSeriesPipelineBase):
                 y_train=y_train,
                 calculating_residuals=True,
             )
+            predictions_train.index = y_train.index
             residuals = y_train - predictions_train
             std_residual = np.sqrt(np.sum(residuals**2) / len(residuals))
 
