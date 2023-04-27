2ae16b3b86244d17e5f130ab198cbbfd9772156a
christopherbunn <chris.bunn@alteryx.com>
2023-04-27 14:20:36 -0400

    Updated tests and release notes

--- evalml/automl/automl_search.py
+++ evalml/automl/automl_search.py
@@ -1126,9 +1126,6 @@ class AutoMLSearch:
                             cached_data,
                             job_log,
                         )
-                        # pipeline_times[pipeline.name] = time_elapsed(
-                        #     start_pipeline_time,
-                        # )
                         pipeline_times[pipeline.name] = (
                             time.time() - start_pipeline_time
                         )
@@ -1166,7 +1163,6 @@ class AutoMLSearch:
                     f"All pipelines in the current AutoML batch produced a score of np.nan on the primary objective {self.objective}. Exception(s) raised: {error_msgs}. Check the 'errors' attribute of the AutoMLSearch object for a full breakdown of errors and tracebacks.",
                 )
             if len(pipeline_times) > 0:
-                # pipeline_times["Total time of batch"] = time_elapsed(start_batch_time)
                 pipeline_times["Total time of batch"] = time.time() - start_batch_time
                 batch_times[self._get_batch_number()] = pipeline_times
 
