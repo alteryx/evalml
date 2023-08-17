a9ae23329cfd73eb5298216d5b82e10b1182461e
remyogasawara <remyogasawara@gmail.com>
2023-08-17 12:08:33 -0700

    get rid of old lines

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -336,18 +336,7 @@ def setup(app):
             "/home/docs/.ipython/profile_default/startup/",
         )
         shutil.copy("set-headers.py", "/home/docs/.ipython/profile_default/startup")
-        shutil.copy(
-            "add_imports.py",
-            "/home/docs/.ipython/profile_default/startup/start.py",
-        )
-        shutil.copy(
-            "add_imports.py",
-            "/home/docs/.jupyter/profile_default/startup/start.py",
-        )
-        shutil.copy(
-            "add_imports.py",
-            "/Users/AYX104984/.ipython/profile_default/startup/start.py",
-        )
+        shutil.copy("set-imports.py", "/home/docs/.ipython/profile_default/startup/")
     app.add_domain(PatchedPythonDomain, override=True)
     app.add_js_file(
         "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js",
