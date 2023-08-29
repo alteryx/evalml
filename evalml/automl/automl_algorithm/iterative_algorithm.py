032db03cac7a39cb0baf5d031f5904fd616a3c2b
remyogasawara <67338690+remyogasawara@users.noreply.github.com>
2023-08-29 12:57:30 -0700

    Updated `excluded_model_families` data type (#4287)
    * update allowed types and documentaion
    
    * add test for linear models

--- evalml/automl/automl_algorithm/iterative_algorithm.py
+++ evalml/automl/automl_algorithm/iterative_algorithm.py
@@ -43,7 +43,7 @@ class IterativeAlgorithm(AutoMLAlgorithm):
             model families. Run evalml.pipelines.components.utils.allowed_model_families("binary") to see options. Change `binary`
             to `multiclass` or `regression` depending on the problem type. Note that if allowed_pipelines is provided,
             this parameter will be ignored.
-        excluded_model_families (list[ModelFamily]): A list of model families to exclude from the estimators used when building pipelines.
+        excluded_model_families (list(str, ModelFamily)): A list of model families to exclude from the estimators used when building pipelines.
         allowed_component_graphs (dict): A dictionary of lists or ComponentGraphs indicating the component graphs allowed in the search.
             The format should follow { "Name_0": [list_of_components], "Name_1": [ComponentGraph(...)] }
 
