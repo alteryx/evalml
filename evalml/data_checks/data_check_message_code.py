049292164d6faee94f626b84b5b076a628528b85
MichaelFu512 <MichaelFu512@gmail.com>
2023-09-01 13:13:38 -0700

    forgot to add the message code

--- evalml/data_checks/data_check_message_code.py
+++ evalml/data_checks/data_check_message_code.py
@@ -145,3 +145,8 @@ class DataCheckMessageCode(Enum):
 
     MISMATCHED_SERIES_LENGTH = "mismatched_series_length"
     """Message code for when one or more unique series in a multiseries dataset is of a different length than the others"""
+
+    MULTISERIES_TIMESERIES_SERIES_ID_NOT_IN_COL = (
+        "multiseries_timeseries_series_id_not_in_col"
+    )
+    """Message code for when given series_id is not in the dataset"""
