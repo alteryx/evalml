cf6bc94a766730fdbed1006c97366ccf437879aa
Becca McBrayer <becca.mcbrayer@alteryx.com>
2023-09-19 13:12:51 -0400

    Update split_data to call split_multiseries_data (#4312)
    * Update split_data to call split_multiseries_data

--- evalml/tests/preprocessing_tests/test_split_data.py
+++ evalml/tests/preprocessing_tests/test_split_data.py
@@ -6,6 +6,7 @@ from evalml.problem_types import (
     ProblemTypes,
     is_binary,
     is_multiclass,
+    is_multiseries,
     is_regression,
     is_time_series,
 )
@@ -19,6 +20,7 @@ def test_split_data(
     X_y_binary,
     X_y_multi,
     X_y_regression,
+    multiseries_ts_data_unstacked,
     make_data_type,
 ):
     if is_binary(problem_type):
@@ -30,6 +32,8 @@ def test_split_data(
     problem_configuration = None
     if is_time_series(problem_type):
         problem_configuration = {"gap": 1, "max_delay": 7, "time_index": "date"}
+        if is_multiseries(problem_type):
+            X, y = multiseries_ts_data_unstacked
 
     X = make_data_type(data_type, X)
     y = make_data_type(data_type, y)
@@ -50,17 +54,28 @@ def test_split_data(
     assert len(y_test) == test_size
     assert isinstance(X_train, pd.DataFrame)
     assert isinstance(X_test, pd.DataFrame)
-    assert isinstance(y_train, pd.Series)
-    assert isinstance(y_test, pd.Series)
+    if not is_multiseries(problem_type):
+        assert isinstance(y_train, pd.Series)
+        assert isinstance(y_test, pd.Series)
+    else:
+        assert isinstance(y_train, pd.DataFrame)
+        assert isinstance(y_test, pd.DataFrame)
+        pd.testing.assert_frame_equal(X_test, X[int(train_size) :], check_dtype=False)
+        pd.testing.assert_frame_equal(y_test, y[int(train_size) :], check_dtype=False)
 
-    if is_time_series(problem_type):
+    if is_time_series(problem_type) and not is_multiseries(problem_type):
         pd.testing.assert_frame_equal(X_test, X[int(train_size) :], check_dtype=False)
         pd.testing.assert_series_equal(y_test, y[int(train_size) :], check_dtype=False)
 
 
 @pytest.mark.parametrize("problem_type", ProblemTypes.all_problem_types)
 @pytest.mark.parametrize("data_type", ["np", "pd", "ww"])
-def test_split_data_defaults(problem_type, data_type, get_test_data_from_configuration):
+def test_split_data_defaults(
+    problem_type,
+    data_type,
+    get_test_data_from_configuration,
+    multiseries_ts_data_unstacked,
+):
     X, y = get_test_data_from_configuration(
         data_type,
         problem_type,
@@ -71,6 +86,8 @@ def test_split_data_defaults(problem_type, data_type, get_test_data_from_configu
     problem_configuration = None
     if is_time_series(problem_type):
         problem_configuration = {"gap": 1, "max_delay": 7, "time_index": "date"}
+        if is_multiseries(problem_type):
+            X, y = multiseries_ts_data_unstacked
         test_pct = 0.1
     else:
         test_pct = 0.2
@@ -92,7 +109,18 @@ def test_split_data_defaults(problem_type, data_type, get_test_data_from_configu
             X = pd.DataFrame(X)
             y = pd.Series(y)
         pd.testing.assert_frame_equal(X_test, X[int(train_size) :], check_dtype=False)
-        pd.testing.assert_series_equal(y_test, y[int(train_size) :], check_dtype=False)
+        if not is_multiseries(problem_type):
+            pd.testing.assert_series_equal(
+                y_test,
+                y[int(train_size) :],
+                check_dtype=False,
+            )
+        else:
+            pd.testing.assert_frame_equal(
+                y_test,
+                y[int(train_size) :],
+                check_dtype=False,
+            )
 
 
 @pytest.mark.parametrize("test", ["fh_limitation", "no_fh_limitation"])
@@ -127,8 +155,33 @@ def test_split_data_ts(test, X_y_regression):
     assert len(y_test) == test_size
 
 
+def test_split_data_calls_multiseries_error(multiseries_ts_data_stacked):
+    X, y = multiseries_ts_data_stacked
+    with pytest.raises(
+        ValueError,
+        match="requires problem_configuration for multiseries",
+    ):
+        split_data(X, y, problem_type="multiseries time series regression")
+
+    with pytest.raises(
+        ValueError,
+        match="needs both series_id and time_index values in the problem_configuration",
+    ):
+        split_data(
+            X,
+            y,
+            problem_type="multiseries time series regression",
+            problem_configuration={"time_index": "date"},
+        )
+
+
 @pytest.mark.parametrize("no_features", [True, False])
-def test_split_multiseries_data(no_features, multiseries_ts_data_stacked):
+@pytest.mark.parametrize("splitting_function", ["split_data", "split_multiseries_data"])
+def test_split_multiseries_data(
+    no_features,
+    splitting_function,
+    multiseries_ts_data_stacked,
+):
     X, y = multiseries_ts_data_stacked
 
     if no_features:
@@ -137,12 +190,22 @@ def test_split_multiseries_data(no_features, multiseries_ts_data_stacked):
     X_train_expected, X_holdout_expected = X[:-10], X[-10:]
     y_train_expected, y_holdout_expected = y[:-10], y[-10:]
 
-    X_train, X_holdout, y_train, y_holdout = split_multiseries_data(
-        X,
-        y,
-        "series_id",
-        "date",
-    )
+    # Results should be identical whether split_multiseries_data is called through
+    # split_data or directly
+    if splitting_function == "split_data":
+        X_train, X_holdout, y_train, y_holdout = split_data(
+            X,
+            y,
+            problem_type="multiseries time series regression",
+            problem_configuration={"time_index": "date", "series_id": "series_id"},
+        )
+    else:
+        X_train, X_holdout, y_train, y_holdout = split_multiseries_data(
+            X,
+            y,
+            "series_id",
+            "date",
+        )
 
     pd.testing.assert_frame_equal(
         X_train.sort_index(axis=1),
